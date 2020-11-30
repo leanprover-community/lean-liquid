d6b20bec650c4a3508c492192744d1307d942529
Johan Commelin <johan@commelin.net>
2020-11-30 13:47:08 +0000

    unsorry definition of polyhedral lattice

--- src/polyhedral_lattice.lean
+++ src/polyhedral_lattice.lean
@@ -12,11 +12,15 @@ equipped with a norm `||·|| : Λ ⊗ R → R`
 that is given by the supremum of finitely many linear functions on `Λ`
 with rational coefficients;
 equivalently,
-the “unit ball” `{λ ∈ Λ ⊗ R | ||λ||Λ ≤ 1}` is a rational polyhedron.
+the “unit ball” `{λ ∈ Λ ⊗ R | ||λ|| ≤ 1}` is a rational polyhedron.
 
 See the paragraph before Thm 9.5 of [Analytic].
 -/
 
+open finset
+
 class polyhedral_lattice (Λ : Type u) extends normed_group Λ :=
-(finite_free : ∃ {ι : Type} [fintype ι] (v : ι → Λ), is_basis ℤ v)
-(is_polyhedral : (sorry : Prop))
+(exists_finite_basis : ∃ {ι : Type} [fintype ι] (v : ι → Λ), is_basis ℤ v)
+(is_polyhedral : ∃ {ι : Type} [nonempty ι] [fintype ι] (f : ι → Λ → ℚ),
+                  ∀ x : Λ, ∥x∥ = finset.max' (finset.image (λ i : ι, f i x) univ)
+                                  (nonempty.image univ_nonempty _))
