34a574d6b9c847230c6de5e5e8ea98a69d02d76b
Johan Commelin <johan@commelin.net>
2020-12-21 18:52:30 +0000

    a stub on the hat construction

---
+++ src/locally_constant/Vhat.lean
@@ -0,0 +1,45 @@
+import locally_constant.analysis
+import topology.category.Profinite
+import topology.algebra.group_completion
+import topology.metric_space.completion
+
+noncomputable theory
+
+variables (V : Type*) (S : Type*) [normed_group V] [topological_space S]
+variables [compact_space S] [t2_space S] [totally_disconnected_space S]
+
+-- move this
+section for_mathlib
+
+namespace uniform_space
+namespace completion
+
+instance (V : Type*) [uniform_space V] [has_norm V] :
+  has_norm (completion V) :=
+{ norm := completion.extension has_norm.norm }
+
+instance : normed_group (completion V) :=
+{ dist_eq :=
+  begin
+    intros x y,
+    apply completion.induction_on₂ x y; clear x y,
+    { sorry },
+    { intros x y,
+      show completion.extension₂ _ _ _ = completion.extension _ _,
+      rw [sub_eq_add_neg, ← completion.coe_neg, ← completion.coe_add, ← sub_eq_add_neg],
+      rw [completion.extension₂_coe_coe, completion.extension_coe, dist_eq_norm],
+      { exact sorry },
+      { exact uniform_continuous_dist } }
+  end,
+  .. (show add_comm_group (completion V), by apply_instance),
+  .. (show metric_space (completion V), by apply_instance) }
+
+end completion
+end uniform_space
+
+end for_mathlib
+
+local attribute [instance] locally_constant.normed_group
+
+@[derive [add_comm_group, metric_space]]
+def hat := uniform_space.completion (locally_constant S V)
