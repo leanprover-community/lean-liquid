5a98a01f905995b00dcdececbcb7beba21dfb79f
Johan Commelin <johan@commelin.net>
2020-12-23 21:37:41 +0000

    forgotten file

---
+++ src/normed_with_aut.lean
@@ -0,0 +1,11 @@
+import NormedGroup
+
+/-- A `normed_with_aut r V` structure on a normed abelian group `V`
+consists of an automorphism `T` satisfying `∥T v∥ = r * ∥v∥`.
+
+In other words, it is a normed `ℤ[T^{±1}]`-module satisfying `∥T v∥ = r * ∥v∥`.
+
+Definition 8.13 of [Analytic] -/
+class normed_with_aut (r : ℝ) (V : NormedGroup) :=
+(T : V ≅ V)
+(norm_T : ∀ v : V, ∥(T.hom : V → V) v∥ = r * ∥v∥)
