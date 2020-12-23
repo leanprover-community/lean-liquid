ae7544ba27d56b3335d1693c64056acae2e96b30
Johan Commelin <johan@commelin.net>
2020-12-23 18:52:22 +0000

    fill in some sorrys

--- src/locally_constant/Vhat.lean
+++ src/locally_constant/Vhat.lean
@@ -21,17 +21,33 @@ instance (V : Type*) [uniform_space V] [has_norm V] :
   has_norm (completion V) :=
 { norm := completion.extension has_norm.norm }
 
+lemma uniform_continuous_norm : uniform_continuous (norm : V → ℝ) :=
+begin
+  rw metric.uniform_continuous_iff,
+  intros ε hε,
+  use [ε, hε],
+  intros x y hxy,
+  rw dist_eq_norm at hxy ⊢,
+  calc ∥∥x∥ - ∥y∥∥
+      = abs(∥x∥ - ∥y∥) : by rw real.norm_eq_abs
+  ... ≤ ∥x - y∥ : abs_norm_sub_norm_le x y
+  ... < ε : hxy
+end
+
 instance : normed_group (completion V) :=
 { dist_eq :=
   begin
     intros x y,
     apply completion.induction_on₂ x y; clear x y,
-    { sorry },
+    { refine is_closed_eq (completion.uniform_continuous_extension₂ _).continuous _,
+      refine continuous.comp _ continuous_sub,
+      exact completion.continuous_extension },
     { intros x y,
       show completion.extension₂ _ _ _ = completion.extension _ _,
+      -- the following line needs `completion.coe_sub`
       rw [sub_eq_add_neg, ← completion.coe_neg, ← completion.coe_add, ← sub_eq_add_neg],
       rw [completion.extension₂_coe_coe, completion.extension_coe, dist_eq_norm],
-      { exact sorry },
+      { exact uniform_continuous_norm _ },
       { exact uniform_continuous_dist } }
   end,
   .. (show add_comm_group (completion V), by apply_instance),
