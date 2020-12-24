4a04f320fafa2286c1d4bb7de0840cd0d25eba06
Johan Commelin <johan@commelin.net>
2020-12-24 08:36:39 +0000

    switch to bounded maps

--- src/NormedGroup.lean
+++ src/NormedGroup.lean
@@ -34,43 +34,71 @@ end for_mathlib
 
 open category_theory
 
-/-- The category of normed abelian groups and continuous group homomorphisms. -/
+/-- The category of normed abelian groups and bounded group homomorphisms. -/
 def NormedGroup : Type (u+1) := bundled normed_group
 
 set_option old_structure_cmd true
 
-/-- A morphism of normed abelian groups is a continuous group homomorphism. -/
-structure normed_group_hom (V W : Type*) [normed_group V] [normed_group W] extends
-  add_monoid_hom V W, continuous_map V W.
+/-- A morphism of normed abelian groups is a bounded group homomorphism. -/
+structure normed_group_hom (V W : Type*) [normed_group V] [normed_group W]
+  extends add_monoid_hom V W :=
+(bound' : ∃ C, ∀ v, ∥to_fun v∥ ≤ C * ∥v∥)
 
 namespace normed_group_hom
 
-attribute [continuity] normed_group_hom.continuous_to_fun
-
 variables {V V₁ V₂ V₃ : Type*}
 variables [normed_group V] [normed_group V₁] [normed_group V₂] [normed_group V₃]
+variables (f g : normed_group_hom V₁ V₂)
 
 instance : has_coe_to_fun (normed_group_hom V₁ V₂) := ⟨_, normed_group_hom.to_fun⟩
 
-variables {f g : normed_group_hom V₁ V₂}
+@[simp] lemma map_zero : f 0 = 0 := f.to_add_monoid_hom.map_zero
 
-protected lemma continuous (f : normed_group_hom V₁ V₂) : continuous f := f.continuous_to_fun
+@[simp] lemma map_add (x y) : f (x + y) = f x + f y := f.to_add_monoid_hom.map_add _ _
+
+@[simp] lemma map_sub (x y) : f (x - y) = f x - f y := f.to_add_monoid_hom.map_sub _ _
+
+@[simp] lemma map_neg (x) : f (-x) = -(f x) := f.to_add_monoid_hom.map_neg _
+
+lemma bound : ∃ C, 0 ≤ C ∧ ∀ v, ∥f v∥ ≤ C * ∥v∥ :=
+begin
+  obtain ⟨C, hC⟩ := f.bound',
+  use [max C 0],
+  simp only [le_max_iff, le_refl, or_true, true_and],
+  intro v,
+  calc ∥f v∥
+      ≤ C * ∥v∥ : hC v
+  ... ≤ max C 0 * ∥v∥ : mul_le_mul (le_max_left _ _) le_rfl (norm_nonneg _) (le_max_right _ _)
+end
+
+lemma lipschitz_of_bound (C : ℝ) (h : ∀x, ∥f x∥ ≤ C * ∥x∥) :
+  lipschitz_with (nnreal.of_real C) f :=
+lipschitz_with.of_dist_le' $ λ x y, by simpa only [dist_eq_norm, f.map_sub] using h (x - y)
+
+theorem antilipschitz_of_bound {K : nnreal} (h : ∀ x, ∥x∥ ≤ K * ∥f x∥) :
+  antilipschitz_with K f :=
+antilipschitz_with.of_le_mul_dist $
+λ x y, by simpa only [dist_eq_norm, f.map_sub] using h (x - y)
 
 protected lemma uniform_continuous (f : normed_group_hom V₁ V₂) :
   uniform_continuous f :=
 begin
-  haveI : is_add_group_hom f := f.to_add_monoid_hom.is_add_group_hom,
-  apply uniform_continuous_of_continuous,
-  exact f.continuous
+  obtain ⟨C, C_pos, hC⟩ := f.bound,
+  exact (lipschitz_of_bound f C hC).uniform_continuous
 end
 
-@[continuity] lemma coe_continuous : continuous (f : V₁ → V₂) := f.continuous_to_fun
+@[continuity]
+protected lemma continuous (f : normed_group_hom V₁ V₂) : continuous f :=
+f.uniform_continuous.continuous
+
+variables {f g}
 
 @[ext] theorem ext (H : ∀ x, f x = g x) : f = g :=
 by cases f; cases g; congr'; exact funext H
 
 instance : has_zero (normed_group_hom V₁ V₂) :=
-⟨{ continuous_to_fun := continuous_const, .. (0 : V₁ →+ V₂) }⟩
+⟨{ bound' := ⟨0, λ v, show ∥(0 : V₂)∥ ≤ 0 * _, by rw [norm_zero, zero_mul]⟩,
+   .. (0 : V₁ →+ V₂) }⟩
 
 instance : inhabited (normed_group_hom V₁ V₂) := ⟨0⟩
 
@@ -79,24 +107,60 @@ by cases f; cases g; cases h; refl
 
 /-- The identity as a continuous map. -/
 def id : normed_group_hom V V :=
-{ .. add_monoid_hom.id V, .. continuous_map.id }
+{ bound' := ⟨1, λ v, show ∥v∥ ≤ 1 * ∥v∥, by rw [one_mul]⟩,
+  .. add_monoid_hom.id V }
 
 /-- The composition of continuous maps, as a continuous map. -/
 def comp (g : normed_group_hom V₂ V₃) (f : normed_group_hom V₁ V₂) :
   normed_group_hom V₁ V₃ :=
-{ .. g.to_add_monoid_hom.comp f.to_add_monoid_hom, .. g.to_continuous_map.comp f.to_continuous_map }
+{ bound' :=
+  begin
+    obtain ⟨Cf, Cf_pos, hf⟩ := f.bound,
+    obtain ⟨Cg, Cg_pos, hg⟩ := g.bound,
+    use [Cg * Cf],
+    assume v,
+    calc ∥g (f v)∥
+        ≤ Cg * ∥f v∥    : hg _
+    ... ≤ Cg * Cf * ∥v∥ : _,
+    rw mul_assoc,
+    exact mul_le_mul le_rfl (hf v) (norm_nonneg _) Cg_pos
+  end
+  .. g.to_add_monoid_hom.comp f.to_add_monoid_hom }
 
 instance : has_add (normed_group_hom V₁ V₂) :=
 ⟨λ f g,
-{ continuous_to_fun := f.continuous.add g.continuous,
+{ bound' :=
+  begin
+    obtain ⟨Cf, Cf_pos, hCf⟩ := f.bound,
+    obtain ⟨Cg, Cg_pos, hCg⟩ := g.bound,
+    use [Cf + Cg],
+    assume v,
+    calc ∥f v + g v∥
+        ≤ ∥f v∥ + ∥g v∥ : norm_add_le _ _
+    ... ≤ Cf * ∥v∥ + Cg * ∥v∥ : add_le_add (hCf _) (hCg _)
+    ... = (Cf + Cg) * ∥v∥ : by rw add_mul
+  end,
   .. (f.to_add_monoid_hom + g.to_add_monoid_hom) }⟩
 
 instance : has_neg (normed_group_hom V₁ V₂) :=
-⟨λ f, { continuous_to_fun := f.continuous.neg, .. (-f.to_add_monoid_hom) }⟩
+⟨λ f,
+{ bound' :=
+  begin
+    obtain ⟨C, C_pos, hC⟩ := f.bound,
+    use C,
+    assume v,
+    calc ∥-(f v)∥
+        = ∥f v∥ : norm_neg _
+    ... ≤ C * ∥v∥ : hC _
+  end, .. (-f.to_add_monoid_hom) }⟩
 
 instance : has_sub (normed_group_hom V₁ V₂) :=
 ⟨λ f g,
-{ continuous_to_fun := f.continuous.sub g.continuous,
+{ bound' :=
+  begin
+    simp only [add_monoid_hom.sub_apply, add_monoid_hom.to_fun_eq_coe, sub_eq_add_neg],
+    exact (f + -g).bound'
+  end,
   .. (f.to_add_monoid_hom - g.to_add_monoid_hom) }⟩
 
 @[simp] lemma coe_zero : ⇑(0 : normed_group_hom V₁ V₂) = 0 := rfl
--- src/locally_constant/NormedGroup.lean
+++ src/locally_constant/NormedGroup.lean
@@ -3,9 +3,17 @@ import NormedGroup
 
 noncomputable theory
 
+lemma real.Sup_mul (r : ℝ) (s : set ℝ) (hr : 0 ≤ r) :
+  Sup ({x | ∃ y ∈ s, r * y = x}) = r * Sup s :=
+begin
+  sorry
+end
+
 namespace NormedGroup
 
 local attribute [instance] locally_constant.normed_group locally_constant.metric_space
+
+set_option pp.all true
 
 @[simps]
 def LocallyConstant (S : Type*) [topological_space S] [compact_space S] :
@@ -13,8 +21,32 @@ def LocallyConstant (S : Type*) [topological_space S] [compact_space S] :
 { obj := λ V, NormedGroup.of $ locally_constant S V,
   map := λ V W f,
   { to_fun := locally_constant.map f,
-    continuous_to_fun := sorry, -- this is false if `f` is not bounded
     map_zero' := by { ext, exact f.map_zero' },
-    map_add' := by { intros x y, ext s, apply f.map_add' } } }
+    map_add' := by { intros x y, ext s, apply f.map_add' },
+    bound' :=
+    begin
+      obtain ⟨C, C_pos, hC⟩ := f.bound,
+      use C,
+      rintro (g : locally_constant _ _),
+      calc Sup (set.range (λ x, ∥f (g x)∥))
+          ≤ Sup (set.range (λ x, C * ∥g x∥)) : _
+      ... = C * Sup (set.range (λ x, ∥g x∥)) : _,
+      { by_cases H : nonempty S, swap,
+        { simp only [set.range_eq_empty.mpr H, real.Sup_empty] },
+        apply real.Sup_le_ub,
+        { obtain ⟨x⟩ := H, exact ⟨_, set.mem_range_self x⟩ },
+        rintro _ ⟨x, rfl⟩,
+        calc ∥f (g x)∥ ≤ C * ∥g x∥ : hC _
+        ... ≤ Sup _ : real.le_Sup _ _ _,
+        { apply real.Sup_exists_of_finite,
+          rw [set.range_comp, set.range_comp],
+          exact (g.range_finite.image _).image _ },
+        { refine ⟨x, _⟩,
+          -- refl doesn't want to close the goal :shock:
+          dsimp [NormedGroup], refl } },
+      { convert real.Sup_mul C _ C_pos,
+        ext,
+        simp only [exists_prop, set.mem_range, exists_exists_eq_and, set.mem_set_of_eq], }
+    end } }
 
 end NormedGroup
