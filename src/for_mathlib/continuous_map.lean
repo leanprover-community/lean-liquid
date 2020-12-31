0d7967fc0d6854890ae17159af290aba7795aa2f
Johan Commelin <johan@commelin.net>
2020-12-31 16:03:27 +0000

    forgotten file

---
+++ src/for_mathlib/continuous_map.lean
@@ -0,0 +1,10 @@
+import topology.continuous_map
+
+namespace continuous_map
+
+variables {X Y : Type*} [topological_space X] [topological_space Y]
+
+@[simp] lemma coe_mk (f : X → Y) (h : continuous f) :
+  ⇑(⟨f, h⟩ : continuous_map X Y) = f := rfl
+
+end continuous_map
