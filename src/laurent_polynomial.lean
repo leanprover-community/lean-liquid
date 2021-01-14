36613251f05f4f262a6c16409c4ca967b6c33ffb
Johan Commelin <johan@commelin.net>
2021-01-14 14:01:15 +0000

    comment out lattice.lean and laurent_polynomial.lean
    we don't need this for the time being,
    and it currently generates warnings that we don't care about.

--- src/laurent_polynomial.lean
+++ src/laurent_polynomial.lean
@@ -1,34 +1,34 @@
-import data.polynomial
-import data.mv_polynomial
-import ring_theory.ideal.operations
+-- import data.polynomial
+-- import data.mv_polynomial
+-- import ring_theory.ideal.operations
 
-universe variables u
+-- universe variables u
 
-constant laurent_polynomial (R : Type u) : Type u
+-- constant laurent_polynomial (R : Type u) : Type u
 
-namespace laurent_polynomial
-open polynomial mv_polynomial
-variables (R : Type u) [comm_ring R]
+-- namespace laurent_polynomial
+-- open polynomial mv_polynomial
+-- variables (R : Type u) [comm_ring R]
 
-@[instance]
-constant comm_ring : comm_ring (laurent_polynomial R)
+-- @[instance]
+-- constant comm_ring : comm_ring (laurent_polynomial R)
 
-variables {R}
+-- variables {R}
 
-constant of_polynomial : polynomial R → laurent_polynomial R
+-- constant of_polynomial : polynomial R → laurent_polynomial R
 
-constant of_inv_polynomial : polynomial R → laurent_polynomial R
+-- constant of_inv_polynomial : polynomial R → laurent_polynomial R
 
-lemma of_polynomial_X_mul_of_inv_polynomial_X :
-  of_polynomial (X : polynomial R) * of_inv_polynomial X = 1 := sorry
+-- lemma of_polynomial_X_mul_of_inv_polynomial_X :
+--   of_polynomial (X : polynomial R) * of_inv_polynomial X = 1 := sorry
 
-constant as_quotient : mv_polynomial bool R →+* laurent_polynomial R
+-- constant as_quotient : mv_polynomial bool R →+* laurent_polynomial R
 
-lemma as_quotient_surjective :
-  function.surjective (@as_quotient R _) :=
-sorry
+-- lemma as_quotient_surjective :
+--   function.surjective (@as_quotient R _) :=
+-- sorry
 
-lemma as_quotient_ker :
-  (@as_quotient R _).ker = ideal.span {X tt * X ff - 1} := sorry
+-- lemma as_quotient_ker :
+--   (@as_quotient R _).ker = ideal.span {X tt * X ff - 1} := sorry
 
-end laurent_polynomial
+-- end laurent_polynomial
