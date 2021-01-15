9436b9c31bbe39b0fa0a3c690850a67c62522e31
Johan Commelin <johan@commelin.net>
2021-01-15 19:30:49 +0000

    document the by-exactI hack

--- src/by_exactI_hack.lean
+++ src/by_exactI_hack.lean
@@ -1,4 +1,5 @@
-section by_exactI_hack
+-- brilliant hack by Gabriel Ebner
+-- thanks!!
 
 open tactic expr
 open lean
@@ -6,11 +7,12 @@ open lean.parser
 open interactive
 open tactic
 
-reserve prefix `​ `:100
+reserve prefix `​ `:100 -- zero-width space Unicode char
 
 @[user_notation]
 meta def exact_notation (_ : parse $ tk "​") (e : parse (lean.parser.pexpr 0)) : parser pexpr := do
 expr.macro d [_] ← pure ``(by skip),
 pure $ expr.macro d [const `tactic.interactive.exactI [] (cast undefined e.reflect)]
 
-end by_exactI_hack
+-- files that import this file can use the zero-width space char
+-- to reset the TC instance cache, and avoid `by exactI` kludges
