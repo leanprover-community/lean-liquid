7d8f979d8ab6fc7b91a465d3673ea50416c94e9b
Johan Commelin <johan@commelin.net>
2021-01-25 16:13:27 +0000

    add defn of polyhedral lattice

--- src/combinatorial_lemma.lean
+++ src/combinatorial_lemma.lean
@@ -1,24 +1,16 @@
 import algebra.group.basic
 import normed_group.NormedGroup
 
+import polyhedral_lattice
+
 /-!
 In this file we state and prove lemmas 9.7 and 9.8 of [Analytic].
 -/
 
-section move_this
-
--- rewrite to include multiplicative version
-def torsion_free (A : Type*) [add_comm_group A] : Prop :=
-∀ (a : A) (n : ℕ), n • a = 0 → n = 0
-
--- do we have this in mathlib for mere groups (not modules)??
-def finitely_generated (A : Type*) [add_comm_group A] : Prop :=
-∃ s : finset A, ∀ a : A, a ∈ add_subgroup.closure (s : set A)
-
 variables (Λ : Type*) [add_comm_group Λ]
 
 /-- Lemma 9.7 of [Analytic]. -/
-lemma lem97 (hΛ_tf : torsion_free Λ) (hΛ_fg : finitely_generated Λ)
+lemma lem97 (hΛ_tf : torsion_free Λ) (hΛ_fg : module.finite ℤ Λ)
   (N : ℕ) (s : finset Λ) :
   ∃ F : finset (Λ →+ ℤ), ∀ x : Λ →+ ℤ, ∃ (x' ∈ F) (y : Λ →+ ℤ),
     x - x' = N • y ∧
@@ -26,5 +18,3 @@ lemma lem97 (hΛ_tf : torsion_free Λ) (hΛ_fg : finitely_generated Λ)
 begin
   sorry
 end
-
-end move_this
