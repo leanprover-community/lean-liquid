0614d3ce8e68cd0e9b12a621e4439368fef4dbac
Johan Commelin <johan@commelin.net>
2021-01-25 19:55:34 +0000

    int is a polyhedral lattice: sorry

--- src/polyhedral_lattice.lean
+++ src/polyhedral_lattice.lean
@@ -3,13 +3,14 @@ import ring_theory.finiteness
 
 import hacks_and_tricks.by_exactI_hack
 
+noncomputable theory
 open_locale big_operators
 
 section move_this
 
 -- rewrite to include multiplicative version
 def torsion_free (A : Type*) [add_comm_group A] : Prop :=
-∀ (a : A) (n : ℕ), n • a = 0 → n = 0
+∀ (a : A) (ha : a ≠ 0) (n : ℕ), n • a = 0 → n = 0
 
 end move_this
 
@@ -27,6 +28,25 @@ begin
   obtain ⟨s, h1, h2⟩ := polyhedral_lattice.polyhedral' A,
   exact ⟨s, h1⟩
 end
+
+instance int : polyhedral_lattice ℤ :=
+{ tf := λ m hm n h,
+  begin
+    rw [← nsmul_eq_smul, nsmul_eq_mul, mul_eq_zero] at h,
+    simpa only [hm, int.coe_nat_eq_zero, or_false, int.nat_cast_eq_coe_nat] using h
+  end,
+  polyhedral' :=
+  begin
+    use {1},
+    split,
+    { simp [finset.coe_singleton],
+      rw eq_top_iff,
+      rintro n -,
+      rw submodule.mem_span_singleton,
+      use n,
+      rw [← gsmul_eq_smul, gsmul_one, int.cast_id] },
+    { sorry }
+  end }
 
 -- lemma polyhedral : ∃ (ι : Type*) [fintype ι] (a : ι → A),
 --   submodule.span ℤ (set.range a) = ⊤ ∧
