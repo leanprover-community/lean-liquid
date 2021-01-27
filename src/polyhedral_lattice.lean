b88e023230920e5c1608c789046d9b12e1efc494
Johan Commelin <johan@commelin.net>
2021-01-27 07:24:04 +0000

    remove bad defn of polyhedral lattice

--- src/polyhedral_lattice.lean
+++ src/polyhedral_lattice.lean
@@ -16,18 +16,14 @@ end move_this
 
 class polyhedral_lattice (A : Type*) [normed_group A] :=
 (tf : torsion_free A)
-(polyhedral' [] : ∃ s : finset A, submodule.span ℤ (s : set A) = ⊤ ∧
-              ∀ n : {a // a ∈ s} → ℤ, ∥∑ a, n a • a.1∥ = ∑ a, ∥ n a ∥ • ∥a.1∥)
+(polyhedral' [] : sorry)
 
 namespace polyhedral_lattice
 
 variables (A : Type*) [normed_group A] [polyhedral_lattice A]
 
 instance : module.finite ℤ A :=
-begin
-  obtain ⟨s, h1, h2⟩ := polyhedral_lattice.polyhedral' A,
-  exact ⟨s, h1⟩
-end
+sorry
 
 instance int : polyhedral_lattice ℤ :=
 { tf := λ m hm n h,
@@ -35,31 +31,6 @@ instance int : polyhedral_lattice ℤ :=
     rw [← nsmul_eq_smul, nsmul_eq_mul, mul_eq_zero] at h,
     simpa only [hm, int.coe_nat_eq_zero, or_false, int.nat_cast_eq_coe_nat] using h
   end,
-  polyhedral' :=
-  begin
-    use {1},
-    split,
-    { simp [finset.coe_singleton],
-      rw eq_top_iff,
-      rintro n -,
-      rw submodule.mem_span_singleton,
-      use n,
-      rw [← gsmul_eq_smul, gsmul_one, int.cast_id] },
-    { intros x,
-      let b : {a : ℤ // a ∈ ({1} : finset ℤ)} := ⟨1, by simp⟩,
-      have : (finset.univ : finset {a // a ∈ ({1} : finset ℤ)}) = {b}, by tauto,
-      simp only [this, mul_one, algebra.id.smul_eq_mul, eq_self_iff_true, finset.mem_singleton,
-        finset.sum_singleton, subtype.coe_mk, norm_one, subtype.val_eq_coe],
-      congr,
-      exact gsmul_int_one _ },
-  end }
-
--- lemma polyhedral : ∃ (ι : Type*) [fintype ι] (a : ι → A),
---   submodule.span ℤ (set.range a) = ⊤ ∧
---   ​∀ n : ι → ℤ, ∥∑ i, n i • a i∥ = ∑ i, n i • ∥a i∥ :=
--- begin
---   obtain ⟨s, h1, h2⟩ := polyhedral_lattice.polyhedral' A,
---   refine ⟨{a // a ∈ s}, _⟩,
--- end
+  polyhedral' := sorry }
 
 end polyhedral_lattice
