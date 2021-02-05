aba116558ab27585dac46f3f5b9c2d2b4556dcac
Johan Commelin <johan@commelin.net>
2021-02-05 20:52:16 +0000

    fix build

--- src/normed_snake.lean
+++ src/normed_snake.lean
@@ -11,10 +11,10 @@ variables (M M' N : system_of_complexes.{u}) (f : M ⟶ M') (g : M' ⟶ N)
 /-- The normed snake lemma, weak version. See Proposition 9.10 from Analytic.pdf -/
 --TODO Add the non weak version for complete system of complxes
 lemma weak_normed_snake (k : ℝ≥0) (m : ℤ) (c₀ : ℝ≥0) [hk : fact (1 ≤ k)]
-  (hf : ∀ c i, is_strict (f.apply c i))
+  (hf : ∀ c i, is_strict (f.apply : M.X c i ⟶ M'.X c i))
   (Hf : ∀ (c : ℝ≥0) (i : ℤ) (hi : i ≤ m + 1) (x : M.X (k * c) i),
-    ∥(M.res x : M.X c i)∥ ≤ k * ∥f.apply (k * c) i x∥)
-  (hg : ∀ c i, (g.apply c i).ker = (f.apply c i).range)
+    ∥(M.res x : M.X c i)∥ ≤ k * ∥f.apply x∥)
+  (hg : ∀ c i, (g.apply : M'.X c i ⟶ N.X c i).ker = f.apply.range)
   (hgquot : system_of_complexes.is_quotient g)
   (hM : M.is_weak_bdd_exact_for_bdd_degree_above_idx k m c₀)
   (hM' : M'.is_weak_bdd_exact_for_bdd_degree_above_idx k m c₀)
@@ -67,15 +67,15 @@ begin
 
   obtain ⟨m', hm'⟩ := (hgquot _ _).surjective n,
   let m₁' := M'.d m',
-  have hm₁' : g.apply _ _ m₁' = N.d n := by simpa [hm'] using (d_apply _ _ g m').symm,
+  have hm₁' : g.apply m₁' = N.d n := by simpa [hm'] using (d_apply _ _ g m').symm,
   obtain ⟨m₁'', hm₁''⟩ := quotient_norm_lift (hgquot _ _) hε₁ (N.d n),
-  have hm₁exist : ∃ m₁ : M.X _ _,  f.apply _ _ m₁ + m₁'' = m₁',
-  { have hrange : m₁' - m₁'' ∈ (f.apply _ _).range,
+  have hm₁exist : ∃ m₁ : M.X _ _,  f.apply m₁ + m₁'' = m₁',
+  { have hrange : m₁' - m₁'' ∈ f.apply.range,
     { rw [← hg _ _, mem_ker  _ _, map_sub, hm₁', hm₁''.1, sub_self] },
     obtain ⟨m₁, hm₁⟩ := (mem_range _ _).1 hrange,
     exact ⟨m₁, by rw [hm₁, sub_add_cancel]⟩ },
   obtain ⟨m₁, hm₁⟩ := hm₁exist,
-  have hm₂ : f.apply _ _ (M.d m₁) = -(M'.d m₁''),
+  have hm₂ : f.apply (M.d m₁) = -(M'.d m₁''),
   { rw [← d_apply, eq_sub_of_add_eq hm₁, map_sub, ← coe_comp, d, d, homological_complex.d_squared,
       coe_zero, ← neg_inj, pi.zero_apply, zero_sub], },
   have hle := Hf _ _ hi3 (M.d m₁),
@@ -86,22 +86,22 @@ begin
     (le_trans zero_le_one hk)),
   obtain ⟨m₀, hm₀⟩ := hM _ hkc _ hi1 (M.res m₁) ε₁ hε₁,
   rw [res_res, d_res _] at hm₀,
-  let mnew' := (M'.res m')  - (f.apply _ _ m₀),
+  let mnew' := M'.res m' - f.apply m₀,
   let mnew₁' := M'.d mnew',
-  have hmnew' : mnew₁' = M'.res m₁'' + f.apply _ _ (M.res m₁ - M.d m₀),
-  { calc mnew₁' = M'.d ((M'.res m')  - (f.apply _ _ m₀)) : by refl
-            ... = M'.res (M'.d m')  - (f.apply _ _ (M.d m₀)) : by rw [map_sub, d_res _, d_apply]
-            ... = M'.res (M'.d m')  - (f.apply _ _ (M.res m₁)) +
-              ((f.apply _ _ (M.res m₁)) - (f.apply _ _ (M.d m₀))) : by abel
-            ... = M'.res m₁'' + f.apply _ _ ((M.res m₁) - (M.d m₀)) : by
+  have hmnew' : mnew₁' = M'.res m₁'' + f.apply (M.res m₁ - M.d m₀),
+  { calc mnew₁' = M'.d (M'.res m' - f.apply m₀) : by refl
+            ... = M'.res (M'.d m') - (f.apply (M.d m₀)) : by rw [map_sub, d_res _, d_apply]
+            ... = M'.res (M'.d m') - (f.apply (M.res m₁)) +
+              (f.apply (M.res m₁) - f.apply (M.d m₀)) : by abel
+            ... = M'.res m₁'' + f.apply ((M.res m₁) - (M.d m₀)) : by
               rw [← map_sub, ← res_apply, ← map_sub, ← sub_eq_of_eq_add' hm₁.symm] },
   have hnormle : ∥mnew₁'∥ ≤ (∥N.d n∥ + ε₁) * (k ^ 2  + 1) + ε₁,
   { replace hm₀ := le_trans hm₀ (add_le_add_right (mul_le_mul_of_nonneg_left hle
       (@nnreal.zero_le_coe k)) ε₁),
     rw [← mul_assoc ↑k _ _] at hm₀,
-    calc ∥mnew₁'∥ = ∥M'.res m₁'' + f.apply _ _ (M.res m₁ - M.d m₀)∥ : by rw [hmnew']
-              ... ≤ ∥M'.res m₁''∥ + ∥f.apply _ _ (M.res m₁ - M.d m₀)∥ : norm_add_le _ _
-              ... ≤ ∥m₁''∥ + ∥f.apply _ _ (M.res m₁ - M.d m₀)∥ : add_le_add_right
+    calc ∥mnew₁'∥ = ∥M'.res m₁'' + f.apply (M.res m₁ - M.d m₀)∥ : by rw [hmnew']
+              ... ≤ ∥M'.res m₁''∥ + ∥f.apply (M.res m₁ - M.d m₀)∥ : norm_add_le _ _
+              ... ≤ ∥m₁''∥ + ∥f.apply (M.res m₁ - M.d m₀)∥ : add_le_add_right
                 (hM'_adm.res_norm_noninc _ _ _ kccnew m₁'') _
               ... = ∥m₁''∥ + ∥M.res m₁ - M.d m₀∥ : by rw [hf _ _ _]
               ... ≤ ∥N.d n∥ + ε₁ + ∥M.res m₁ - M.d m₀∥ : add_le_add_right (le_of_lt hm₁''.2)  _
@@ -110,12 +110,12 @@ begin
   obtain ⟨mnew₀, hmnew₀⟩ := hM' _ hc _ (lt_trans hi (sub_one_lt m)) mnew' _ hε₁,
   replace hmnew₀ := le_trans hmnew₀ (add_le_add_right (mul_le_mul_of_nonneg_left
     hnormle (@nnreal.zero_le_coe k)) ε₁),
-  let nnew₀ := g.apply _ _ mnew₀,
-  have hmnewlift : g.apply _ _ ((M'.res mnew') - (M'.d mnew₀)) = N.res n - N.d nnew₀,
-  { suffices h : g.apply _ _ mnew' = N.res n,
+  let nnew₀ := g.apply mnew₀,
+  have hmnewlift : g.apply ((M'.res mnew') - (M'.d mnew₀)) = N.res n - N.d nnew₀,
+  { suffices h : g.apply mnew' = N.res n,
     { rw [map_sub, ← res_apply, ← d_apply, h, res_res] },
     rw [map_sub],
-    have hker : (f.apply _ _) m₀ ∈ (g.apply _ _).ker,
+    have hker : f.apply m₀ ∈ g.apply.ker,
     { rw [hg _ _, mem_range _ _],
       use m₀ },
     rw [(mem_ker _ _).1 hker, sub_zero, ← res_apply, hm'] },
