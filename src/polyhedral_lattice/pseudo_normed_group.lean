5b07925eba95401b9ae15483b646fa1a9e9fcb08
Johan Commelin <johan@commelin.net>
2021-02-06 05:39:26 +0000

    move some facts on compactness to own file

--- src/polyhedral_lattice/pseudo_normed_group.lean
+++ src/polyhedral_lattice/pseudo_normed_group.lean
@@ -2,18 +2,32 @@ import polyhedral_lattice.basic
 import normed_group.pseudo_normed_group
 import pseudo_normed_group.system_of_complexes -- ← minimize
 
+noncomputable theory
 open_locale nnreal
 
 namespace polyhedral_lattice
+
+open pseudo_normed_group
 
 variables (Λ : Type*) (r' : ℝ≥0) (M : Type*)
 variables [normed_group Λ] [polyhedral_lattice Λ]
 variables [profinitely_filtered_pseudo_normed_group_with_Tinv r' M]
 
-example : pseudo_normed_group Λ := by apply_instance
+instance : discrete_topology Λ :=
+sorry
+
+lemma filtration_finite (c : ℝ≥0) : (filtration Λ c).finite :=
+sorry
+
+instance filtration_fintype (c : ℝ≥0) : fintype (filtration Λ c) :=
+(filtration_finite Λ c).fintype
 
 instance : profinitely_filtered_pseudo_normed_group Λ :=
-sorry
+{ compact := λ c, by apply_instance, -- compact of finite
+  continuous_add' := λ _ _, continuous_of_discrete_topology,
+  continuous_neg' := λ _, continuous_of_discrete_topology,
+  embedding_cast_le := sorry,
+  .. (show pseudo_normed_group Λ, by apply_instance) }
 
 instance : profinitely_filtered_pseudo_normed_group_with_Tinv r' (Λ →+ M) :=
 sorry
