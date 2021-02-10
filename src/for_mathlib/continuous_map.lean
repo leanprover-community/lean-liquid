782431d2ea645ae0aa8d7a677abc15e07b79cc3c
Johan Commelin <johan@commelin.net>
2021-02-10 12:14:16 +0000

    remove dead code

--- src/for_mathlib/continuous_map.lean
+++
@@ -1,8 +0,0 @@
-import topology.continuous_map
-
-namespace continuous_map
-
-variables {X Y : Type*} [topological_space X] [topological_space Y]
-
-end continuous_map
-#lint- only unused_arguments def_lemma doc_blame
