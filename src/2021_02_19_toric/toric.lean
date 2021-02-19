de3ce89892095a3a688b66a1d53ccce162437e5a
adomani <adomani@gmail.com>
2021-02-19 13:12:30 +0100

    add a comment to a comment

--- src/2021_02_19_toric/toric.lean
+++ src/2021_02_19_toric/toric.lean
@@ -49,10 +49,16 @@ lemma smul_nonneg_iff (r : R) (hr : 0 < r) (p : P) :
 begin
   split,
   { intro h,
+
     rw le_iff_lt_or_eq at h ⊢,
     refine h.imp (smul_pos_iff_of_pos hr).mp _,
     intro hrp,
     /- is this even true? do we need some extra `torsion_free` assumption here? -/
+    /- we may need a "cancellable" assumption on `r`.  This feels close to the counterexample
+    ordered_comm_semiring where multiplication by `2` was not injective.
+    I would think about `ℤ × zmod 2` with some linear order (if it is possible, say where all the
+    `(n, 1)` satisfy `(n, 0) < (n, 1) < (n + 1, 0)`) and then exploiting the fact that
+    `2 • (0,1) = (0,0)`.  Maybe what I described is not a linear_ordered_comm_ring?  -/
     sorry },
   { intro h,
     simpa only [smul_zero] using smul_le_smul_of_nonneg h hr.le }
