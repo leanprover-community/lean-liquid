1ec9b23b55ce88e911625241bb4990640b3d8687
adomani <adomani@gmail.com>
2021-02-19 15:56:14 +0100

    before changing to subrings

--- src/2021_02_19_toric/toric.lean
+++ src/2021_02_19_toric/toric.lean
@@ -1,7 +1,7 @@
 import data.polynomial.degree.lemmas
 import algebra.module.ordered
 
-variables (R M N P : Type*)
+variables (R₀ R M N P : Type*)
 
 section pairing
 
@@ -10,27 +10,49 @@ variables [comm_semiring R] [add_comm_monoid M] [add_comm_monoid N] [semimodule 
 
 @[derive has_coe_to_fun] def pairing := M →ₗ[R] N →ₗ[R] P
 
+variables {R M N P}
+
 def pairing.flip : pairing R M N P → pairing R N M P := linear_map.flip
 
 end pairing
 
-namespace add_submonoid
+namespace submodule
 
 variables {M} [ordered_comm_ring R] [add_comm_monoid M] [semimodule R M]
 
 def saturated (s : add_submonoid M) : Prop :=
 ∀ (r : R) (hr : 0 < r) (m : M), r • m ∈ s → m ∈ s
 
-end add_submonoid
+def saturation (s : submodule R M) : submodule R M :=
+{ carrier := { m : M | m = 0 ∨ ∃ (r : R), 0 < r ∧ r • m ∈ s },
+  zero_mem' := or.inl rfl,
+  add_mem' := begin
+    rintros a b (rfl | ra) (rfl | rb),
+    { rw [add_zero],
+      exact or.inl rfl },
+    { rw zero_add,
+      refine or.inr rb },
+    { rw add_zero,
+      refine or.inr ra },
+    { rcases ra with ⟨ra, ra0, ras⟩,
+      rcases rb with ⟨rb, rb0, rbs⟩,
+      refine or.inr _,
+      refine ⟨(ra * rb), mul_pos ra0 rb0, _⟩,
+      rw [smul_add, mul_smul _ _ b, mul_comm ra, mul_smul],
+      exact s.add_mem (s.smul_mem _ ras) (s.smul_mem _ rbs) }
+  end,
+   }
+
+end submodule
 
 namespace pairing
 
-variables [ordered_comm_ring R] [add_comm_monoid M] [add_comm_monoid N] [semimodule R M]
+variables [linear_ordered_comm_ring R] [add_comm_monoid M] [add_comm_monoid N] [semimodule R M]
   [semimodule R N] [ordered_add_comm_monoid P] [semimodule R P]
 
 variables {R M N P} (f : pairing R M N P)
 
-def dual_set (s : set M) : add_submonoid N :=
+def dual_set (s : set M) : submodule R N :=
 { carrier := { n : N | ∀ m ∈ s, (0 : P) ≤ f m n },
   zero_mem' := λ m hm, by simp only [linear_map.map_zero],
   add_mem' := λ n1 n2 hn1 hn2 m hm, begin
@@ -53,6 +75,8 @@ begin
     rw le_iff_lt_or_eq at h ⊢,
     refine h.imp (smul_pos_iff_of_pos hr).mp _,
     intro hrp,
+    refine eq_of_smul_eq_smul_of_pos_of_le _ hr _,
+    rwa smul_zero,
     /- is this even true? do we need some extra `torsion_free` assumption here? -/
     /- we may need a "cancellable" assumption on `r`.  This feels close to the counterexample
     ordered_comm_semiring where multiplication by `2` was not injective.
@@ -70,6 +94,62 @@ begin
   specialize hn m hm,
   rwa [linear_map.map_smul, smul_nonneg_iff r hr] at hn,
   apply_instance
+end
+
+lemma dual_subset {s t : set M} (st : s ⊆ t) : f.dual_set t ≤ f.dual_set s :=
+λ n hn m hm, hn m (st hm)
+
+lemma subset_dual_dual {s : set M} : s ⊆ f.flip.dual_set (f.dual_set s) :=
+λ m hm _ hm', hm' m hm
+
+lemma subset_dual_set_of_subset_dual_set {S : set M} {T : set N}
+  (ST : S ⊆ f.flip.dual_set T) :
+  T ⊆ f.dual_set S :=
+λ n hn m hm, ST hm _ hn
+
+lemma le_dual_set_of_le_dual_set {S : submodule R M} {T : submodule R N}
+  (ST : S ≤ f.flip.dual_set T) :
+  T ≤ f.dual_set S :=
+subset_dual_set_of_subset_dual_set _ ST
+
+lemma dual_set_closure_eq {s : set M} :
+  f.dual_set (submodule.span R s) = f.dual_set s :=
+begin
+  refine le_antisymm (dual_subset _ submodule.subset_span) _,
+  intros n hn m hm,
+  apply submodule.span_induction hm hn,
+  { simp only [linear_map.map_zero, linear_map.zero_apply] },
+  { intros x y hx hy,
+    simpa only [linear_map.add_apply, linear_map.map_add] using add_nonneg hx hy },
+  sorry
+end
+
+/-
+define saturation
+dual of saturation eq dual
+-/
+
+/- flip the inequalities assuming saturatedness -/
+lemma le_dual_set_of_le_dual_set_satu {S : submodule R M} {T : submodule R N}
+  (ST : S ≤ f.flip.dual_set T) :
+  T ≤ f.dual_set S :=
+subset_dual_set_of_subset_dual_set _ ST
+
+lemma subset_dual_set_iff {S : set M} {T : set N} :
+  S ⊆ f.flip.dual_set T ↔ T ⊆ f.dual_set S :=
+⟨subset_dual_set_of_subset_dual_set f, subset_dual_set_of_subset_dual_set f.flip⟩
+
+lemma le_dual_set_iff {S : submodule R M} {T : submodule R N} :
+  S ≤ f.flip.dual_set T ↔ T ≤ f.dual_set S :=
+subset_dual_set_iff _
+
+lemma dual_dual_of_saturated {S : submodule R M} (Ss : S.saturated R) :
+  f.flip.dual_set (f.dual_set S) = S :=
+begin
+  refine le_antisymm _ (subset_dual_dual f),
+  intros m hm,
+
+  sorry,
 end
 
 end pairing
