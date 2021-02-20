8848ae6e12ea579549d5a4259360c6ed08e480b1
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-02-20 22:50:00 +0000

    one sorry left

--- src/2021_02_19_toric/toric.lean
+++ src/2021_02_19_toric/toric.lean
@@ -50,112 +50,135 @@ def saturation (s : submodule R₀ M) : submodule R₀ M :=
     exact s.smul_mem _ hrm,
   end }
 
+lemma le_saturation (s : submodule R₀ M) : s ≤ saturation R₀ s :=
+λ m hm, ⟨1, is_regular_one, by rwa one_smul⟩
+
 end submodule
 
 namespace pairing
 
---variables [comm_semiring R₀] [comm_semiring R] [add_comm_monoid M] [add_comm_monoid N]
---  [semimodule R M] [semimodule R N] [ordered_add_comm_monoid P] [semimodule R P]
-
 variables {R₀ R M N P} (f : pairing R M N P)
 
 def dual_set (s : set M) : submodule R₀ N :=
-{ carrier := { n : N | ∀ m ∈ s, (0 : P) ≤ f m n },
-  zero_mem' := λ m hm, by simp only [linear_map.map_zero],
+{ carrier := { n : N | ∀ m ∈ s, f m n ∈ P₀ },
+  zero_mem' := λ m hm, by simp only [linear_map.map_zero, P₀.zero_mem],
   add_mem' := λ n1 n2 hn1 hn2 m hm, begin
     rw linear_map.map_add,
-    exact add_nonneg (hn1 m hm) (hn2 m hm)
-  end }
+    exact P₀.add_mem (hn1 m hm) (hn2 m hm),
+  end,
+  smul_mem' := begin
+    rintro r n h m hms,
+    simp [h m hms, P₀.smul_mem],
+  end
+}
 
 lemma mem_dual_set (s : set M) (n : N) :
-  n ∈ f.dual_set s ↔ ∀ m ∈ s, 0 ≤ f m n := iff.rfl
+  n ∈ f.dual_set P₀ s ↔ ∀ m ∈ s, f m n ∈ P₀ := iff.rfl
 
-variables [ordered_semimodule R P]
+section saturated
 
--- move this
-lemma smul_nonneg_iff (r : R) (hr : 0 < r) (p : P) :
-  0 ≤ r • p ↔ 0 ≤ p :=
-begin
-  split,
-  { intro h,
+variables {P₀} (hP₀ : P₀.saturated R₀)
+include hP₀
 
-    rw le_iff_lt_or_eq at h ⊢,
-    refine h.imp (smul_pos_iff_of_pos hr).mp _,
-    intro hrp,
-    refine eq_of_smul_eq_smul_of_pos_of_le _ hr _,
-    rwa smul_zero,
-    /- is this even true? do we need some extra `torsion_free` assumption here? -/
-    /- we may need a "cancellable" assumption on `r`.  This feels close to the counterexample
-    ordered_comm_semiring where multiplication by `2` was not injective.
-    I would think about `ℤ × zmod 2` with some linear order (if it is possible, say where all the
-    `(n, 1)` satisfy `(n, 0) < (n, 1) < (n + 1, 0)`) and then exploiting the fact that
-    `2 • (0,1) = (0,0)`.  Maybe what I described is not a linear_ordered_comm_ring?  -/
-    sorry },
-  { intro h,
-    simpa only [smul_zero] using smul_le_smul_of_nonneg h hr.le }
-end
+lemma smul_regular_iff {r : R₀} (hr : is_regular r) (p : P) :
+  r • p ∈ P₀ ↔ p ∈ P₀ :=
+⟨hP₀ _ hr _, P₀.smul_mem _⟩
 
-lemma dual_set_saturated (s : set M) : (f.dual_set s).saturated R :=
-begin
-  intros r hr n hn m hm,
-  specialize hn m hm,
-  rwa [linear_map.map_smul, smul_nonneg_iff r hr] at hn,
-  apply_instance
-end
+lemma dual_set_saturated (s : set M) (hP₀ : P₀.saturated R₀) :
+  (f.dual_set P₀ s).saturated R₀ :=
+λ r hr n hn m hm,
+by simpa [smul_regular_iff hP₀ hr] using hn m hm
 
-lemma dual_subset {s t : set M} (st : s ⊆ t) : f.dual_set t ≤ f.dual_set s :=
+end saturated
+
+-- this instance is missing -- see forthcoming PR by KMB 20/02/21
+instance : is_scalar_tower R₀ R (N →ₗ[R] P) :=
+{ smul_assoc := λ _ _ _, linear_map.ext $ by simp }
+
+variable {P₀}
+
+lemma dual_subset {s t : set M} (st : s ⊆ t) : f.dual_set P₀ t ≤ f.dual_set P₀ s :=
 λ n hn m hm, hn m (st hm)
 
-lemma subset_dual_dual {s : set M} : s ⊆ f.flip.dual_set (f.dual_set s) :=
+lemma mem_span_dual_set (s : set M) :
+  f.dual_set P₀ (submodule.span R₀ s) = f.dual_set P₀ s :=
+begin
+  apply le_antisymm,
+  { apply dual_subset,
+    exact submodule.subset_span,
+  },
+  { intros n hn m hm,
+    rw mem_dual_set at hn,
+    apply submodule.span_induction hm hn,
+    { simp only [linear_map.map_zero, submodule.zero_mem, linear_map.zero_apply] },
+    { intros x y hx hy, simp [P₀.add_mem hx hy] },
+    { intros r m hm,
+      simp [P₀.smul_mem _ hm] } }
+end
+
+lemma subset_dual_dual {s : set M} : s ⊆ f.flip.dual_set P₀ (f.dual_set P₀ s) :=
 λ m hm _ hm', hm' m hm
 
 lemma subset_dual_set_of_subset_dual_set {S : set M} {T : set N}
-  (ST : S ⊆ f.flip.dual_set T) :
-  T ⊆ f.dual_set S :=
+  (ST : S ⊆ f.flip.dual_set P₀ T) :
+  T ⊆ f.dual_set P₀ S :=
 λ n hn m hm, ST hm _ hn
 
-lemma le_dual_set_of_le_dual_set {S : submodule R M} {T : submodule R N}
-  (ST : S ≤ f.flip.dual_set T) :
-  T ≤ f.dual_set S :=
+lemma le_dual_set_of_le_dual_set {S : submodule R₀ M} {T : submodule R₀ N}
+  (ST : S ≤ f.flip.dual_set P₀ T) :
+  T ≤ f.dual_set P₀ S :=
 subset_dual_set_of_subset_dual_set _ ST
 
 lemma dual_set_closure_eq {s : set M} :
-  f.dual_set (submodule.span R s) = f.dual_set s :=
+  f.dual_set P₀ (submodule.span R₀ s) = f.dual_set P₀ s :=
 begin
   refine le_antisymm (dual_subset _ submodule.subset_span) _,
   intros n hn m hm,
   apply submodule.span_induction hm hn,
-  { simp only [linear_map.map_zero, linear_map.zero_apply] },
+  { simp only [linear_map.map_zero, linear_map.zero_apply, P₀.zero_mem] },
   { intros x y hx hy,
-    simpa only [linear_map.add_apply, linear_map.map_add] using add_nonneg hx hy },
-  sorry
+    simp only [linear_map.add_apply, linear_map.map_add, P₀.add_mem hx hy] },
+  { intros r m hmn,
+    simp [P₀.smul_mem r hmn] },
 end
 
-/-
-define saturation
-dual of saturation eq dual
--/
+lemma dual_eq_dual_saturation {S : set M} (hP₀ : P₀.saturated R₀) :
+  f.dual_set P₀ S = f.dual_set P₀ ((submodule.span R₀ S).saturation R₀) :=
+begin
+  apply le_antisymm,
+  { rintro n hn m ⟨r, hr_reg, hrm⟩,
+    rw ← smul_regular_iff hP₀ hr_reg,
+    rw [← mem_span_dual_set, mem_dual_set] at hn,
+    specialize hn (r • m) hrm,
+    simpa using hn },
+  { apply dual_subset,
+    refine set.subset.trans (submodule.subset_span : S ⊆ submodule.span R₀ S) _,
+    apply submodule.le_saturation R₀ (submodule.span R₀ S) },
+end
 
 /- flip the inequalities assuming saturatedness -/
-lemma le_dual_set_of_le_dual_set_satu {S : submodule R M} {T : submodule R N}
-  (ST : S ≤ f.flip.dual_set T) :
-  T ≤ f.dual_set S :=
+lemma le_dual_set_of_le_dual_set_satu {S : submodule R₀ M} {T : submodule R₀ N}
+  (ST : S ≤ f.flip.dual_set P₀ T) :
+  T ≤ f.dual_set P₀ S :=
 subset_dual_set_of_subset_dual_set _ ST
 
 lemma subset_dual_set_iff {S : set M} {T : set N} :
-  S ⊆ f.flip.dual_set T ↔ T ⊆ f.dual_set S :=
+  S ⊆ f.flip.dual_set P₀ T ↔ T ⊆ f.dual_set P₀ S :=
 ⟨subset_dual_set_of_subset_dual_set f, subset_dual_set_of_subset_dual_set f.flip⟩
 
-lemma le_dual_set_iff {S : submodule R M} {T : submodule R N} :
-  S ≤ f.flip.dual_set T ↔ T ≤ f.dual_set S :=
+lemma le_dual_set_iff {S : submodule R₀ M} {T : submodule R₀ N} :
+  S ≤ f.flip.dual_set P₀ T ↔ T ≤ f.dual_set P₀ S :=
 subset_dual_set_iff _
 
-lemma dual_dual_of_saturated {S : submodule R M} (Ss : S.saturated R) :
-  f.flip.dual_set (f.dual_set S) = S :=
+lemma dual_dual_of_saturated {S : submodule R₀ M} (Ss : S.saturated R₀) :
+  f.flip.dual_set P₀ (f.dual_set P₀ S) = S :=
 begin
   refine le_antisymm _ (subset_dual_dual f),
   intros m hm,
-
+  rw mem_dual_set at hm,
+  change ∀ (n : N), n ∈ (dual_set P₀ f ↑S) → f m n ∈ P₀ at hm,
+  simp_rw mem_dual_set at hm,
+  -- is this true? I (KMB) don't know and the guru (Damiano) has left!
   sorry,
 end
 
