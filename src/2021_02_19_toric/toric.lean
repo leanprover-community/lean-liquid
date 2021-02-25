00b8425e43351b8ed3fdb67a6342a81210a7c400
adomani <adomani@gmail.com>
2021-02-25 12:52:53 +0100

    save to get cache

--- src/2021_02_19_toric/toric.lean
+++ src/2021_02_19_toric/toric.lean
@@ -3,6 +3,7 @@ import algebra.module.ordered
 import algebra.regular
 import ring_theory.noetherian
 import linear_algebra.finite_dimensional
+--import analysis.normed_space.inner_product
 
 /-- In the intended application, these are the players:
 * `R₀ = ℕ`;
@@ -10,9 +11,17 @@ import linear_algebra.finite_dimensional
 * `M`and `N` are free finitely generated `ℤ`-modules that are dual to each other;
 * `P = ℤ` is the target of the natural pairing `M ⊗ N → ℤ`.
 -/
-variables (R₀ R M N P : Type*)
+
+-- the example is here since otherwise Lean complains that `section does not accept doc string`
+example : true := trivial
+
+-- Here we make the general statements that require few assumptions on the various types.
+section abstract
+
+variables (R₀ R M : Type*)
 
 namespace submodule
+
 
 section comm_semiring
 
@@ -47,8 +56,7 @@ def saturation (s : submodule R₀ M) : submodule R₀ M :=
 lemma le_saturation (s : submodule R₀ M) : s ≤ saturation s :=
 λ m hm, ⟨1, is_regular_one, by rwa one_smul⟩
 
-/- I (DT) extracted this lemma from the proof of `dual_eq_dual_saturation`, since it seems a
-lemma that we may use elsewhere as well. -/
+/-- The set `S` is contained in the saturation of the submodule spanned by `S` itself. -/
 lemma set_subset_saturation  {S : set M} :
   S ⊆ (submodule.saturation (submodule.span R₀ S)) :=
 set.subset.trans (submodule.subset_span : S ⊆ submodule.span R₀ S)
@@ -96,16 +104,31 @@ def pointed (s : submodule R₀ M) : Prop := ∃ φ : M →ₗ[R] R, ∀ x : M, 
 
 variables [algebra R₀ R] [is_scalar_tower R₀ R M]
 
-/-- This lemma shows that any `R₀`-submodule of `R` is pointed, since the identity function is
-a "separating hyperplane".  This should not happen if the module is not cyclic for `R`. -/
+/-- Any `R₀`-submodule of `R` is pointed, since the identity function is a "separating hyperplane".
+This should not happen if the module is not cyclic for `R`. -/
 lemma pointed_of_sub_R {s : submodule R₀ R} : pointed R s :=
 ⟨1, λ _ _ h, h⟩
+
+/-- The zero submodule of any `R`-module `M` is pointed, since the zero function is a
+"separating hyperplane". -/
+lemma pointed_of_bot : pointed R (⊥ : submodule R₀ M) :=
+⟨0, λ x xb h, xb⟩
+
+lemma fd {ι : Type*} (v : ι → R) (ind : linear_independent R v) :
+  pointed R (submodule.span R₀ (v '' set.univ)) :=
+pointed_of_sub_R R
 
 /--  Hopefully, this lemma will be easy to prove. -/
 lemma sup_extremal_rays {s : submodule R₀ M} (sp : s.pointed R) :
   (⨆ r ∈ s.extremal_rays, r) = s :=
 begin
   refine le_antisymm (bsupr_le $ λ i hi, hi.1) _,
+  intros m ms t ht,
+  rcases ht with ⟨y, rfl⟩,
+  simp only [forall_apply_eq_imp_iff', supr_le_iff, set.mem_range, mem_coe, set.mem_Inter,
+    set.mem_set_of_eq, exists_imp_distrib],
+  intros a,
+  rcases sp with ⟨el, lo⟩,
   sorry
 end
 
@@ -135,35 +158,60 @@ end integral_domain
 end submodule
 
 
+section pairing
+
 variables [comm_semiring R₀] [comm_semiring R] [algebra R₀ R]
   [add_comm_monoid M] [semimodule R₀ M] [semimodule R M] [is_scalar_tower R₀ R M]
+
+variables (N P : Type*)
   [add_comm_monoid N] [semimodule R₀ N] [semimodule R N] [is_scalar_tower R₀ R N]
   [add_comm_monoid P] [semimodule R₀ P] [semimodule R P] [is_scalar_tower R₀ R P]
   (P₀ : submodule R₀ P)
 
-section pairing
-
-
 /-- An R-pairing on the R-modules M, N, P is an R-linear map M -> Hom_R(N,P). -/
 @[derive has_coe_to_fun] def pairing := M →ₗ[R] N →ₗ[R] P
 
-instance pairing.inhabited : inhabited (pairing R M N P) :=
+namespace pairing
+
+instance inhabited : inhabited (pairing R M N P) :=
 ⟨{to_fun := 0, map_add' := by simp, map_smul' := by simp }⟩
 
-variables {R M N P}
+variables {R₀ R M N P}
 
 /--  Given a pairing between the `R`-modules `M` and `N`, we obtain a pairing between `N` and `M`
 by exchanging the factors. -/
-def pairing.flip : pairing R M N P → pairing R N M P := linear_map.flip
+def flip : pairing R M N P → pairing R N M P := linear_map.flip
 
-end pairing
+variables (f : pairing R M N P)
 
-variables {M}
+/-- For a given pairing `<_, _> : M × N → P` and an element `m ∈ M`, `mul_left` is the linear map
+`n ↦ <m, n>`.
+-- Left multiplication may not be needed.
+def mul_left (m : M) : N →ₗ[R] P :=
+{ to_fun := λ n, f m n,
+  map_add' := λ x y, by simp only [linear_map.add_apply, linear_map.map_add],
+  map_smul' := λ x y, by simp only [linear_map.smul_apply, linear_map.map_smul] }
 
+/-- For a given pairing `<_, _> : M × N → P` and an element `n ∈ N`, `mul_right` is the linear map
+`m ↦ <m, n>`. -/
+def mul_right (n : N) : M →ₗ[R] P :=
+{ to_fun := λ m, f m n,
+  map_add' := λ x y, by simp only [linear_map.add_apply, linear_map.map_add],
+  map_smul' := λ x y, by simp only [linear_map.smul_apply, linear_map.map_smul] }
+-/
 
-namespace pairing
+example {n : N} : f.flip n = f.flip n := rfl
 
-variables {R₀ R M N P} (f : pairing R M N P)
+/--  A pairing `M × N → P` is `left_nondegenerate` if `0 ∈ N` is the only element of `N` pairing
+to `0` with all elements of `M`. -/
+def left_nondegenerate : Prop := ∀ n : N, (∀ m : M, f m n = 0) → n = 0
+
+/--  A pairing `M × N → P` is `right_nondegenerate` if `0 ∈ M` is the only element of `M` pairing
+to `0` with all elements of `N`. -/
+def right_nondegenerate : Prop := ∀ m : M, (∀ n : N, f m n = 0) → m = 0
+
+/--  A pairing `M × N → P` is `perfect` if it is left and right nondegenerate. -/
+def perfect : Prop := left_nondegenerate f ∧ right_nondegenerate f
 
 /--  For a subset `s ⊆ M`, the `dual_set s` is the submodule consisting of all elements of `N`
 that have "positive pairing with all the elements of `s`.  "Positive" means that it lies in the
@@ -321,4 +369,209 @@ begin
   sorry,
 end
 
+/-
+def to_linear_dual (f : pairing R M N R) : N →ₗ[R] (M →ₗ[R] R) :=
+{ to_fun := λ n,
+  { to_fun := λ m, f m n,
+    map_add' := λ x y, by simp only [linear_map.add_apply, linear_map.map_add],
+    map_smul' := λ x y, by simp only [linear_map.smul_apply, linear_map.map_smul] },
+  map_add' := λ x y, by simpa only [linear_map.map_add],
+  map_smul' := λ r n, by simpa only [algebra.id.smul_eq_mul, linear_map.map_smul] }
+
+lemma to_ld (f : pairing R M N R) (n : N) : to_linear_dual f n = mul_right f n := rfl
+
+-- this lemma requires some extra hypotheses: at the very least, some finite-generation
+-- condition: the "standard" scalar product on `ℝ ^ (⊕ ℕ)` has power-series as its dual
+-- but is non-degenerate.
+/-- A pairing `f` between two `R`-modules `M` and `N` with values in `R` is perfect if every
+linear function `l : M →ₗ[R] R` is represented as -/
+lemma left_nondegenerate_exists {f : pairing R M N R} (r : right_nondegenerate f) :
+  ∀ l : M →ₗ[R] R, ∃ n : N, ∀ m : M, l m = f m n :=
+begin
+  intros l,
+  sorry,
+end
+-/
+
 end pairing
+
+end pairing
+
+end abstract
+
+-- ending the section to clear out all the assumptions
+
+/- In the intended application, these are the players:
+* `R₀ = ℕ`;
+* `R = ℤ`;
+* `M`and `N` are free finitely generated `ℤ`-modules that are dual to each other;
+* `P = ℤ` is the target of the natural pairing `M ⊗ N → ℤ`.
+-/
+
+section concrete
+
+namespace pairing
+
+open pairing submodule
+
+variables {M : Type*} [add_comm_group M] --[semimodule ℕ M]
+-- [semimodule ℤ M]
+--variables {M N : Type*} [add_comm_monoid M] --[semimodule ℕ M] [semimodule ℤ M]
+  --[algebra ℕ ℤ] [is_scalar_tower ℕ ℤ M]
+
+--variables {P : Type*}
+--  [add_comm_monoid N] --[semimodule ℕ N] [semimodule ℤ N] --[is_scalar_tower ℕ ℤ N]
+--  [add_comm_monoid P] --[semimodule ℕ P] [semimodule ℤ P] --[is_scalar_tower ℕ ℤ P]
+--  (P₀ : submodule ℕ P)
+
+lemma of_non_deg {ι : Type*} {f : pairing ℤ M M ℤ} {v : ι → M}
+  (nd : perfect f) (sp : submodule.span ℤ (v '' set.univ)) :
+  pointed ℤ (submodule.span ℕ (v '' set.univ)) :=
+begin
+  tidy?,
+  sorry
+end
+
+
+def standard_pairing_Z : pairing ℤ ℤ ℤ ℤ :=
+{ to_fun := λ z,
+  { to_fun := λ n, z * n,
+    map_add' := mul_add z,
+    map_smul' := λ m n, algebra.mul_smul_comm m z n },
+  map_add' := λ x y, by simpa [add_mul],
+  map_smul' := λ x y, by simpa only [algebra.smul_mul_assoc] }
+
+lemma nond_Z : right_nondegenerate standard_pairing_Z :=
+λ m hm, eq.trans (mul_one m).symm (hm 1)
+
+
+def standard_pairing_Z_sq : pairing ℤ (ℤ × ℤ) (ℤ × ℤ) ℤ :=
+{ to_fun := λ z,
+  { to_fun := λ n, z.1 * n.1 + z.2 * n.2,
+    map_add' := λ x y, by { rw [prod.snd_add, prod.fst_add], ring },
+    map_smul' := λ x y,
+      by simp only [smul_add, algebra.mul_smul_comm, prod.smul_snd, prod.smul_fst] },
+  map_add' := λ x y, begin
+    congr,
+    ext,
+    dsimp,
+    rw [prod.snd_add, prod.fst_add, add_mul],
+    ring,
+  end,
+  map_smul' := λ x y, begin
+    congr,
+    simp only [smul_add, prod.smul_snd, linear_map.coe_mk, prod.smul_fst, algebra.smul_mul_assoc],
+  end }
+
+lemma nond_Z_sq : right_nondegenerate standard_pairing_Z_sq :=
+begin
+  refine λ  m hm, prod.ext _ _,
+  { obtain (F : m.fst * (1 : ℤ) + m.snd * (0 : ℤ) = 0) := hm (1, 0),
+    simpa using F },
+  { obtain (F : m.fst * (0 : ℤ) + m.snd * (1 : ℤ) = 0) := hm (0, 1),
+    simpa using F }
+end
+
+lemma fd (v : fin 2 → ℤ × ℤ) (ind : linear_independent ℤ v) :
+  pointed ℤ (submodule.span ℕ (v '' set.univ)) :=
+begin
+  refine ⟨_, _⟩,
+  convert mul_right standard_pairing_Z_sq (v 0 + v 1),
+--  convert @mul_right ℤ (ℤ × ℤ) _ _ _ (ℤ × ℤ) ℤ _ _ _ _ standard_pairing_Z_sq ((1, 1) : ℤ × ℤ),
+  intros m hm m0,
+  induction m with m1 m2,
+  congr,
+
+--  tidy?,
+
+  refine (mul_right standard_pairing_Z_sq ((1, 1) : ℤ × ℤ) : ℤ × ℤ →ₗ[ℤ] ℤ),
+--  refine ((λ m : ℤ × ℤ, standard_pairing_Z_sq m (1,1)) : ℤ × ℤ →ₗ[ℤ] ℤ),
+  refine
+  { to_fun := λ m, standard_pairing_Z_sq m (1,1),
+    map_add' :=
+      by simp only [forall_const, eq_self_iff_true, linear_map.add_apply, linear_map.map_add],
+    map_smul' := λ x m, begin
+      rw [standard_pairing_Z_sq, algebra.id.smul_eq_mul, linear_map.map_smul, linear_map.coe_mk, linear_map.coe_mk],
+      simp only [has_scalar.smul, gsmul_int_int, linear_map.coe_mk],
+  end },
+  simp at *, fsplit, work_on_goal 0 { fsplit, work_on_goal 0 { intros ᾰ, cases ᾰ }, work_on_goal 1 { intros x y, cases y, cases x, dsimp at * }, work_on_goal 2 { intros m x, cases x, dsimp at * } }, work_on_goal 3 { intros x ᾰ ᾰ_1, cases x, dsimp at *, simp at *, simp at *, fsplit, work_on_goal 0 { assumption } },
+  { refl },
+  { simp [(•)] },
+
+  convert pointed_of_sub_R M,
+end
+
+#exit
+
+lemma fd {ι : Type*} (s : finset ι) (v : ι → ℤ × ℤ) (ind : linear_independent ℤ v) :
+  pointed ℤ (submodule.span ℕ (v '' set.univ)) :=
+begin
+  refine ⟨_, _⟩,
+  convert mul_right standard_pairing_Z_sq (∑ a in s, v a),
+--  convert @mul_right ℤ (ℤ × ℤ) _ _ _ (ℤ × ℤ) ℤ _ _ _ _ standard_pairing_Z_sq ((1, 1) : ℤ × ℤ),
+  intros m hm m0,
+  induction m with m1 m2,
+  congr,
+
+--  tidy?,
+
+  refine (mul_right standard_pairing_Z_sq ((1, 1) : ℤ × ℤ) : ℤ × ℤ →ₗ[ℤ] ℤ),
+--  refine ((λ m : ℤ × ℤ, standard_pairing_Z_sq m (1,1)) : ℤ × ℤ →ₗ[ℤ] ℤ),
+  refine
+  { to_fun := λ m, standard_pairing_Z_sq m (1,1),
+    map_add' :=
+      by simp only [forall_const, eq_self_iff_true, linear_map.add_apply, linear_map.map_add],
+    map_smul' := λ x m, begin
+      rw [standard_pairing_Z_sq, algebra.id.smul_eq_mul, linear_map.map_smul, linear_map.coe_mk, linear_map.coe_mk],
+      simp only [has_scalar.smul, gsmul_int_int, linear_map.coe_mk],
+  end },
+  simp at *, fsplit, work_on_goal 0 { fsplit, work_on_goal 0 { intros ᾰ, cases ᾰ }, work_on_goal 1 { intros x y, cases y, cases x, dsimp at * }, work_on_goal 2 { intros m x, cases x, dsimp at * } }, work_on_goal 3 { intros x ᾰ ᾰ_1, cases x, dsimp at *, simp at *, simp at *, fsplit, work_on_goal 0 { assumption } },
+  { refl },
+  { simp [(•)] },
+
+  convert pointed_of_sub_R M,
+end
+
+lemma fd {ι : Type*} (v : ι → ℤ × ℤ) (ind : linear_independent ℤ v) :
+  pointed ℤ (submodule.span ℕ (v '' set.univ)) :=
+begin
+  refine ⟨_, _⟩,
+  convert mul_right standard_pairing_Z_sq (1, 1),
+--  convert @mul_right ℤ (ℤ × ℤ) _ _ _ (ℤ × ℤ) ℤ _ _ _ _ standard_pairing_Z_sq ((1, 1) : ℤ × ℤ),
+  intros m hm m0,
+
+  refine (mul_right standard_pairing_Z_sq ((1, 1) : ℤ × ℤ) : ℤ × ℤ →ₗ[ℤ] ℤ),
+--  refine ((λ m : ℤ × ℤ, standard_pairing_Z_sq m (1,1)) : ℤ × ℤ →ₗ[ℤ] ℤ),
+  refine
+  { to_fun := λ m, standard_pairing_Z_sq m (1,1),
+    map_add' :=
+      by simp only [forall_const, eq_self_iff_true, linear_map.add_apply, linear_map.map_add],
+    map_smul' := λ x m, begin
+      rw [standard_pairing_Z_sq, algebra.id.smul_eq_mul, linear_map.map_smul, linear_map.coe_mk, linear_map.coe_mk],
+      simp only [has_scalar.smul, gsmul_int_int, linear_map.coe_mk],
+  end },
+  simp at *, fsplit, work_on_goal 0 { fsplit, work_on_goal 0 { intros ᾰ, cases ᾰ }, work_on_goal 1 { intros x y, cases y, cases x, dsimp at * }, work_on_goal 2 { intros m x, cases x, dsimp at * } }, work_on_goal 3 { intros x ᾰ ᾰ_1, cases x, dsimp at *, simp at *, simp at *, fsplit, work_on_goal 0 { assumption } },
+  { refl },
+  { simp [(•)] },
+
+  convert pointed_of_sub_R M,
+end
+
+
+
+lemma pointed_of_sub_Z {ι : Type*} (v : ι → ℤ) (ind : linear_independent ℤ v) :
+  pointed ℤ (submodule.span ℕ (v '' set.univ)) :=
+by convert pointed_of_sub_R ℤ
+
+lemma fd {ι : Type*} (v : ι → M) (ind : linear_independent ℤ v) :
+  pointed ℤ (submodule.span ℕ (v '' set.univ)) :=
+begin
+  tidy?,
+  convert pointed_of_sub_R M,
+end
+
+
+
+end pairing
+
+end concrete
