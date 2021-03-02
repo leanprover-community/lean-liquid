1267135639ed3accc2e293f544e77a8a0cd7c3fb
Johan Commelin <johan@commelin.net>
2021-03-02 16:06:01 +0000

    change m to be an integer

--- src/normed_spectral.lean
+++ src/normed_spectral.lean
@@ -7,12 +7,12 @@ universe variables u
 
 /-- The assumptions on `M` in Proposition 9.6 bundled into a structure. Note that in `cond3b`
   our `q` is one smaller than the `q` in the notes (so that we don't have to deal with `q - 1`). -/
-structure normed_spectral_conditions (m : ℕ) (k K : ℝ≥0) [fact (1 ≤ k)]
+structure normed_spectral_conditions (m : ℤ) (k K : ℝ≥0) [fact (1 ≤ k)]
   (ε : ℝ) (hε : 0 < ε) (k₀ : ℝ≥0) [fact (1 ≤ k₀)]
   (M : system_of_double_complexes)
   (k' : ℝ≥0) [fact (k₀ ≤ k')] [fact (1 ≤ k')] (c₀ H : ℝ≥0) [fact (0 < H)] :=
-(col_exact : ∀ j ≤ m, (M.col j).is_bounded_exact k K (m+1) c₀)
-(row_exact : ∀ i ≤ m + 1, (M.row i).is_bounded_exact k K m c₀)
+(col_exact : ∀ j ≤ m, (M.col j).is_bounded_exact k K m c₀)
+(row_exact : ∀ i ≤ m + 1, (M.row i).is_bounded_exact k K (m-1) c₀)
 (h : Π {q : ℤ} [fact (q ≤ m)] {c} [fact (c₀ ≤ c)], M.X (k' * c) 0 (q+1) ⟶ M.X c 1 q)
 (norm_h_le : ∀ (q : ℤ) [fact (q ≤ m)] (c) [fact (c₀ ≤ c)] (x : M.X (k' * c) 0 (q+1)), ​∥h x∥ ≤ H * ∥x∥)
 (cond3b : ∀ (q : ℤ) [fact (q+1 ≤ m)] (c) [fact (c₀ ≤ c)]
@@ -22,7 +22,7 @@ structure normed_spectral_conditions (m : ℕ) (k K : ℝ≥0) [fact (1 ≤ k)]
 
 namespace normed_spectral_conditions
 
-variables (m : ℕ) (k K : ℝ≥0) [fact (1 ≤ k)]
+variables (m : ℤ) (k K : ℝ≥0) [fact (1 ≤ k)]
 variables (ε : ℝ) (hε : 0 < ε) (k₀ : ℝ≥0) [fact (1 ≤ k₀)]
 variables (M : system_of_double_complexes.{u})
 variables (k' : ℝ≥0) [fact (k₀ ≤ k')] [fact (1 ≤ k')] (c₀ H : ℝ≥0) [fact (0 < H)]
@@ -56,7 +56,7 @@ end normed_spectral_conditions
 Constants (max (k' * k') (2 * k₀ * H)) and K in the statement are not the right ones.
 We need to investigate the consequences of the k Zeeman effect here.
 -/
-theorem analytic_9_6 (m : ℕ) (k K : ℝ≥0) [fact (1 ≤ k)] :
+theorem analytic_9_6 (m : ℤ) (k K : ℝ≥0) [fact (1 ≤ k)] :
   ∃ (ε : ℝ) (hε : ε > 0) (k₀ : ℝ≥0) [fact (1 ≤ k₀)],
   ∀ (M : system_of_double_complexes) (k' : ℝ≥0) [fact (k₀ ≤ k')] [fact (1 ≤ k')] -- follows
     (c₀ H : ℝ≥0) [fact (0 < H)],
@@ -64,9 +64,16 @@ theorem analytic_9_6 (m : ℕ) (k K : ℝ≥0) [fact (1 ≤ k)] :
     (Hd : ∀ c q (x : M.X c (-1) q), M.d x = 0)
     (Hd' : ∀ c p (x : M.X c p (-1)), M.d' x = 0)
     (cond : normed_spectral_conditions m k K ε hε k₀ M k' c₀ H),
-  (M.row 0).is_bounded_exact (max (k' * k') (2 * k₀ * H)) K (m+1) c₀ :=
+  (M.row 0).is_bounded_exact (max (k' * k') (2 * k₀ * H)) K m c₀ :=
 begin
-  induction m with m hm,
+  apply int.induction_on m,
+  swap 3,
+  { intro m, intros, refine ⟨1, zero_lt_one, 1, le_rfl, _⟩,
+    introsI,
+    intros c hc i hi,
+    refine Hneg c hc i _,
+    sorry },
+  -- induction m with m hm,
   { -- base case m = 0
     -- ε = 1/(2k) works
     use [1/(2*k)],
@@ -78,17 +85,15 @@ begin
     use k,
     use (by assumption),
     introsI M k' _k' _1k' c₀ H _H Hneg Hd Hd' cond,
-    intros c hc i hi x,
-    change i < 1 at hi,
-    -- Statement is of the form "for all x ∈ M_{0,i+1} exists y ∈ M_{0,i} such that..."
-    -- Cases i<-1 are trivial because x=y=0 works.
+    intros c hc i hi,
     cases lt_or_le i (-1 : ℤ) with h h,
-    { -- this should deal with -1
-      exact Hneg c hc i h x },
-    -- cases i = -1 and i = 0 left
+    { exact Hneg c hc i h },
+    -- Statement is of the form "for all x ∈ M_{0,i+1} exists y ∈ M_{0,i} such that..."
     interval_cases i,
-    { sorry },
-    { sorry }
+    { intro x,
+      simp only [row_d, Hd', sub_zero],
+      have := cond.col_exact 0 le_rfl c hc (-1) (by linarith),
+      sorry },
   },
   { -- inductive step
     sorry }
