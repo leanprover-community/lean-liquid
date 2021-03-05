f98adda68fb5a0796cb675b4cf4aa64906214a87
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-03-05 23:06:39 +0000

    finished soft_truncation'

--- src/system_of_complexes/soft_truncation_with_h_proofs.lean
+++ src/system_of_complexes/soft_truncation_with_h_proofs.lean
@@ -152,40 +152,23 @@ local attribute [instance] int.has_succ
 def soft_truncation' : cochain_complex' NormedGroup ℤ ⥤ cochain_complex' NormedGroup ℤ :=
 { obj := λ C, soft_truncation'.obj C,
   map := λ C₁ C₂ f, soft_truncation'.map f,
-  map_id' := λ X, by sorry,
-  map_comp' := sorry }
+  map_id' := λ X, begin
+    ext n x,
+    cases n, cases n,
+    { induction x,
+      { refl },
+      { refl } },
+    { refl },
+    { ext }
+  end,
+  map_comp' := λ X Y Z f g, begin
+    ext n x,
+    cases n, cases n,
+    { induction x,
+      { refl },
+      { refl } },
+    { refl },
+    { refl }
+  end }
 
 end NormedGroup
-
-namespace system_of_complexes
-
-variables (C : system_of_complexes)
-
-@[simps]
-def soft_truncation' : system_of_complexes ⥤ system_of_complexes :=
-(whiskering_right _ _ _).obj $ NormedGroup.soft_truncation'
-
-lemma soft_truncation'_d_neg (c : ℝ≥0) (i : ℤ) (hi : i < 0) :
-  (d : (soft_truncation'.obj C) c i ⟶ _) = 0 := sorry
-
-variables (k K : ℝ≥0) (m : ℤ) [hk : fact (1 ≤ k)] (c₀ : ℝ≥0)
-include hk
-
-lemma soft_truncation'_is_bounded_exact_iff
-  (hC : ∀ c, (d : (soft_truncation'.obj C) c (-2) ⟶ _) = 0) :
-  (soft_truncation'.obj C).is_bounded_exact k K m c₀ ↔ C.is_bounded_exact k K m c₀ :=
-sorry
-
-lemma soft_truncation'_is_weak_bounded_exact_iff
-  (hC : ∀ c, (d : (soft_truncation'.obj C) c (-2) ⟶ _) = 0) :
-  (soft_truncation'.obj C).is_weak_bounded_exact k K m c₀ ↔ C.is_weak_bounded_exact k K m c₀ :=
-sorry
-
-
-/-
-TODO
-
-* lemmas for how `has_shift` interacts with bounded exactness
--/
-
-end system_of_complexes
