55f017b4c2238f7d5891d19b0adcc7c94bc8aa5c
adomani <adomani@gmail.com>
2021-03-05 21:04:09 +0100

    with instance, for Eric

--- src/toric_2021_02_19/toric.lean
+++ src/toric_2021_02_19/toric.lean
@@ -6,6 +6,10 @@ import toric_2021_02_19.span_as_sum
 import linear_algebra.basic
 import linear_algebra.finite_dimensional
 import algebra.big_operators.basic
+import data.real.nnreal
+import facts.nnreal
+import algebra.algebra.basic
+import topology.instances.nnreal
 --import analysis.normed_space.inner_product
 
 /-! In the intended application, these are the players:
@@ -15,7 +19,7 @@ import algebra.big_operators.basic
 * `P = ℤ` is the target of the natural pairing `M ⊗ N → ℤ`.
 -/
 
-open_locale big_operators classical
+open_locale big_operators classical nnreal
 
 -- Here we make the general statements that require few assumptions on the various types.
 section abstract
@@ -530,10 +534,48 @@ pointed_of_is_basis_is_inj (is_inj_nonneg.pR_ocr R) bv
 
 /-  This lemmas is an application of `pointed_of_is_basis_is_inj`: it is present just as a proof
 of concept that `pointed_of_is_basis_is_inj` applies in this case. -/
-lemma pointed_of_is_basis {ι : Type*} (v : ι → M) (bv : is_basis ℤ v) :
+lemma pointed_of_integers {ι : Type*} {v : ι → M} (bv : is_basis ℤ v) :
   pointed ℤ (submodule.span ℕ (set.range v)) :=
 pointed_of_is_basis_is_inj (is_inj_nonneg.nat ℤ) bv
 
+/-  This lemmas is an application of `pointed_of_is_basis_is_inj`: it is present just as a proof
+of concept that `pointed_of_is_basis_is_inj` applies in this case. -/
+lemma pointed_of_rational {ι : Type*} {v : ι → M} [module ℚ M] (bv : is_basis ℚ v) :
+  pointed ℚ (submodule.span ℕ (set.range v)) :=
+pointed_of_is_basis_is_inj (is_inj_nonneg.nat ℚ) bv
+
+/-  This lemmas is an application of `pointed_of_is_basis_is_inj`: it is present just as a proof
+of concept that `pointed_of_is_basis_is_inj` applies in this case. -/
+lemma pointed_of_nat {R ι : Type*} [ordered_comm_ring R] [nontrivial R] [module R M] {v : ι → M}
+  (bv : is_basis R v) :
+  pointed R (submodule.span ℕ (set.range v)) :=
+pointed_of_is_basis_is_inj (is_inj_nonneg.nat R) bv
+
+
+instance : algebra ℝ≥0 ℝ := nnreal.to_real_hom.to_algebra
+
+variables {N : Type*} [add_comm_monoid N]
+
+def semimodule.of_algebra (R S : Type*) [comm_semiring R] [semiring S] [algebra R S]
+  [semimodule S N] :
+  semimodule R N :=
+{ smul := λ a b, algebra_map R S a • b,
+  one_smul := λ a, by simp only [one_smul, ring_hom.map_one],
+  mul_smul := λ x y m, by simp [(•), mul_smul ((algebra_map R S) x) ((algebra_map R S) y) m],
+  smul_add := λ r m n, smul_add ((algebra_map R S) r) m n,
+  smul_zero := λ r, smul_zero ((algebra_map R S) r),
+  add_smul := λ a b m, by simp [(•), add_smul ((algebra_map R S) a) ((algebra_map R S) b) m],
+  zero_smul := λ m, by simp only [ring_hom.map_zero, zero_smul] }
+
+instance [semimodule ℝ N] : semimodule ℝ≥0 N := semimodule.of_algebra ℝ≥0 ℝ
+
+instance ist [semimodule ℝ N] : is_scalar_tower ℝ≥0 ℝ N :=
+{ smul_assoc := λ a b c, show (a.val • b) • c = a • b • c, by { rw smul_assoc a.val b c, congr } }
+
+/--  Without the instance `ist`, the proof below does not work. -/
+lemma pointed_of_nnreal {ι : Type*} [module ℝ M] {v : ι → M} (bv : is_basis ℝ v) :
+  pointed ℝ (submodule.span ℝ≥0 (set.range v)) :=
+pointed_of_is_basis_is_inj (is_inj_nonneg.pR_ocr ℝ) bv
 
 lemma of_non_deg {ι : Type*} {f : pairing ℤ M M ℤ} {v : ι → M}
   (nd : perfect f) (sp : submodule.span ℤ (v '' set.univ)) :
