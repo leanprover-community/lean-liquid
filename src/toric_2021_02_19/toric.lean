a89665fb3eccd76885cfb0f8ffa2f41c7927af92
adomani <adomani@gmail.com>
2021-03-07 07:25:09 +0100

    several attempts, before Kevin, Kenny and Patrick

--- src/toric_2021_02_19/toric.lean
+++ src/toric_2021_02_19/toric.lean
@@ -9,6 +9,7 @@ import algebra.big_operators.basic
 import data.real.nnreal
 import facts.nnreal
 import algebra.algebra.basic
+import topology.separation
 import topology.instances.nnreal
 --import analysis.normed_space.inner_product
 
@@ -497,7 +498,269 @@ variables {M : Type*} [add_comm_group M] --[semimodule ℕ M]
 --  [add_comm_monoid P] --[semimodule ℕ P] [semimodule ℤ P] --[is_scalar_tower ℕ ℤ P]
 --  (P₀ : submodule ℕ P)
 
-lemma pointed_of_span_pos_is_inj {R S : Type*} [ordered_comm_ring S]
+/- Kenny's proof. -/
+lemma discrete_topology.of_subset {X : Type*} [topological_space X] {s t : set X}
+  (ds : discrete_topology s) (ts : t ⊆ s) :
+  discrete_topology t :=
+begin
+  rw [← singletons_open_iff_discrete, set_coe.forall] at ⊢ ds,
+  intros a ta,
+  specialize ds a (ts ta),
+  rw is_open_induced_iff at ⊢ ds,  -- don't just unfold
+  rcases ds with ⟨sa, osa, csa⟩,
+  refine ⟨sa, osa, _⟩,
+  rw [← (function.injective.image_injective subtype.coe_injective).eq_iff,
+      set.image_preimage_eq_inter_range, subtype.range_coe, set.image_singleton,
+      subtype.coe_mk] at csa ⊢,
+  rw [← set.inter_eq_self_of_subset_right ts, ← set.inter_assoc, csa],
+  exact set.subset.antisymm (set.inter_subset_left _ _)
+    (set.singleton_subset_iff.2 ⟨rfl, ta⟩)
+end
+
+
+lemma discrete_topology.of_subset_old {X : Type*} [topological_space X] {s t : set X}
+  (ds : discrete_topology s) (ts : t ⊆ s) :
+  discrete_topology t :=
+begin
+  rw [← singletons_open_iff_discrete, set_coe.forall] at ⊢ ds,
+  intros a ta,
+  rcases ds a (ts ta) with ⟨sa, osa, csa⟩,
+  refine ⟨sa, osa, _⟩,
+  obtain F : coe '' (coe ⁻¹' sa) = coe '' _ := congr_arg (λ ss, (coe : {x // x ∈ s} → X) '' ss) csa,
+  rw [set.image_singleton, subtype.image_preimage_coe, subtype.coe_mk] at F,
+  have sat : sa ∩ t ⊆ sa ∩ s := set.inter_subset_inter_right sa ts,
+  have sata : sa ∩ t = {a},
+  { refine set.subset.antisymm (λ x hx, _) (λ x (hx : x = a), _),
+    { rw ← F,
+      exact sat hx },
+    { subst hx,
+      refine ⟨set.mem_of_mem_inter_left (_ : x ∈ sa ∩ s), ta⟩,
+      exact set.singleton_subset_iff.mp (set.subset.antisymm_iff.mp F.symm).1 } },
+  ext1 x,
+  cases x with x xt,
+  suffices : x ∈ sa ↔ x = a, by simpa only [set.mem_singleton_iff],
+  refine ⟨λ xa, _, _⟩,
+  { have : x ∈ sa ∩ t := ⟨xa, xt⟩,
+    rwa sata at this },
+  { rintros rfl,
+    refine set.singleton_subset_iff.mp (λ el elx, _),
+    exact set.mem_of_mem_inter_left ((set.subset.antisymm_iff.mp sata.symm).1 elx) }
+end
+
+/-  Kevin's proof. -/
+lemma finite.smul_of_finite {S M : Type*} [semiring S] [add_comm_monoid M] [semimodule S M]
+  {G : set S} {v : set M} (fG : G.finite) (fv : v.finite) :
+  (G • v).finite :=
+fG.image2 (•) fv
+
+lemma finite.pointed_of_span_pos_is_inj {R S : Type*} [semiring S]
+  [comm_semiring R] [semimodule R M] [semimodule S M] [algebra R S]
+  [is_scalar_tower R S M] {G : set S} {v : set M}
+  (fG : G.finite) (spg : submodule.span R G = ⊤)
+  (fv : v.finite) (hv : submodule.span S v = ⊤) :
+  ∃ t : set M, t.finite ∧ submodule.span R (t : set M) = restrict_scalars R (span S (v : set M)) :=
+⟨G • v, finite.smul_of_finite fG fv, span_smul spg v⟩
+
+lemma finset.pointed_of_span_pos_is_inj {R S : Type*} [semiring S]
+  [comm_semiring R] [semimodule R M] [semimodule S M] [algebra R S]
+  [is_scalar_tower R S M]
+  (G : finset S) (spg : submodule.span R (G : set S) = ⊤)
+  (v : finset M) (hv : submodule.span S (v : set M) = ⊤) :
+  ∃ t : finset M, submodule.span R (t : set M) = restrict_scalars R (span S (v : set M)) :=
+begin
+  obtain ⟨t, ft, co⟩ := finite.pointed_of_span_pos_is_inj G.finite_to_set spg v.finite_to_set hv,
+  haveI ff : fintype t := ft.fintype,
+  refine ⟨t.to_finset, by simpa only [set.coe_to_finset]⟩
+end
+
+
+
+
+
+/--  The submodule spanned by a set `s` over an `R`-algebra `S` is spanned as an `R`-module by
+`s ∪ - s`, if for every element `a ∈ S`, either `a` or `- a` is in the image of `R`. -/
+lemma pointed_of_span_pos_is_inj {R S : Type*} [ordered_semiring S] [topological_space S]
+  [order_topology S] [comm_semiring R] [semimodule R M] [semimodule S M] [algebra R S]
+  [is_scalar_tower R S M]
+  -- the `R`-algebra `S` is compactly generated as an `R`-module
+  (G : set S) (cG : is_compact G) (spg : submodule.span R G = ⊤)
+  -- `R` is discrete as an `S`-module
+  -- this works well, for instance, in the case `ℤ ⊆ ℝ`.
+  -- It does not apply in the case `ℚ ⊆ ℝ`
+  (dR : discrete_topology (set.range (algebra_map R S)))
+  -- the `R`-lattice structure on `M` is given by the span of the set `v`
+  (v : set M) (hv : submodule.span S v = ⊤)
+  -- a finitely generated `S`-submodule of `M` is also finitely generated over `R`.
+  (s : finset M) (pro : finset S) :
+  ∃ t : finset M, submodule.span R (t : set M) =
+    ((submodule.span S (s : set M)).restrict_scalars R) ⊓ submodule.span R v :=
+begin
+  let GS : set S := (set.range (algebra_map R S)) ∩ G,
+  haveI dGS : discrete_topology GS :=
+    discrete_topology.of_subset dR ((set.range ⇑(algebra_map R S)).inter_subset_left G),
+  have cGS : is_compact (set.univ : set GS), sorry,
+  have GS_finite : (set.univ : set GS).finite := finite_of_is_compact_of_discrete set.univ cGS,
+  set GSM : set M := (GS : set S) • (s : set M),
+  have : GSM.finite,sorry,
+  refine ⟨this.to_finset, _⟩,
+  sorry,
+/-
+  simp,
+  ext x,
+  rw mem_inf,
+  refine ⟨λ hh, ⟨_, _⟩, _⟩,
+  rintro j ⟨jj, _, rfl⟩,
+  rintro l ⟨ll, _, rfl⟩,
+  dsimp at *,
+  let Gf : finset M := pro • s,
+--  {tidy?},
+  refine span_smul spg (s : set M),
+  --refine span_smul spg (s : set M),
+
+   intros hh,
+
+
+  apply finset.finite_to_set_to_finset,
+  fconstructor,
+  fconstructor,
+
+  convert this,
+  use this,
+  refine ⟨⟨GSM, this⟩, _⟩,
+  apply fintype_of_compact_of_discrete,
+
+    rw ← singletons_open_iff_discrete at ⊢ dR,
+
+    -- con questo voglio concludere la finitezza
+    --apply fintype_of_compact_of_discrete,
+  apply span_smul spg,
+  ext m,
+  refine ⟨λ hm, _, λ hm, _⟩,
+  { refine (span S (s : set M)).mem_coe.mpr _,
+    rcases mem_span_set.mp hm with ⟨c, csup, rfl⟩,
+    refine sum_mem _ (λ a as, (_ : c a • a ∈ span S s)),
+    rw ← algebra_map_smul S (c a) a,
+    obtain caS : (algebra_map R S (c a) ∈ (⊤ : submodule R S)) := mem_top,
+    rw ← spg at caS,
+    rcases mem_span_set.mp caS with ⟨d, dsup, did⟩,
+    rw [← did],
+    rw finsupp.sum_smul,
+    refine smul_mem (span S s) _ _,
+    obtain cams : a ∈ s ∪ - s := set.mem_of_mem_of_subset as csup,
+    cases (set.mem_union a s _).mp cams,
+    { exact subset_span h },
+    { refine (neg_mem_iff _).mp (subset_span h) } },
+  { rcases mem_span_set.mp hm with ⟨c, csup, rfl⟩,
+    rw [mem_carrier, mem_coe],
+    refine sum_mem _ (λ a as, (_ : c a • a ∈ span R (s ∪ - s))),
+    rcases ff (c a) with ⟨ca, cap | can⟩,
+    { rw [cap, algebra_map_smul],
+      refine smul_mem _ ca _,
+      refine subset_span (set.mem_union_left _ _),
+      exact set.mem_of_mem_of_subset (finset.mem_coe.mpr as) csup },
+    { rw [can, neg_smul, algebra_map_smul, ← smul_neg],
+      refine smul_mem _ ca _,
+      refine subset_span (set.mem_union_right _ _),
+      rw [set.mem_neg, neg_neg],
+      exact set.mem_of_mem_of_subset (finset.mem_coe.mpr as) csup } }
+-/
+end
+
+/--  The submodule spanned by a set `s` over an `R`-algebra `S` is spanned as an `R`-module by
+`s ∪ - s`, if for every element `a ∈ S`, either `a` or `- a` is in the image of `R`. -/
+lemma pointed_of_span_pos_is_inj_wrong_maybe {R S : Type*} [ordered_comm_ring S]
+  [comm_semiring R] [semimodule R M] [module S M] [algebra R S] [is_scalar_tower R S M]
+  (ff : ∀ a : S, ∃ n : R, a = algebra_map R S n ∨ a = - algebra_map R S n) (s : set M) :
+  (submodule.span R (s ∪ - s)).carrier = submodule.span S (s : set M) :=
+begin
+  ext m,
+  refine ⟨λ hm, _, λ hm, _⟩,
+  { refine (span S (s : set M)).mem_coe.mpr _,
+    rcases mem_span_set.mp hm with ⟨c, csup, rfl⟩,
+    refine sum_mem _ (λ a as, (_ : c a • a ∈ span S s)),
+    rw ← algebra_map_smul S (c a) a,
+    refine smul_mem (span S s) _ _,
+    obtain cams : a ∈ s ∪ - s := set.mem_of_mem_of_subset as csup,
+    cases (set.mem_union a s _).mp cams,
+    { exact subset_span h },
+    { refine (neg_mem_iff _).mp (subset_span h) } },
+  { rcases mem_span_set.mp hm with ⟨c, csup, rfl⟩,
+    rw [mem_carrier, mem_coe],
+    refine sum_mem _ (λ a as, (_ : c a • a ∈ span R (s ∪ - s))),
+    rcases ff (c a) with ⟨ca, cap | can⟩,
+    { rw [cap, algebra_map_smul],
+      refine smul_mem _ ca _,
+      refine subset_span (set.mem_union_left _ _),
+      exact set.mem_of_mem_of_subset (finset.mem_coe.mpr as) csup },
+    { rw [can, neg_smul, algebra_map_smul, ← smul_neg],
+      refine smul_mem _ ca _,
+      refine subset_span (set.mem_union_right _ _),
+      rw [set.mem_neg, neg_neg],
+      exact set.mem_of_mem_of_subset (finset.mem_coe.mpr as) csup } }
+end
+
+
+lemma pointed_of_span_pos_is_inj_2 {ι R S : Type*} [ordered_comm_ring S]
+  [comm_semiring R] [semimodule R M] [module S M] [algebra R S] [is_scalar_tower R S M]
+  (ff : ∀ s : S, ∃ n : R, s = algebra_map R S n ∨ s = - algebra_map R S n)
+  {v : ι → M} (bv : is_basis S v) (s : finset M) (hRS : is_inj_nonneg (algebra_map R S)) :
+  ∃ sR : finset M,
+    (submodule.span R (sR : set M)).carrier = submodule.span S (s : set M) :=
+begin
+  let ms : finset M := s.image (λ i, - i),
+  refine ⟨s ∪ (s.image (λ i, - i)), _⟩,
+  ext m,
+  refine ⟨_, _⟩;intros hm,
+  { refine (span S (s : set M)).mem_coe.mpr _,
+    rcases mem_span_finset.mp hm with ⟨c, rfl⟩,
+    refine sum_mem _ (λ a as, _),
+    rw ← algebra_map_smul S (c a) a,
+    refine smul_mem (span S (s : set M)) _ _,
+    sorry,
+/-
+    cases finset.mem_union.mp as,
+    have : a ∈ span S {a} := mem_span_singleton_self a,
+    have asu : {a} ⊆ s := finset.singleton_subset_iff.mpr h,
+    have : a ∈ (span S ↑s).carrier,refine set.mem_of_mem_of_subset asu _,
+    exact add_comm_group.to_add_comm_monoid M,
+    exact _inst_5,exact coe_to_lift,
+    simp,
+    convert asu,
+    simp,
+    refine set.mem_of_mem_of_subset _ this,
+    simp,
+    rintros?,
+    dsimp,
+-/
+     },
+  { --rw [mem_coe] at hm,
+    rcases mem_span_set.mp hm with ⟨c, csup, rfl⟩,
+    rw [mem_carrier, mem_coe],
+    refine sum_mem _ _,
+    intros a as,
+    dsimp,
+    rcases ff (c a) with ⟨ca, cap | can⟩,
+    rw [cap, algebra_map_smul],
+    refine smul_mem _ ca _,
+    simp,
+    sorry,
+    sorry,
+  },
+end
+
+lemma pointed_of_span_pos_is_inj_3 {ι R S : Type*} [linear_ordered_field S]
+  [comm_semiring R] [semimodule R M] [module S M] [algebra R S] [is_scalar_tower R S M]
+  (ff : ∀ s : S, 0 ≤ s → ∃ n d : R, s = algebra_map R S n / algebra_map R S d)
+  {v : ι → M} (bv : is_basis S v) {s : finset M} (hRS : is_inj_nonneg (algebra_map R S)) :
+  ∃ sR : finset M, (sR : set M) ⊆ (submodule.span R (set.range v ∪ set.range (λ i, - v i))) ∧
+    (submodule.span R (sR : set M)).carrier =
+      submodule.span R (set.range v) ∩ submodule.span S (set.range v) :=
+begin
+
+  sorry,
+end
+
+lemma pointed_of_span_pos_is_inj_4 {R S : Type*} [ordered_comm_ring S]
   [comm_semiring R] [semimodule R M] [module S M] [algebra R S] [is_scalar_tower R S M]
   [no_zero_divisors S] {s : set M} {l : M →ₗ[S] S}
   --[ordered_add_comm_monoid N] [semimodule S N] [ordered_semimodule S N]
