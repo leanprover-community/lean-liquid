4d9366bee362551e8660d6a9e6cb695fc1ba9830
Patrick Massot <patrickmassot@free.fr>
2021-03-14 00:06:06 +0100

    PR notes

--- src/for_mathlib/topology.lean
+++ src/for_mathlib/topology.lean
@@ -8,18 +8,22 @@ lemma closure_subset_preimage_closure_image {f : X → Y} {s : set X} (h : conti
   closure s ⊆ f ⁻¹' (closure (f '' s)) :=
 by { rw ← set.image_subset_iff, exact image_closure_subset_closure_image h }
 
+-- PRed in #6673
 lemma subsingleton.range_eq {α β : Type*} {f : α → β} (hs : subsingleton $ set.range f) (a b : α) : f a = f b :=
 subtype.mk.inj (subsingleton.elim (⟨f a, set.mem_range_self a⟩ : set.range f) ⟨f b, set.mem_range_self b⟩)
 
+-- PRed in #6673
 lemma subsingleton.image_eq {α β : Type*} {s : set α} {f : α → β} (hs : subsingleton $ f '' s) (a b : s) :
   f a = f b :=
 by { rw set.image_eq_range at hs, apply hs.range_eq }
 
+-- PRed in #6673
 lemma is_totally_disconnected_of_totally_disconnected_space
   [totally_disconnected_space X] (s : set X) :
   is_totally_disconnected s :=
 λ t hts ht, totally_disconnected_space.is_totally_disconnected_univ _ t.subset_univ ht
 
+-- PRed in #6673
 lemma is_preconnected.subsingleton [totally_disconnected_space X] {s : set X}
   (hs : is_preconnected s) :
   subsingleton s :=
@@ -28,16 +32,19 @@ is_totally_disconnected_of_totally_disconnected_space s s (set.subset.refl _) hs
 namespace embedding
 open filter
 
+-- PRed in #6673
 lemma t2_space [t2_space Y] {f : X → Y} (hf : embedding f) : t2_space X :=
 t2_iff_nhds.mpr $ λ x y h, hf.inj $ eq_of_nhds_ne_bot
 ⟨λ H, by simpa [hf.to_inducing.nhds_eq_comap, ← comap_inf, H, not_ne_bot.mpr] using h⟩
 
+-- PRed in #6673
 lemma is_totally_disconnected {f : X → Y} (hf : embedding f)
   (s : set X) (h : is_totally_disconnected (f '' s)) :
   is_totally_disconnected s :=
 λ t hts ht, ⟨λ a b,subtype.ext $ hf.inj $
   (h (f '' t) (set.image_subset f hts) (ht.image f hf.continuous.continuous_on)).image_eq a b⟩
 
+-- PRed in #6673
 lemma totally_disconnected_space [totally_disconnected_space Y] {f : X → Y} (hf : embedding f) :
   totally_disconnected_space X :=
 ⟨hf.is_totally_disconnected univ $ is_totally_disconnected_of_totally_disconnected_space (f '' univ)⟩
@@ -46,6 +53,7 @@ end embedding
 
 namespace inducing
 
+-- PRed in #6673
 lemma exists_open {f : X → Y} (hf : inducing f) ⦃U : set X⦄ (hU : is_open U) :
   ∃ V, is_open V ∧ f ⁻¹' V = U :=
 begin
@@ -56,6 +64,7 @@ end
 open filter
 open_locale filter topological_space
 
+-- PRed in #6673
 lemma is_compact {f : X → Y} (hf : inducing f) (s : set X) (hs : is_compact (f '' s)) :
   is_compact s :=
 begin
