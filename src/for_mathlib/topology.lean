c5fef5833991dc0f4bb248974d0989583704160e
Patrick Massot <patrickmassot@free.fr>
2021-03-13 23:40:45 +0100

    More topology golf

--- src/for_mathlib/topology.lean
+++ src/for_mathlib/topology.lean
@@ -1,12 +1,19 @@
 import topology.separation
 import topology.subset_properties
-
+open set
 variables {X Y : Type*} [topological_space X] [topological_space Y]
 
 -- PRed in #6669
 lemma closure_subset_preimage_closure_image {f : X → Y} {s : set X} (h : continuous f) :
   closure s ⊆ f ⁻¹' (closure (f '' s)) :=
 by { rw ← set.image_subset_iff, exact image_closure_subset_closure_image h }
+
+lemma subsingleton.range_eq {α β : Type*} {f : α → β} (hs : subsingleton $ set.range f) (a b : α) : f a = f b :=
+subtype.mk.inj (subsingleton.elim (⟨f a, set.mem_range_self a⟩ : set.range f) ⟨f b, set.mem_range_self b⟩)
+
+lemma subsingleton.image_eq {α β : Type*} {s : set α} {f : α → β} (hs : subsingleton $ f '' s) (a b : s) :
+  f a = f b :=
+by { rw set.image_eq_range at hs, apply hs.range_eq }
 
 lemma is_totally_disconnected_of_totally_disconnected_space
   [totally_disconnected_space X] (s : set X) :
@@ -19,44 +26,21 @@ lemma is_preconnected.subsingleton [totally_disconnected_space X] {s : set X}
 is_totally_disconnected_of_totally_disconnected_space s s (set.subset.refl _) hs
 
 namespace embedding
+open filter
 
-lemma t2_space [t2_space Y] {f : X → Y} (hf : embedding f) :
-  t2_space X :=
-{ t2 := λ x y h,
-  begin
-    obtain ⟨U, V, hU, hV, hx, hy, hUV⟩ := t2_separation (hf.inj.ne h),
-    refine ⟨f ⁻¹' U, f ⁻¹' V,
-      hf.continuous.is_open_preimage _ hU,
-      hf.continuous.is_open_preimage _ hV,
-      set.mem_preimage.mpr hx,
-      set.mem_preimage.mpr hy, _⟩,
-    rw ← set.disjoint_iff_inter_eq_empty at hUV ⊢,
-    exact hUV.preimage _
-  end }
+lemma t2_space [t2_space Y] {f : X → Y} (hf : embedding f) : t2_space X :=
+t2_iff_nhds.mpr $ λ x y h, hf.inj $ eq_of_nhds_ne_bot
+⟨λ H, by simpa [hf.to_inducing.nhds_eq_comap, ← comap_inf, H, not_ne_bot.mpr] using h⟩
 
 lemma is_totally_disconnected {f : X → Y} (hf : embedding f)
   (s : set X) (h : is_totally_disconnected (f '' s)) :
   is_totally_disconnected s :=
-begin
-  rintro t hts ht,
-  have htc : is_preconnected (f '' t) := ht.image f hf.continuous.continuous_on,
-  haveI := h _ (set.image_subset _ hts) htc,
-  constructor,
-  intros a b,
-  ext,
-  apply hf.inj,
-  have := @subsingleton.elim (f '' t) _
-    ⟨f a, set.mem_image_of_mem f a.2⟩ ⟨f b, set.mem_image_of_mem f b.2⟩,
-  simpa only [subtype.mk_eq_mk]
-end
+λ t hts ht, ⟨λ a b,subtype.ext $ hf.inj $
+  (h (f '' t) (set.image_subset f hts) (ht.image f hf.continuous.continuous_on)).image_eq a b⟩
 
 lemma totally_disconnected_space [totally_disconnected_space Y] {f : X → Y} (hf : embedding f) :
   totally_disconnected_space X :=
-{ is_totally_disconnected_univ :=
-  begin
-    apply hf.is_totally_disconnected,
-    apply is_totally_disconnected_of_totally_disconnected_space
-  end }
+⟨hf.is_totally_disconnected univ $ is_totally_disconnected_of_totally_disconnected_space (f '' univ)⟩
 
 end embedding
 
