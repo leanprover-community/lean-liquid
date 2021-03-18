b493d9fcebd9e7ebba45b238a9c72aa28cb550db
Johan Commelin <johan@commelin.net>
2021-03-18 14:43:20 +0000

    definition of the double complex featuring in proof of 9.5, some sorry's left

--- src/for_mathlib/preadditive_category.lean
+++ src/for_mathlib/preadditive_category.lean
@@ -4,11 +4,34 @@ namespace category_theory
 
 namespace preadditive
 
-variables {C : Type*} [category C] [preadditive C] {X Y Z : C}
+open limits
+
+variables {C D : Type*} [category C] [category D] [preadditive C] {X Y Z : C}
 
 def comp_hom : (X ⟶ Y) →+ (Y ⟶ Z) →+ (X ⟶ Z) :=
 add_monoid_hom.mk' (λ f, left_comp _ f) $
   λ f₁ f₂, add_monoid_hom.ext $ λ g, (right_comp _ g).map_add f₁ f₂
+
+instance : preadditive (D ⥤ C) :=
+{ hom_group := λ F G,
+  { add := λ α β,
+    { app := λ X, α.app X + β.app X,
+      naturality' := by { intros, rw [comp_add, add_comp, α.naturality, β.naturality] } },
+    zero := { app := λ X, 0, naturality' := by { intros, rw [zero_comp, comp_zero] } },
+    neg := λ α,
+    { app := λ X, -α.app X,
+      naturality' := by { intros, rw [comp_neg, neg_comp, α.naturality] } },
+    sub := λ α β,
+    { app := λ X, α.app X - β.app X,
+      naturality' := by { intros, rw [comp_sub, sub_comp, α.naturality, β.naturality] } },
+    add_assoc := by { intros, ext, apply add_assoc },
+    zero_add := by { intros, ext, apply zero_add },
+    add_zero := by { intros, ext, apply add_zero },
+    sub_eq_add_neg := by { intros, ext, apply sub_eq_add_neg },
+    add_left_neg := by { intros, ext, apply add_left_neg },
+    add_comm := by { intros, ext, apply add_comm } },
+  add_comp' := by { intros, ext, apply add_comp },
+  comp_add' := by { intros, ext, apply comp_add } }
 
 end preadditive
 
--- src/thm95/double_complex.lean
+++ src/thm95/double_complex.lean
@@ -2,6 +2,8 @@ import polyhedral_lattice.cosimplicial
 import polyhedral_lattice.Hom
 import pseudo_normed_group.system_of_complexes
 
+import simplicial.alternating_face_map
+
 import thm95.constants
 
 .
@@ -23,13 +25,23 @@ variables (V : NormedGroup) [normed_with_aut r V]
 variables (Λ : PolyhedralLattice) (M : ProFiltPseuNormGrpWithTinv r')
 variables (N : ℕ) [fact (0 < N)]
 
-def Cech_nerve : simplicial_object (ProFiltPseuNormGrpWithTinv r') :=
-(PolyhedralLattice.cosimplicial Λ N).op ⋙ PolyhedralLattice.Hom M
+def Cech_nerve : simplex_category ⥤ (ProFiltPseuNormGrpWithTinv r')ᵒᵖ :=
+PolyhedralLattice.cosimplicial Λ N ⋙ PolyhedralLattice.Hom M
 
-def Cech_augmentation_map : (Cech_nerve r' Λ M N).obj (op $ mk 0) ⟶ Hom Λ M :=
-(PolyhedralLattice.Hom M).map
-  (PolyhedralLattice.cosimplicial_augmentation_map Λ N).op
+/-- Warning: this is a map in the *opposite* category. -/
+def Cech_augmentation_map :
+  (PolyhedralLattice.Hom M).obj Λ ⟶ (Cech_nerve r' Λ M N).obj (mk 0) :=
+(PolyhedralLattice.Hom M).map (PolyhedralLattice.cosimplicial_augmentation_map Λ N)
 
--- def double_complex := sorry
+def cosimplicial_system_of_complexes : simplex_category ⥤ system_of_complexes :=
+Cech_nerve r' Λ M N ⋙ BD.System c' r V r'
+
+def augmentation_map :
+  (BD.System c' r V r').obj (op $ Hom Λ M) ⟶
+  (cosimplicial_system_of_complexes BD c' r r' V Λ M N).obj (mk 0) :=
+(BD.System c' r V r').map (Cech_augmentation_map r' Λ M N)
+
+def double_complex : @cochain_complex ℕ system_of_complexes _ _ _ :=
+alt_face_map_cocomplex (augmentation_map BD c' r r' V Λ M N) sorry
 
 end thm95
