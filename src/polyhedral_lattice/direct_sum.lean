e23db366baefd3c004d2448e4df8f46f14ddaad4
Johan Commelin <johan@commelin.net>
2021-03-20 12:47:52 +0000

    polyhedral_lattice/direct_sum is quasi-dead code

--- src/polyhedral_lattice/direct_sum.lean
+++ src/polyhedral_lattice/direct_sum.lean
@@ -49,41 +49,43 @@ normed_group.of_core _ $
     apply dfinsupp.neg_apply -- this is missing for direct_sum
   end }
 
-instance : polyhedral_lattice (⨁ i, Λ i) :=
-{ finite_free := sorry,
-  polyhedral :=
-  begin
-    have := λ i, polyhedral_lattice.polyhedral (Λ i),
-    choose J _instJ x hx using this, resetI,
-    refine ⟨Σ i, J i, infer_instance, λ j, direct_sum.of _ j.1 (x _ j.2), _⟩,
-    intro l,
-    have := λ i, hx i (l i),
-    choose d hd c H1 H2 using this,
-    let d' : ι → ℕ := λ i₀, ∏ i in (finset.univ.erase i₀), d i,
-    have hl : l = ∑ i, direct_sum.of _ i (l i),
-    { sorry },
-    refine ⟨∏ i, d i, _, λ j, d' j.1 * c j.1 j.2, _, _⟩,
-    { apply nat.pos_of_ne_zero,
-      rw finset.prod_ne_zero_iff,
-      rintro i - hi,
-      exact nat.not_lt_zero 0 (hi.subst $ hd i) },
-    { rw [hl, finset.smul_sum, ← finset.univ_sigma_univ, finset.sum_sigma],
-      apply fintype.sum_congr,
-      intro i,
-      rw [← finset.insert_erase (finset.mem_univ i), finset.prod_insert (finset.not_mem_erase _ _),
-        mul_comm, mul_smul, ← nsmul_eq_smul (d i), ← add_monoid_hom.map_nsmul, nsmul_eq_smul, H1,
-        add_monoid_hom.map_sum, finset.smul_sum],
-      apply fintype.sum_congr,
-      intro j,
-      rw [mul_smul, ← nsmul_eq_smul (c i j), add_monoid_hom.map_nsmul, nsmul_eq_smul] },
-    { rw [direct_sum_norm_def, ← finset.univ_sigma_univ, finset.sum_sigma, finset.mul_sum],
-      apply fintype.sum_congr,
-      intro i,
-      dsimp,
-      rw [← finset.insert_erase (finset.mem_univ i), finset.prod_insert (finset.not_mem_erase _ _),
-        nat.cast_mul, mul_right_comm, H2],
-      -- now use H2
-      sorry }
-  end }
+-- === superseded by a version using `finsupp`, so no need to complete this
+
+-- instance : polyhedral_lattice (⨁ i, Λ i) :=
+-- { finite_free := by admit,
+--   polyhedral :=
+--   begin
+--     have := λ i, polyhedral_lattice.polyhedral (Λ i),
+--     choose J _instJ x hx using this, resetI,
+--     refine ⟨Σ i, J i, infer_instance, λ j, direct_sum.of _ j.1 (x _ j.2), _⟩,
+--     intro l,
+--     have := λ i, hx i (l i),
+--     choose d hd c H1 H2 using this,
+--     let d' : ι → ℕ := λ i₀, ∏ i in (finset.univ.erase i₀), d i,
+--     have hl : l = ∑ i, direct_sum.of _ i (l i),
+--     { admit },
+--     refine ⟨∏ i, d i, _, λ j, d' j.1 * c j.1 j.2, _, _⟩,
+--     { apply nat.pos_of_ne_zero,
+--       rw finset.prod_ne_zero_iff,
+--       rintro i - hi,
+--       exact nat.not_lt_zero 0 (hi.subst $ hd i) },
+--     { rw [hl, finset.smul_sum, ← finset.univ_sigma_univ, finset.sum_sigma],
+--       apply fintype.sum_congr,
+--       intro i,
+--       rw [← finset.insert_erase (finset.mem_univ i), finset.prod_insert (finset.not_mem_erase _ _),
+--         mul_comm, mul_smul, ← nsmul_eq_smul (d i), ← add_monoid_hom.map_nsmul, nsmul_eq_smul, H1,
+--         add_monoid_hom.map_sum, finset.smul_sum],
+--       apply fintype.sum_congr,
+--       intro j,
+--       rw [mul_smul, ← nsmul_eq_smul (c i j), add_monoid_hom.map_nsmul, nsmul_eq_smul] },
+--     { rw [direct_sum_norm_def, ← finset.univ_sigma_univ, finset.sum_sigma, finset.mul_sum],
+--       apply fintype.sum_congr,
+--       intro i,
+--       dsimp,
+--       rw [← finset.insert_erase (finset.mem_univ i), finset.prod_insert (finset.not_mem_erase _ _),
+--         nat.cast_mul, mul_right_comm, H2],
+--       -- now use H2
+--       admit }
+--   end }
 
 end polyhedral_lattice
