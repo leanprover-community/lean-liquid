0d1f70e2be3f2f6d5405bc189cab9700c7a4aa44
Mario Carneiro <di.gama@gmail.com>
2021-03-25 15:34:44 -0400

    categorify FiltrationPow

--- src/pseudo_normed_group/FiltrationPow.lean
+++ src/pseudo_normed_group/FiltrationPow.lean
@@ -21,7 +21,7 @@ Profinite.of (pseudo_normed_group.filtration M c)
 
 @[simps]
 def ProFiltPseuNormGrpWithTinv.level
-  (r' c : ℝ≥0) : ProFiltPseuNormGrpWithTinv r' ⥤ Profinite :=
+  (r c : ℝ≥0) : ProFiltPseuNormGrpWithTinv r ⥤ Profinite :=
 { obj := λ M, pseudo_normed_group.filtration_obj M c,
   map := λ M N f, ⟨f.level c, f.level_continuous c⟩ }
 
@@ -29,7 +29,7 @@ open NormedGroup opposite Profinite pseudo_normed_group category_theory breen_de
 open profinitely_filtered_pseudo_normed_group
 open profinitely_filtered_pseudo_normed_group_with_Tinv
 
-/-- The "functor" that sends `M` and `c` to `(filtration M c)^n` -/
+/-- The functor that sends `A` to `A^n` -/
 @[simps]
 def Pow (n : ℕ) : Profinite ⥤ Profinite :=
 { obj := λ A, of (A^n),
@@ -43,55 +43,36 @@ def Pow (n : ℕ) : Profinite ⥤ Profinite :=
       exact f.2.comp (continuous_apply j),
     end } }
 
-universe variable u
-variables {r' : ℝ≥0} {M M₁ M₂ M₃ : Type u}
-variables [profinitely_filtered_pseudo_normed_group_with_Tinv r' M]
-variables [profinitely_filtered_pseudo_normed_group_with_Tinv r' M₁]
-variables [profinitely_filtered_pseudo_normed_group_with_Tinv r' M₂]
-variables [profinitely_filtered_pseudo_normed_group_with_Tinv r' M₃]
-variables (c c₁ c₂ c₃ c₄ : ℝ≥0) (l m n : ℕ) (ϕ : basic_universal_map m n)
-variables (f : profinitely_filtered_pseudo_normed_group_with_Tinv_hom r' M₁ M₂)
-variables (g : profinitely_filtered_pseudo_normed_group_with_Tinv_hom r' M₂ M₃)
-
 @[simps]
 def profinitely_filtered_pseudo_normed_group_with_Tinv.Tinv₀_hom
+  {r' : ℝ≥0} (M : Type*) [profinitely_filtered_pseudo_normed_group_with_Tinv r' M]
   (c c₂ : ℝ≥0) [fact (r'⁻¹ * c ≤ c₂)] : filtration_obj M c ⟶ filtration_obj M c₂ :=
 by exact ⟨Tinv₀ c c₂, Tinv₀_continuous _ _⟩
 
 open profinitely_filtered_pseudo_normed_group_with_Tinv
 
 /-- The "functor" that sends `M` and `c` to `(filtration M c)^n` -/
-def FiltrationPow (r' c : ℝ≥0) (n : ℕ) :
-  ProFiltPseuNormGrpWithTinv r' ⥤ Profinite :=
-ProFiltPseuNormGrpWithTinv.level r' c ⋙ Pow n
+@[simps] def FiltrationPow (r : ℝ≥0) (c : ℝ≥0) (n : ℕ) :
+  ProFiltPseuNormGrpWithTinv r ⥤ Profinite :=
+ProFiltPseuNormGrpWithTinv.level r c ⋙ Pow n
 
 namespace FiltrationPow
 
 @[simps]
-def cast_le [fact (c₁ ≤ c₂)] : (FiltrationPow r' c₁ n).obj M ⟶ (FiltrationPow r' c₂ n).obj M :=
-(Pow n).map ⟨cast_le, (embedding_cast_le c₁ c₂).continuous⟩
-
-@[simp] lemma cast_le_refl : cast_le r' c c n = 𝟙 (FiltrationPow r' M c n) := by { ext, refl }
-
-lemma cast_le_trans [fact (c₁ ≤ c₂)] [fact (c₂ ≤ c₃)] [fact (c₁ ≤ c₃)] :
-  cast_le r' c₁ c₂ n ≫ cast_le r' c₂ c₃ n = @cast_le r' M _ c₁ c₃ n _ :=
-by { ext, refl }
-
-lemma map_comp_cast_le [fact (c₁ ≤ c₂)] :
-  map r' c₁ n f ≫ cast_le r' c₁ c₂ n = cast_le r' c₁ c₂ n ≫ map r' c₂ n f :=
-by { ext, refl }
+def cast_le (r : ℝ≥0) (c₁ c₂ : ℝ≥0) [fact (c₁ ≤ c₂)] (n : ℕ) :
+  nat_trans (FiltrationPow r c₁ n) (FiltrationPow r c₂ n) :=
+{ app := λ M, (Pow n).map ⟨cast_le, (embedding_cast_le c₁ c₂).continuous⟩,
+  naturality' := λ M N f, by { ext, refl } }
 
 @[simps]
-def Tinv : FiltrationPow r' M c n ⟶ FiltrationPow r' M (r'⁻¹ * c) n :=
-(Pow n).map (Tinv₀_hom _ c (r'⁻¹ * c))
+def Tinv (r : ℝ≥0) (c c₂) [fact (r⁻¹ * c ≤ c₂)] (n) :
+  nat_trans (FiltrationPow r c n) (FiltrationPow r c₂ n) :=
+{ app := λ M, (Pow n).map (Tinv₀_hom M c c₂),
+  naturality' := λ M N f, by { ext x j, exact (f.map_Tinv (x j).1).symm } }
 
-lemma map_comp_Tinv :
-  map r' c n f ≫ Tinv r' c n = Tinv r' c n ≫ map r' (r'⁻¹ * c) n f :=
-by { ext x j, exact (f.map_Tinv (x j).1).symm }
-
-lemma cast_le_comp_Tinv [fact (c₁ ≤ c₂)] :
-  cast_le r' c₁ c₂ n ≫ (@Tinv r' M _ c₂ n) =
-    Tinv r' c₁ n ≫ cast_le r' (r'⁻¹ * c₁) (r'⁻¹ * c₂) n :=
+lemma cast_le_vcomp_Tinv {r : ℝ≥0} (c₁ c₂ c₃ : ℝ≥0)
+  [fact (c₁ ≤ c₂)] [fact (c₂ ≤ c₃)] [fact (r⁻¹ * c₁ ≤ c₂)] [fact (r⁻¹ * c₂ ≤ c₃)] (n : ℕ) :
+  (cast_le r c₁ c₂ n).vcomp (Tinv r c₂ c₃ n) = (Tinv r c₁ c₂ n).vcomp (cast_le r c₂ c₃ n) :=
 by { ext, refl }
 
 end FiltrationPow
@@ -99,19 +80,34 @@ end FiltrationPow
 namespace breen_deligne
 namespace basic_universal_map
 
+universe variable u
+variables (r c c₁ c₂ c₃ c₄ : ℝ≥0) {l m n : ℕ} (ϕ : basic_universal_map m n)
+
 open FiltrationPow
 
-variables (M) {l m n}
-
 @[simps]
-def eval_FP [ϕ.suitable c₁ c₂] : FiltrationPow r' M c₁ m ⟶ FiltrationPow r' M c₂ n :=
-{ to_fun := ϕ.eval_png₀ M c₁ c₂,
-  continuous_to_fun := ϕ.eval_png₀_continuous M c₁ c₂ }
+def eval_FP [ϕ.suitable c₁ c₂] : nat_trans (FiltrationPow r c₁ m) (FiltrationPow r c₂ n) :=
+{ app := λ M,
+  { to_fun := ϕ.eval_png₀ M c₁ c₂,
+    continuous_to_fun := ϕ.eval_png₀_continuous M c₁ c₂ },
+  naturality' := λ M₁ M₂ f, begin
+    ext1 x,
+    change ϕ.eval_png₀ M₂ c₁ c₂ ((FiltrationPow r c₁ m).map f x) =
+      (FiltrationPow r c₂ n).map f (ϕ.eval_png₀ M₁ c₁ c₂ x),
+    ext j,
+    dsimp only [basic_universal_map.eval_png₀],
+    simp only [basic_universal_map.eval_png_apply, f.map_sum,
+      FiltrationPow_map_to_fun_coe, subtype.coe_mk, pow_incl_apply, f.level_coe],
+    apply fintype.sum_congr,
+    intro i,
+    simp only [← gsmul_eq_smul],
+    exact (f.to_add_monoid_hom.map_gsmul _ _).symm
+  end }
 
 lemma eval_FP_comp (g : basic_universal_map m n) (f : basic_universal_map l m)
   [hg : g.suitable c₂ c₃] [hf : f.suitable c₁ c₂] [(g.comp f).suitable c₁ c₃] :
-  (g.comp f).eval_FP r' M c₁ c₃ =
-  f.eval_FP r' M c₁ c₂ ≫ g.eval_FP r' M c₂ c₃ :=
+  (g.comp f).eval_FP r c₁ c₃ =
+  (f.eval_FP r c₁ c₂).vcomp (g.eval_FP r c₂ c₃) :=
 begin
   ext j s i,
   dsimp,
@@ -121,39 +117,26 @@ begin
   refl,
 end
 
-lemma map_comp_eval_FP [ϕ.suitable c₁ c₂] :
-  map r' c₁ m f ≫ ϕ.eval_FP r' M₂ c₁ c₂ = ϕ.eval_FP r' M₁ c₁ c₂ ≫ map r' c₂ n f :=
-begin
-  ext1 x,
-  show ϕ.eval_png₀ M₂ c₁ c₂ (map r' c₁ m f x) = map r' c₂ n f (ϕ.eval_png₀ M₁ c₁ c₂ x),
-  ext j,
-  dsimp only [basic_universal_map.eval_png₀],
-  simp only [basic_universal_map.eval_png_apply, f.map_sum, map_to_fun, subtype.coe_mk,
-    pow_incl_apply, f.level_coe],
-  apply fintype.sum_congr,
-  intro i,
-  simp only [← gsmul_eq_smul],
-  exact (f.to_add_monoid_hom.map_gsmul _ _).symm
-end
-
 lemma cast_le_comp_eval_FP
   [fact (c₁ ≤ c₂)] [ϕ.suitable c₂ c₄] [ϕ.suitable c₁ c₃] [fact (c₃ ≤ c₄)] :
-  cast_le r' c₁ c₂ m ≫ ϕ.eval_FP r' M c₂ c₄ = ϕ.eval_FP r' M c₁ c₃ ≫ cast_le r' c₃ c₄ n :=
+  (cast_le r c₁ c₂ m).vcomp (ϕ.eval_FP r c₂ c₄) =
+  (ϕ.eval_FP r c₁ c₃).vcomp (cast_le r c₃ c₄ n) :=
 by { ext, refl }
 
-open profinitely_filtered_pseudo_normed_group_with_Tinv
+open FiltrationPow
 
-lemma Tinv_comp_eval_FP [ϕ.suitable c₁ c₂] :
-  Tinv r' c₁ m ≫ ϕ.eval_FP r' M (r'⁻¹ * c₁) (r'⁻¹ * c₂) =
-    ϕ.eval_FP r' M c₁ c₂ ≫ Tinv r' c₂ n :=
+lemma Tinv_comp_eval_FP (r c₁ c₂ c₃ : ℝ≥0)
+  [fact (r⁻¹ * c₁ ≤ c₂)] [fact (r⁻¹ * c₂ ≤ c₃)] [ϕ.suitable c₁ c₂] [ϕ.suitable c₂ c₃] :
+  (Tinv r c₁ c₂ m).vcomp (ϕ.eval_FP r c₂ c₃) =
+  (ϕ.eval_FP r c₁ c₂).vcomp (Tinv r c₂ c₃ n) :=
 begin
-  ext1 x,
-  show ϕ.eval_png₀ M (r'⁻¹ * c₁) (r'⁻¹ * c₂) (Tinv r' c₁ m x) =
-    Tinv r' c₂ n (ϕ.eval_png₀ M c₁ c₂ x),
+  ext M x : 3,
+  change ϕ.eval_png₀ M c₂ c₃ ((Tinv r c₁ c₂ m).app M x) =
+    (Tinv r c₂ c₃ n).app M (ϕ.eval_png₀ M c₁ c₂ x),
   ext j,
   dsimp only [eval_png₀],
-  simp only [eval_png_apply, map_to_fun, subtype.coe_mk, pow_incl_apply,
-    FiltrationPow.Tinv, Pow_map_to_fun, Tinv₀_hom_to_fun, Tinv₀_coe,
+  simp only [eval_png_apply, subtype.coe_mk, pow_incl_apply,
+    FiltrationPow.Tinv, FiltrationPow_map_to_fun_coe, Pow_map_to_fun, Tinv₀_hom_to_fun, Tinv₀_coe,
     profinitely_filtered_pseudo_normed_group_hom.map_sum],
   apply fintype.sum_congr,
   intro i,
