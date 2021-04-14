dfd249e5f54bef577b4e32acf2fa76878eb9fefe
adomani <adomani@gmail.com>
2021-04-14 18:03:50 +0200

    shorter

--- src/toric/lem97.lean
+++ src/toric/lem97.lean
@@ -60,13 +60,10 @@ begin
 end
 
 lemma aux_2 {N : ℕ} (hN : 0 < N) {l : ι → Λ} {S₀ : finset (Λ →+ ℤ)}
-  (hS₀ : submodule.span ℕ ↑S₀ = explicit_dual_set l)
-  (f r : (Λ →+ ℤ) → ℕ) :
-  let S : Type u_1 := {x // x ∈ S₀},
-      Y : Type u_1 := S → fin N,
+  (hS₀ : submodule.span ℕ ↑S₀ = explicit_dual_set l) {f r : (Λ →+ ℤ) → ℕ} :
+  let Y : Type u_1 := {x // x ∈ S₀} → fin N,
       ψ : Y → Λ →+ ℤ :=
-        λ (y : Y),
-          ∑ (s : {x // x ∈ S₀}) in S₀.attach, (y s).val • s.val,
+        λ (y : Y), ∑ (s : {x // x ∈ S₀}) in S₀.attach, (y s).val • s.val,
       B : finset (Λ →+ ℤ) := finset.image ψ finset.univ,
       g : (Λ →+ ℤ) → fin N := λ (i : Λ →+ ℤ), ⟨f i % N, nat.mod_lt _ hN⟩,
       x' : Λ →+ ℤ := ∑ (i : Λ →+ ℤ) in S₀, (g i).val • i
@@ -76,7 +73,7 @@ lemma aux_2 {N : ℕ} (hN : 0 < N) {l : ι → Λ} {S₀ : finset (Λ →+ ℤ)}
      ∀ (i : ι),
        x' (l i) ≤ (⇑∑ (i : Λ →+ ℤ) in S₀, f i • i) (l i) :=
 begin
-  intros S Y ψ B g x' hr hx' H i,
+  intros Y ψ B g x' hr hx' H i,
   dsimp [x'],
   rw [sub_nonpos.symm, sub_eq_add_neg, ← add_monoid_hom.neg_apply, ← finset.sum_neg_distrib,
     add_monoid_hom.finset_sum_apply, add_monoid_hom.finset_sum_apply, ← finset.sum_add_distrib],
@@ -95,12 +92,10 @@ begin
   exact nat.mod_le _ _
 end
 
-lemma aux_3 {Λ ι : Type*} (N : ℕ) [add_comm_group Λ] (hN : 0 < N) (l : ι → Λ)
-  (S₀ : finset (Λ →+ ℤ)) (hS₀ : submodule.span ℕ ↑S₀ = explicit_dual_set l) :
+lemma aux_3 {N : ℕ} (hN : 0 < N) {l : ι → Λ} {S₀ : finset (Λ →+ ℤ)}
+  (hS₀ : submodule.span ℕ ↑S₀ = explicit_dual_set l) :
   let Y : Type u_1 := {x // x ∈ S₀} → fin N,
-      ψ : Y → Λ →+ ℤ :=
-        λ (y : Y),
-          ∑ (s : {x // x ∈ S₀}) in S₀.attach, (y s).val • s.val,
+      ψ : Y → Λ →+ ℤ := λ (y : Y), ∑ (s : {x // x ∈ S₀}) in S₀.attach, (y s).val • s.val,
       B : finset (Λ →+ ℤ) := finset.image ψ finset.univ
   in ∀ (x : Λ →+ ℤ),
        x ∈ explicit_dual_set l →
@@ -128,7 +123,7 @@ begin
   { rw [hr, finset.smul_sum, ← finset.sum_add_distrib],
     simp_rw [← smul_assoc, ← add_smul, add_comm (N • _) _],
     refl },
-  exact aux_2 hN hS₀ (λ i, f i) r hr hx' H,
+  exact aux_2 hN hS₀ hr hx' H,
 end
 
 lemma lem97_pos (hΛ : finite_free Λ) [fintype ι] (N : ℕ) (hN : 0 < N) (l : ι → Λ) :
@@ -137,14 +132,9 @@ lemma lem97_pos (hΛ : finite_free Λ) [fintype ι] (N : ℕ) (hN : 0 < N) (l : 
    x = N • y + x' ∧ ∀ i, x' (l i) ≤ x (l i) :=
 begin
   obtain ⟨S₀, hS₀⟩ := explicit_gordan hΛ l,
-  let S:= { x // x ∈ S₀},
-  let Y := S → (fin N),
+  let Y := { x // x ∈ S₀ } → (fin N),
   let ψ := (λ y : Y, ∑ s in finset.attach S₀, (y s).1 • s.val),--modification?
-  let B := finset.image ψ finset.univ,
-  use B,
-  split,
-  { exact aux_1 hS₀ },
-  { refine aux_3 N hN l S₀ hS₀ },
+  exact ⟨finset.image ψ finset.univ, aux_1 hS₀, aux_3 hN hS₀⟩,
 end
 
 section sign_vectors
@@ -157,9 +147,8 @@ instance sign_vectors_inhabited : inhabited (sign_vectors ι) := ⟨(λ i, 1)⟩
 
 def fintype_sign_vectors [fintype ι] : fintype (sign_vectors ι) := pi.fintype
 
-/--Given a list l of elements of Λ and a functional x, (pos_vector l x) is the sign-vector of
-the values of x (l i).
--/
+/-- Given a list l of elements of Λ and a functional x, (pos_vector l x) is the sign-vector of
+the values of x (l i). -/
 def pos_vector (l : ι → Λ) (x : Λ →+ ℤ) : sign_vectors ι :=
 λ i, nonzero_sign (x (l i))
 
@@ -169,21 +158,23 @@ def coe_to_signs : (sign_vectors ι) → (ι → ℤ) :=
 instance coe_signs : has_coe (sign_vectors ι) (ι → ℤ) := ⟨ coe_to_signs ⟩
 
 instance smul_signs : has_scalar (sign_vectors ι) (ι → Λ) :=
-{smul := λ ε l i, (ε i : ℤ) • l i }
+{ smul := λ ε l i, (ε i : ℤ) • l i }
+
+lemma pos_vector_nonneg (l : ι → Λ) (x : Λ →+ ℤ) (j : ι) :
+  0 ≤ x ((pos_vector l x • l) j) :=
+begin
+  rw [pos_vector, nonzero_sign],
+  simp only [has_scalar.smul, add_monoid_hom.map_gsmul, gsmul_int_int],
+  split_ifs,
+  { rwa [units.coe_one, one_mul] },
+  { rw [units.coe_neg_one, neg_one_mul],
+    refine (neg_pos.mpr (not_le.mp h)).le }
+end
+
 
 lemma smul_to_explicit_dual_set (l : ι → Λ) (x : Λ →+ ℤ) :
   x ∈ (explicit_dual_set ((pos_vector l x) • l)) :=
-begin
-  intro j,
-  simp only [pos_vector, nonzero_sign, has_scalar.smul, id.def,
-    ge_iff_le, add_monoid_hom.map_gsmul, gsmul_int_int],
-  by_cases h_pos : x(l j) ≥ 0,
-  { rwa [if_pos h_pos, units.coe_one, one_mul], },
-  { rw [if_neg h_pos, units.coe_neg, units.coe_one, neg_mul_eq_neg_mul_symm,
-      one_mul, neg_nonneg],
-    rw not_le at h_pos,
-    exact le_of_lt h_pos },
-end
+λ j, pos_vector_nonneg l x j
 
 lemma pos_vector_id_if_nonneg (l : ι → Λ) (x : Λ →+ ℤ) (i : ι) : 0 ≤ x (l i) →
     (pos_vector l x • l) i = l i :=
@@ -212,14 +203,16 @@ def pos_A [fintype ι] (hΛ : finite_free Λ) (N : ℕ) (hN : 0 < N)
   (l : ι → Λ) (ε : sign_vectors ι) : finset (Λ →+ ℤ) :=
 some (lem97_pos hΛ N hN (ε • l))
 
-lemma posA_to_explicit [fintype ι] (hΛ : finite_free Λ) (N : ℕ) (hN : 0 < N)
-  (l : ι → Λ) (ε : sign_vectors ι) (x' : Λ →+ ℤ) (H : x' ∈ pos_A hΛ N hN l ε) : x' ∈ explicit_dual_set (ε • l)
-  := (some_spec (lem97_pos hΛ N hN (ε • l))).1 x' H
+lemma posA_to_explicit [fintype ι] (hΛ : finite_free Λ) (N : ℕ) (hN : 0 < N) (l : ι → Λ)
+  (ε : sign_vectors ι) (x' : Λ →+ ℤ) (H : x' ∈ pos_A hΛ N hN l ε) :
+  x' ∈ explicit_dual_set (ε • l) :=
+(some_spec (lem97_pos hΛ N hN (ε • l))).1 x' H
 
 
-lemma exists_good_pair [fintype ι] (hΛ : finite_free Λ) (N : ℕ) (hN : 0 < N) (l : ι → Λ) (ε : sign_vectors ι)
-  (x : Λ →+ ℤ) (H : x ∈ (explicit_dual_set (ε • l))) : ∃ x' y : (Λ →+ ℤ),
-  x' ∈ pos_A hΛ N hN l ε ∧ x = N • y + x' ∧ ∀ i, x' ((ε • l) i) ≤ x ((ε • l) i) :=
+lemma exists_good_pair [fintype ι] (hΛ : finite_free Λ) (N : ℕ) (hN : 0 < N) (l : ι → Λ)
+  (ε : sign_vectors ι) (x : Λ →+ ℤ) (H : x ∈ (explicit_dual_set (ε • l))) :
+  ∃ x' y : (Λ →+ ℤ),
+    x' ∈ pos_A hΛ N hN l ε ∧ x = N • y + x' ∧ ∀ i, x' ((ε • l) i) ≤ x ((ε • l) i) :=
 begin
   obtain ⟨x', hx', ⟨y, hy⟩⟩ := (some_spec (lem97_pos hΛ N hN (ε • l))).2 x H,
   exact ⟨x', y, hx', hy⟩,
@@ -251,8 +244,7 @@ begin
       posA_to_explicit hΛ N hN l (pos_vector l x) x' mem_x',
     replace h_pos' : 0 ≤ x' (((pos_vector l x) • l) i) := h_pos' _,
     by_cases h_pos : 0 ≤ x (l i),
-    {
-      have h_posvect_id : ((pos_vector l x) • l) i = l i := pos_vector_id_if_nonneg l x i h_pos,
+    { have h_posvect_id : ((pos_vector l x) • l) i = l i := pos_vector_id_if_nonneg l x i h_pos,
       replace h_pos' : 0 ≤ x' (l i) := h_pos'.trans (le_of_eq (congr_arg x' h_posvect_id)),
       refine or.inl ⟨h_pos', _⟩,
       rw ← h_posvect_id,
@@ -260,15 +252,65 @@ begin
     { specialize hx' i,
       have h_posvect_neg : ((pos_vector l x) • l) i = - l i :=
         pos_vector_neg_if_neg l x i (not_le.mp h_pos),
-      replace h_pos' : 0 ≥ x' (l i),
-      { rw [h_posvect_neg, x'.map_neg] at h_pos',
+      rw [h_posvect_neg] at *,
+      replace h_pos' : x' (l i) ≤ 0,
+      { rw [x'.map_neg] at h_pos',
         exact neg_nonneg.mp h_pos' },
-      rw h_posvect_neg at hx',
       refine or.inr ⟨h_pos', _⟩,
       simpa only [neg_le_neg_iff, add_monoid_hom.sub_apply, add_monoid_hom.map_neg, sub_nonpos]
         using hx' } }
 end
-#lint
+
+lemma nat.monotone_pow (k : ℕ) : monotone (λ n : ℕ, n ^ k) :=
+λ x y xy, pow_le_pow_of_le_left (nat.zero_le x) xy k
+
+lemma nat.strict_mono_pow {k : ℕ} (k0 : 0 < k) : strict_mono (λ n : ℕ, n ^ k) :=
+λ x y xy, pow_lt_pow_of_lt_left xy x.zero_le k0
+
+lemma nat.injective_pow {k : ℕ} (k0 : 0 < k) : function.injective (λ n : ℕ, n ^ k) :=
+injective_of_increasing (<) (<) _ (nat.strict_mono_pow k0)
+
+lemma int.nat_abs_eq_self (a : ℤ) : (a.nat_abs : ℤ) = a ↔ 0 ≤ a :=
+⟨λ h, by { rw ← h, exact int.coe_zero_le a.nat_abs }, λ h, int.nat_abs_of_nonneg h⟩
+
+lemma abs_add_eq_iff_abs_mul_eq {R : Type*} [linear_ordered_comm_ring R] {a b : R} :
+  abs (a + b) = abs a + abs b ↔ abs (a * b) = a * b :=
+begin
+  refine ⟨λ h, _, λ h, _⟩,
+  { simpa [abs_mul, eq_comm, add_pow_two, mul_assoc] using (congr_arg (λ x : R, x ^ 2) h) },
+  { rcases (mul_nonneg_iff.mp (abs_eq_self.mp h)) with ⟨a0, b0⟩ | ⟨a0, b0⟩,
+    { rw [abs_of_nonneg a0, abs_of_nonneg b0, abs_of_nonneg (add_nonneg a0 b0)] },
+    { rw [abs_of_nonpos a0, abs_of_nonpos b0, abs_of_nonpos (add_nonpos a0 b0), neg_add] } }
+end
+
+lemma nat_abs_add_eq_iff_nat_abs_mul_eq {a b : ℤ} :
+  int.nat_abs (a + b) = a.nat_abs + b.nat_abs ↔ ((a * b).nat_abs : ℤ) = a * b :=
+begin
+  rw [← int.abs_eq_nat_abs, ← abs_add_eq_iff_abs_mul_eq, int.abs_eq_nat_abs, int.abs_eq_nat_abs,
+    int.abs_eq_nat_abs],
+  norm_cast,
+end
+
+lemma nat_abs_add_eq_add_nat_abs_iff (a b : ℤ) :
+  int.nat_abs (a + b) = int.nat_abs a + int.nat_abs b ↔ (0 ≤ a ∧ 0 ≤ b ∨ a ≤ 0 ∧ b ≤ 0) :=
+nat_abs_add_eq_iff_nat_abs_mul_eq.trans ((int.nat_abs_eq_self _).trans mul_nonneg_iff)
+--  rw [nat_abs_add_eq_iff_nat_abs_mul_eq, int.nat_abs_eq_self],exact mul_nonneg_iff,
+
+lemma nat_smul_old (a b : ℕ) : a • (b : ℤ) = ((a • b : ℕ) : ℤ) :=
+by simp [(•)]
+
+/-
+lemma nat_smul (a b : ℕ) : a • (b : ℤ) = ((a • b : ℕ) : ℤ) :=
+by simp only [has_scalar.smul, nsmul_eq_mul, int.nat_cast_eq_coe_nat, int.coe_nat_mul]
+-/
+
+lemma nat_smul_nat_abs (a : ℕ) (b : ℤ) : a • b.nat_abs = (a • b).nat_abs :=
+begin
+  nth_rewrite 0 [← int.nat_abs_of_nat a],
+  rw [smul_eq_mul, ← int.nat_abs_mul, ← smul_eq_mul],
+  apply congr_arg,
+  simp [(•)],
+end
 
 /-- Lemma 9.7 of [Analytic]. -/
 lemma lem97' [fintype ι] (hΛ : finite_free Λ) (N : ℕ) (hN : 0 < N) (l : ι → Λ) :
@@ -278,20 +320,15 @@ lemma lem97' [fintype ι] (hΛ : finite_free Λ) (N : ℕ) (hN : 0 < N) (l : ι 
 begin
   obtain ⟨A, hA⟩ := lem97 hΛ N hN l,
   refine ⟨A, λ x, _⟩,
-  rcases hA x with ⟨x', mem_x', y, rfl, hx'⟩,
-  refine ⟨x', mem_x', y, rfl, λ i, _⟩,
-  specialize hx' i,
-  convert (abs_add_eq_add_abs_iff _ x').mpr _,
-  change abs ((N • y + x') (l i)) = abs (N * (y (l i))) + abs (x' (l i)),
-  rw abs_add_eq_add_abs_iff,
+  rcases hA x with ⟨x', mem_x', y, hy, hx'⟩,
   use [x', mem_x', y, hy],
   intro i,
   specialize hx' i,
-  zify,
-  simp only [← int.abs_eq_nat_abs, hy, add_monoid_hom.add_apply, add_monoid_hom.nat_smul_apply],
-  convert_to abs (N • y (l i) + x' (l i)) = abs (N • y (l i)) + abs (x' (l i)) using 2,
-  { rw [← nsmul_eq_smul, nsmul_eq_mul, int.nat_cast_eq_coe_nat, abs_mul, int.coe_nat_abs], },
-  apply (abs_add_eq_add_abs_iff (N • y (l i)) (x' (l i))).mpr,
-  rw [← sub_eq_iff_eq_add] at hy,
-  simpa only [hy, add_monoid_hom.nat_smul_apply, and_comm] using hx',
+  subst hy,
+  rw ← nat_abs_add_eq_add_nat_abs_iff at hx',
+  simp at ⊢ hx',
+  rw [add_comm, hx', add_comm],
+  simp only [add_left_inj, int.coe_nat_add],
+  rw ← nat_smul_nat_abs,
+  refl,
 end
