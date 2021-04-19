3f6a1d8b08e489edaed970cbfb08048bacae46cc
adomani <adomani@gmail.com>
2021-04-19 05:16:57 +0200

    separate lemmas

--- src/for_mathlib/nnreal.lean
+++ src/for_mathlib/nnreal.lean
@@ -1,25 +1,27 @@
 import data.real.nnreal
+import topology.instances.nnreal
 
 open_locale nnreal
 
-variables (r r' : ℝ≥0) [fact (0 < r)] [fact (0 < r')] [fact (r < r')] [fact (r' ≤ 1)]
-
 namespace nnreal
 
-lemma div_le_div_left_of_le {a b c : ℝ≥0} (b0 : 0 < b) (c0 : 0 < c) :
-  c ≤ b → a / b ≤ a / c :=
+variables {R : Type*} [linear_ordered_comm_group_with_zero R]
+
+
+lemma div_le_div_left_of_le {a b c : ℝ≥0} (b0 : 0 < b) (c0 : 0 < c) (cb : c ≤ b) :
+  a / b ≤ a / c :=
 begin
   by_cases a0 : a = 0,
-  { exact λ _, by rw [a0, zero_div, zero_div] },
-  { have ai : 0 < a := zero_lt_iff.mpr a0,
-    cases a with a ha,
-    cases b with b hb,
-    cases c with c hc,
-    have a00 : 0 < a := lt_of_le_of_ne ha (ne_of_lt ai),
-    intros cb,
-    erw [div_le_div_left a00 b0 c0],
-    exact cb }
+  { rw [a0, zero_div, zero_div] },
+  { cases a with a ha,
+    replace a0 : 0 < a := lt_of_le_of_ne ha (ne_of_lt (zero_lt_iff.mpr a0)),
+    exact (div_le_div_left a0 b0 c0).mpr cb }
 end
+
+lemma div_le_div_left {a b c : ℝ≥0} (a0 : 0 < a) (b0 : 0 < b) (c0 : 0 < c) :
+  a / b ≤ a / c ↔ c ≤ b :=
+by rw [nnreal.div_le_iff b0.ne.symm, div_mul_eq_mul_div, nnreal.le_div_iff_mul_le c0.ne.symm,
+  mul_le_mul_left a0]
 
 lemma pow_mono_decr_exp {a : ℝ≥0} (m n : ℕ) (mn : m ≤ n) (a1 : a ≤ 1) :
   a ^ n ≤ a ^ m :=
