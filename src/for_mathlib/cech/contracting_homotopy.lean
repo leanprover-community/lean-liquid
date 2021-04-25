56b65b997f019def0a0737aa2daa6e10c7beea6c
Adam Topaz <github@adamtopaz.com>
2021-04-25 11:16:03 -0600

    Kill sorry

--- src/for_mathlib/cech/contracting_homotopy.lean
+++ src/for_mathlib/cech/contracting_homotopy.lean
@@ -24,8 +24,28 @@ abbreviation contracting_homotopy (n : ℕ) :
   (conerve_complex f M).X (n+1) ⟶ (conerve_complex f M).X n :=
 M.map $ (cech_splitting f g splitting n).op
 
+section finset_sum_helpers
+
+lemma helper.sum_eq {n} {α : Type*} [add_comm_monoid α] (ι : fin (n+1) → α) :
+  (∑ (i : fin (n+1)), ι i) = ι 0 + ∑ i : fin n, ι i.succ :=
+fin.sum_univ_succ (λ (i : fin (n + 1)), ι i)
+
+lemma helper.sum_eq_zero_of_zeros {n} {α : Type*} [add_comm_monoid α] (ι : fin n → α) :
+  (∀ i, ι i = 0) → ∑ i, ι i = 0 := λ h, fintype.sum_eq_zero (λ (a : fin n), ι a) h
+
+lemma helper.op_comp {E : Type*} [category E] {a b c : E} (h : a ⟶ b) (l : b ⟶ c) :
+  (h ≫ l).op = l.op ≫ h.op := rfl
+
+lemma helper.op_id {E : Type*} [category E] {a : E} :
+  (𝟙 a).op = 𝟙 (opposite.op a) := rfl
+
+lemma helper.op_eq_id {E : Type*} [category E] {a : E} (h : a ⟶ a) :
+  h.op = 𝟙 _ ↔ h = 𝟙 _ := by sorry
+
+end finset_sum_helpers
+
 theorem is_contracting_homotopy (n : ℕ) :
-  (conerve_complex f M).d (n+1) (n+2) ≫ contracting_homotopy f M g splitting (n+1) -
+  (conerve_complex f M).d (n+1) (n+2) ≫ contracting_homotopy f M g splitting (n+1) +
   contracting_homotopy f M g splitting n ≫ (conerve_complex f M).d n (n+1) = 𝟙 _ :=
 begin
   delta conerve_complex,
@@ -36,7 +56,26 @@ begin
   swap, finish,
   dsimp only [cosimplicial_object.coboundary],
   simp only [preadditive.sum_comp, preadditive.comp_sum],
-  sorry
+  rw [helper.sum_eq, add_assoc, ← finset.sum_add_distrib],
+  rw ← add_zero (𝟙 _),
+  swap, apply_instance,
+  congr' 1,
+  { simp only [category_theory.category.comp_id,
+    fin.coe_zero,
+    one_gsmul,
+    category_theory.eq_to_hom_refl,
+    category_theory.functor.comp_map,
+    --category_theory.cech_obj_map,
+    category_theory.functor.right_op_map,
+    pow_zero],
+    delta contracting_homotopy,
+    rw ← M.map_comp,
+    erw ← M.map_id,
+    congr' 1,
+    rw ← helper.op_comp,
+    erw helper.op_eq_id,
+    erw cech_splitting_face_zero },
+  { sorry }
 end
 
 end cech
