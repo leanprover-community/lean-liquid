ffec2fa0be8356019317172d60b9965341297047
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-04-26 19:13:27 +0100

    more API

--- src/for_mathlib/grading.lean
+++ src/for_mathlib/grading.lean
@@ -4,18 +4,58 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard
 -/
 import algebra.direct_sum
-import algebra.monoid_algebra -- to check we can grade a monoid algebra
-import data.polynomial -- to check we can grade a polynomial ring
-import ring_theory.noetherian -- for the lemma we need for Gordan
---import for_mathlib.dfinsupp -- finsupp <-> dfinsupp
+import group_theory.subgroup
+
 /-!
 
 # Grading of a ring by a monoid
 
 A grading of a ring `R` by a monoid `M` is a decomposition R ≃ ⨁ Rₘ as an internal
-direct sum of subgroups indexed by `m`, satisfying 1 ∈ R₀ and RₘRₙ⊆R_{m+n}
+direct sum of subgroups indexed by `m`, satisfying `1 ∈ R₀` and `RₘRₙ⊆R_{m+n}`
 
 -/
+-- MOVE
+open_locale direct_sum
+
+-- should be in algebra.direct_sum
+lemma direct_sum.to_add_monoid_apply {ι : Type*} [decidable_eq ι]
+  {β : ι → Type*} [Π (i : ι), add_comm_monoid (β i)]
+  [ Π (i : ι) (x : β i), decidable (x ≠ 0)]
+  {γ : Type*} [add_comm_monoid γ]
+  (f : Π (i : ι), β i →+ γ) (b : ⨁ i, β i):
+  direct_sum.to_add_monoid f b = dfinsupp.sum b (λ i, f i) :=
+dfinsupp.sum_add_hom_apply _ _
+
+-- should be in dfinsupp
+namespace dfinsupp
+
+variables (ι : Type*) (β : ι → Type*) [decidable_eq ι] [Π (j : ι), has_zero (β j)]
+  (i : ι) (f : Π₀ i, β i)
+
+lemma eq_single_iff : (∀ ⦃j : ι⦄, j ≠ i → f j = 0) ↔ dfinsupp.single i (f i) = f :=
+⟨λ h, dfinsupp.ext $ λ j, begin
+  by_cases hj : j = i,
+  { subst hj,
+    apply single_eq_same },
+  { rw h hj,
+    exact single_eq_of_ne (ne.symm hj) },
+end,
+begin
+  rintro hf j h,
+  rw ← hf,
+  exact single_eq_of_ne h.symm,
+end⟩
+
+/-- A version of `dfinsupp.sum_single_index` which takes monoids homomorphisms. Useful for
+  sometimes avoiding `@sum_single_index _ _ _ _ …`. -/
+lemma add_monoid_hom_sum_single_index {ι : Type*} {β : ι → Type*} [decidable_eq ι]
+  [_inst_1 : Π (i : ι), add_monoid (β i)]
+  [_inst_2 : Π (i : ι) (x : β i), decidable (x ≠ 0)]
+  {γ : Type*} [add_comm_monoid γ] {i : ι} {b : β i} {h : Π (i : ι), β i →+ γ} :
+  (dfinsupp.single i b).sum (λ i, h i) = h i b :=
+sum_single_index (h i).map_zero
+
+end dfinsupp
 
 /-
 
@@ -51,9 +91,13 @@ lemma add_subgroup_is_internal.to_add_submonoid
     add_submonoid_is_internal (λ i, (A i).to_add_submonoid) :=
 iff.rfl
 
--- that's the end of Eric's PR but we seem to need more: we don't have the projections!
+-- that's the end of Eric's PR but we seem to need more: we don't have the projections
+-- as monoid homs
 
-variables {M : Type*} [decidable_eq ι] [add_comm_monoid M] (A : ι → Type*) [∀ i, add_comm_monoid (A i)]
+section unused_in_this_file
+
+variables {M : Type*} [decidable_eq ι] [add_comm_monoid M] (A : ι → Type*)
+  [∀ i, add_comm_monoid (A i)]
 
 def projection (j : ι) : (⨁ i, A i) →+ A j :=
 { to_fun := λ f, f j,
@@ -66,6 +110,8 @@ lemma projection_of_same (j : ι) (aj : A j) : projection A j (of (λ i, A i) j 
 lemma projection_of_ne {i j : ι} (h : i ≠ j) (ai : A i) :
   projection A j (of (λ i, A i) i ai) = 0 :=
 dfinsupp.single_eq_of_ne h
+
+end unused_in_this_file
 
 end direct_sum
 
@@ -109,10 +155,11 @@ noncomputable def decomposition (g : monoid_grading M R) :
   R ≃+ ⨁ m, g.pieces m :=
 (add_equiv.of_bijective _ g.is_direct_sum).symm
 
-/-- If r : R and R is graded by M then `piece r m` is the element rₘ of Rₘ such that ∑ₘ rₘ = r.  -/
-@[to_additive "If r : R and R is graded by M then `piece r m` is the element rₘ of Rₘ such that ∑ₘ rₘ = r."]
-noncomputable def piece (g : monoid_grading M R) (r : R) (m : M) : R :=
-direct_sum.projection (λ m, g.pieces m) m (decomposition g r)
+/-- Decomposing `r` into `(rₘ)ₘ : ⨁ m, g.pieces m` and then adding the pieces gives `r` again. -/
+lemma sum_decomposition (g : monoid_grading M R) (r : R) :
+  (direct_sum.to_add_monoid (λ m, (g.pieces m).subtype) : (⨁ m, g.pieces m) →+ R)
+    (g.decomposition r) = r :=
+g.decomposition.symm_apply_apply r
 
 /-- If `r ∈ Rₘ` then the element of `R` which is `r` at `m` and zero elsewhere, is `r`. -/
 @[to_additive]
@@ -126,7 +173,7 @@ begin
   refl,
 end
 
-/-- If `r ∈ Rₘ` then `r` is the element of `⨁Rₘ` which is `r` at `m` and 0 elsewhere. -/
+/-- If `r ∈ Rₘ` then `r` is the element of `⨁Rₘ` which is `r` at `m` and `0` elsewhere. -/
 @[to_additive]
 lemma eq_decomposition_of_mem_piece'' {g : monoid_grading M R} {r : R} {m : M}
   (hr : r ∈ g.pieces m) :
@@ -152,15 +199,46 @@ begin
   refl,
 end
 
+/-
+
+## A note on decomposition
+
+If `g : monoid_grading M R` and `r : R` then its `m`th component `rₘ` is `g.decomposition r m`
+
+-/
+
+-- let's test the API for grading
+lemma mem_piece_iff_single_support {R : Type*} [semiring R] {M : Type*} [monoid M]
+  [decidable_eq M] (g : monoid_grading M R) (r : R) (m : M) :
+  r ∈ g.pieces m ↔ ∀ n, n ≠ m → g.decomposition r n = 0 :=
+begin
+  split,
+  { intros hrm n hn,
+    rw eq_decomposition_of_mem_piece'' hrm,
+    exact direct_sum.projection_of_ne _ hn.symm _ },
+  { intro h,
+    rw dfinsupp.eq_single_iff at h,
+    -- can't use `classical` because `decidable_eq M` gets lost
+    letI : ∀ n, decidable_eq (g.pieces n) := λ _, classical.dec_eq _,
+    rw [← g.sum_decomposition r, direct_sum.to_add_monoid_apply, ← h,
+        dfinsupp.add_monoid_hom_sum_single_index],
+    exact (g.decomposition r m).2 }
+end
+
 end graded_pieces
 
-/-! ## rings are graded by subgroups -/
+/-!
+
+## rings are graded by subgroups
+
+If a ring (or even an add_comm_group) is an internal direct sum of add_submonoids
+then they're all add_subgroups.
+
+-/
 
 variables {M : Type*} [monoid M] [decidable_eq M] {R : Type*} [ring R]
 
-example (A : Type) [add_comm_group A] (x y : A) : x + y = 0 → -x = y := neg_eq_of_add_eq_zero
-
--- to_additive can't handle the add_subgroup :-(
+-- to_additive can't handle this for some reason so I need to prove it twice
 /-- A monoid_grading on a ring be add_submonoids is in fact by add_subgroups -/
 def add_subgroup_pieces (g : monoid_grading M R) (m : M) : add_subgroup R :=
 { neg_mem' := λ x hx, begin
@@ -199,159 +277,5 @@ def add_subgroup_pieces {M : Type*} [add_monoid M] [decidable_eq M]
   ..g.pieces m}
 
 attribute [to_additive monoid_grading.add_subgroup_pieces] add_subgroup_pieces
-
-end add_monoid_grading
-
-
-open_locale direct_sum
--- should be in algebra.direct_sum
-lemma direct_sum.to_add_monoid_apply {ι : Type*} [decidable_eq ι]
-  {β : ι → Type*} [Π (i : ι), add_comm_monoid (β i)]
-  [ Π (i : ι) (x : β i), decidable (x ≠ 0)]
-  {γ : Type*} [add_comm_monoid γ]
-  (f : Π (i : ι), β i →+ γ) (b : ⨁ i, β i):
-  direct_sum.to_add_monoid f b = dfinsupp.sum b (λ i, f i) :=
-dfinsupp.sum_add_hom_apply _ _
-
-/-!
-
-## test : grading a polynomial ring
-
--/
-
-
-section polynomials
-
-open polynomial
-
-open_locale direct_sum
-
-noncomputable theory
-
-abbreviation monomial.submonoid (R : Type) [semiring R] (i : ℕ) : add_submonoid (polynomial R) :=
-(monomial i : R →ₗ polynomial R).to_add_monoid_hom.mrange
-
-abbreviation monomial.to_submonoid (R : Type) [semiring R] (i : ℕ) : R →+ monomial.submonoid R i :=
-(monomial i : R →ₗ polynomial R).to_add_monoid_hom.mrange_restrict
-
-def polynomial_equiv (R : Type) [semiring R] :
-  (⨁ i, monomial.submonoid R i) ≃+ polynomial R :=
-add_monoid_hom.to_add_equiv
-  (direct_sum.to_add_monoid $ λ i,
-    (monomial.submonoid R i).subtype)
-  (finsupp.lift_add_hom $ λ n,
-    (direct_sum.of (λ i, monomial.submonoid R i) n).comp (monomial.to_submonoid R n))
-  (begin
-    apply direct_sum.add_hom_ext',
-    rintro i,
-    apply add_monoid_hom.ext,
-    rintro ⟨x, r, rfl⟩,
-    dsimp,
-    simp [monomial],
-    refl,
-  end)
-  (begin
-    ext i r : 2,
-    dsimp,
-    simp [monomial],
-  end).
-
-end polynomials
-
-/-!
-
-## test : grading a monoid algebra
-
--/
-
-namespace add_monoid_algebra
-
--- second test case: grading add_monoid_algebras.
-
-variables (R ι : Type) [semiring R] (M : Type) [add_monoid M] [decidable_eq M] [decidable_eq ι]
-
-open polynomial
-
-open_locale direct_sum
-
-noncomputable theory
-
-
-/-- `monomial s a` is the monomial `a * X^s` -/
-noncomputable def Mm (i : M) : R →ₗ[R] add_monoid_algebra R M :=
-finsupp.lsingle i
-
-abbreviation monomial.submonoid (i : M) : add_submonoid (add_monoid_algebra R M) :=
-(Mm R M i : R →ₗ add_monoid_algebra R M).to_add_monoid_hom.mrange
-
-abbreviation monomial.to_submonoid (i : M) : R →+ monomial.submonoid R M i :=
-(Mm R M i : R →ₗ add_monoid_algebra R M).to_add_monoid_hom.mrange_restrict
-
-def polynomial_equiv :
-  (⨁ i, monomial.submonoid R M i) ≃+ add_monoid_algebra R M :=
-add_monoid_hom.to_add_equiv
-  (direct_sum.to_add_monoid $ λ i,
-    (monomial.submonoid R M i).subtype)
-  (finsupp.lift_add_hom $ λ n,
-    (direct_sum.of (λ i, monomial.submonoid R M i) n).comp (monomial.to_submonoid R M n))
-  (begin
-    ext i ⟨x, r, rfl⟩ : 2,
-    simpa [Mm],
-  end)
-  (begin
-    ext i r : 2,
-    simp [Mm],
-  end).
-
-end add_monoid_algebra
-
-/-
-
-## The theorem we need for Gordan and hence LTE
-
-If A is ℤ-graded and Noetherian then A_{≥0} is a finitely-generated A₀-algebra
-
--/
-
-namespace add_monoid_grading
-
-def zero_piece_subsemiring (A : Type*) [semiring A] (M : Type*) [add_monoid M]
-  [decidable_eq M] (g : add_monoid_grading M A) : subsemiring A :=
-{
-  one_mem' := g.grading_one,
-  mul_mem' := λ r s, begin
-    convert g.grading_mul 0 0 r s,
-    rw add_zero,
-    refl,
-  end,
-  ..g.pieces 0
-}
-
-def zero_piece_subring (A : Type*) [ring A] (M : Type*) [add_monoid M]
-  [decidable_eq M] (g : add_monoid_grading M A) : subring A :=
-{
-  one_mem' := g.grading_one,
-  mul_mem' := λ r s, begin
-    convert g.grading_mul 0 0 r s,
-    rw add_zero,
-    refl,
-  end,
-  ..g.add_subgroup_pieces 0
-}
-
---theorem zero_piece_subring_eq_decomposition_support_zero :
-
-
-def nonneg_piece_subring_of_int_grading (A : Type*) [ring A]
-  (g : add_monoid_grading ℤ A) : subring A :=
-{ carrier := {a : A | ∀ n : ℤ, n < 0 → g.decomposition a n = 0 },
-  zero_mem' := sorry,
-  add_mem' := sorry,
-  neg_mem' := sorry,
-  one_mem' := sorry,
-  mul_mem' := sorry, }
-
---theorem nonnegative_subalgebra_fg_over_zero_subalgebra_of_int_grading_of_noeth
---  (A : Type*) [comm_ring A] [is_noetherian_ring A] (g : add_monoid_grading ℤ A) :
 
 end add_monoid_grading
