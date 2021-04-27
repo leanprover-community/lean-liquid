9104dd3f755ab38c4f104c2db2442f403c21b614
Adam Topaz <github@adamtopaz.com>
2021-04-27 14:58:34 -0600

    Upgrade to locally constant

--- src/for_mathlib/Profinite/locally_constant.lean
+++ src/for_mathlib/Profinite/locally_constant.lean
@@ -13,7 +13,7 @@ open opposite locally_constant
 variables (X : Profinite) (M : NormedGroup)
 
 lemma locally_constant_factors_fun (f : locally_constant X M) :
-  ∃ (I : X.clopen_cover) (g : I → M), g ∘ I.proj = f :=
+  ∃ (I : X.clopen_cover) (g : locally_constant I M), g ∘ I.proj = f :=
 begin
   let Us : set (set X) := {U | (∃ m : M, U = f ⁻¹' {m}) ∧ U.nonempty},
   have Us_open : ∀ U ∈ Us, is_open U,
@@ -30,10 +30,11 @@ begin
     rcases h with ⟨x,h1 : f x = _, h2 : f x = _⟩,
     rw [← h1, h2] },
   let I := Profinite.clopen_cover.of_open_cover Us Us_open Us_nonempty Us_cover Us_disjoint,
-  use I,
-  let g : I → M := λ i, f (classical.some (I.nonempty i)),
-  use g,
-  ext,
+  let g_fun : I → M := λ i, f (classical.some (I.nonempty i)),
+  have : is_locally_constant g_fun := λ S, is_open_discrete _,
+  let g : locally_constant I M := ⟨g_fun, this⟩,
+  refine ⟨I,g,_⟩,
+  ext x,
   let i : ↥I := ⟨f ⁻¹' { f x }, ⟨⟨f x, rfl⟩, ⟨x, by tauto⟩⟩⟩,
   have : I.proj x = i,
   { symmetry,
@@ -43,9 +44,9 @@ begin
   rw this,
   have h1 := classical.some_spec (I.nonempty i),
   dsimp at h1,
-  rw set.mem_preimage at h1,
-  simp only [set.mem_singleton_iff] at h1,
+  simp only [set.mem_preimage, set.mem_singleton_iff] at h1,
   rw ← h1,
+  refl,
 end
 
 end NormedGroup
