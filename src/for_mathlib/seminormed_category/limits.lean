e1675b6e5573b503d81aca7a4e8904c272721084
Adam Topaz <github@adamtopaz.com>
2021-04-29 13:36:51 -0600

    Start on api for bounded_cone

---
+++ src/for_mathlib/seminormed_category/limits.lean
@@ -0,0 +1,51 @@
+import data.real.basic
+import for_mathlib.seminormed_category.basic
+import category_theory.limits.has_limits
+
+namespace category_theory
+
+namespace limits
+
+universes v u
+
+variables {C : Type u} [category.{v} C] [semi_normed_category C]
+variables {J : Type v} [small_category J]
+
+namespace cone
+
+def bounded_by {F : J ⥤ C} (S : cone F) (c : ℝ) : Prop :=
+∀ j : J, ∥ S.π.app j ∥ ≤ c
+
+end cone
+
+structure bounded_cone (F : J ⥤ C) extends cone F :=
+(cone : cone F)
+(bounded : ∃ c, cone.bounded_by c)
+
+namespace bounded_cone
+
+noncomputable instance {F : J ⥤ C} : has_norm (bounded_cone F) := has_norm.mk $
+λ S, Inf { a | S.cone.bounded_by a }
+
+lemma norm_le {F : J ⥤ C} (S : bounded_cone F) (j : J) :
+  ∥ S.cone.π.app j ∥ ≤ ∥ S ∥ := real.lb_le_Inf _ S.bounded (λ c hc, hc j)
+
+lemma norm_nonneg {F : J ⥤ C} (S : bounded_cone F) : 0 ≤ ∥ S ∥ :=
+begin
+  by_cases h : nonempty J,
+  { rcases h with ⟨j⟩,
+    refine real.lb_le_Inf _ S.bounded (λ c hc, le_trans (norm_nonneg _) (hc j)) },
+  suffices : ∥ S ∥ = 0, by simp [this],
+  suffices : {a | S.cone.bounded_by a} = set.univ,
+  { change Inf _ = _,
+    simp [this, real.Inf_def, real.Sup_univ] },
+  rw set.eq_univ_iff_forall,
+  intros c j,
+  exact false.elim (h ⟨j⟩),
+end
+
+end bounded_cone
+
+end limits
+
+end category_theory
