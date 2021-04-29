88c7ed0c645ec90f396f60e7b1cf1020c1b7e5b2
Adam Topaz <github@adamtopaz.com>
2021-04-29 16:01:44 -0600

    Add cocone

--- src/for_mathlib/seminormed_category/limits.lean
+++ src/for_mathlib/seminormed_category/limits.lean
@@ -20,9 +20,20 @@ def bounded_by {F : J ⥤ C} (S : cone F) (c : ℝ) : Prop :=
 
 end cone
 
+namespace cocone
+
+def bounded_by {F : J ⥤ C} (S : cocone F) (c : ℝ) : Prop :=
+0 ≤ c ∧ ∀ j : J, ∥ S.ι.app j ∥ ≤ c
+
+end cocone
+
 structure bounded_cone (F : J ⥤ C) :=
 (cone : cone F)
 (bounded : ∃ c, 0 ≤ c ∧ cone.bounded_by c)
+
+structure bounded_cocone (F : J ⥤ C) :=
+(cocone : cocone F)
+(bounded : ∃ c, 0 ≤ c ∧ cocone.bounded_by c)
 
 namespace bounded_cone
 
@@ -73,6 +84,56 @@ end
 
 end bounded_cone
 
+namespace bounded_cocone
+
+noncomputable instance {F : J ⥤ C} : has_norm (bounded_cocone F) := has_norm.mk $
+λ S, Inf { a | 0 ≤ a ∧ S.cocone.bounded_by a }
+
+lemma norm_le {F : J ⥤ C} (S : bounded_cocone F) (j : J) :
+  ∥ S.cocone.ι.app j ∥ ≤ ∥ S ∥ := real.lb_le_Inf _ S.bounded (λ c hc, hc.2.2 _)
+
+lemma norm_le_of_bounded_by {F : J ⥤ C} (S : bounded_cocone F) (a : ℝ)
+  (h : S.cocone.bounded_by a) : ∥ S ∥ ≤ a :=
+begin
+  apply real.Inf_le,
+  use 0,
+  intros c hc, exact hc.1,
+  refine ⟨h.1, h⟩,
+end
+
+lemma norm_nonneg {F : J ⥤ C} (S : bounded_cocone F) : 0 ≤ ∥ S ∥ :=
+begin
+  apply real.lb_le_Inf,
+  exact S.bounded,
+  intros c hc, exact hc.1,
+end
+
+structure is_colimit {F : J ⥤ C} (t : bounded_cocone F) :=
+(desc  : Π (s : bounded_cocone F), t.cocone.X ⟶ s.cocone.X)
+(fac'  : ∀ (s : bounded_cocone F) (j : J), t.cocone.ι.app j ≫ desc s = s.cocone.ι.app j . obviously)
+(uniq' : ∀ (s : bounded_cocone F) (m : t.cocone.X ⟶ s.cocone.X)
+  (w : ∀ j : J, t.cocone.ι.app j ≫ m = s.cocone.ι.app j), m = desc s . obviously)
+
+restate_axiom is_colimit.fac'
+attribute [simp,reassoc] is_colimit.fac
+restate_axiom is_colimit.uniq'
+
+lemma desc_norm_le {F : J ⥤ C} {t s : bounded_cocone F}
+  (h : t.is_colimit) : ∥ s ∥ ≤ ∥ h.desc s ∥ * ∥ t ∥ :=
+begin
+  refine real.Inf_le _ ⟨0,λ c hc, hc.1⟩ ⟨mul_nonneg (_root_.norm_nonneg _) (norm_nonneg _),_⟩,
+  refine ⟨mul_nonneg (_root_.norm_nonneg _) (norm_nonneg _),λ j, _⟩,
+  rw [← h.fac],
+  refine le_trans (semi_normed_category.norm_comp _ _ _ _ _) _,
+  suffices : ∥ t.cocone.ι.app j ∥ ≤ ∥ t ∥,
+  { have hh : 0 ≤ ∥ h.desc s ∥ := _root_.norm_nonneg _,
+    rw mul_comm,
+    exact mul_le_mul_of_nonneg_left this hh },
+  apply norm_le,
+end
+
+end bounded_cocone
+
 end limits
 
 namespace functor
@@ -80,6 +141,15 @@ namespace functor
 def map_bounded_cone {F : J ⥤ C} (S : limits.bounded_cone F) :
   limits.bounded_cone (F ⋙ G) :=
 { cone := G.map_cone S.cone,
+  bounded := begin
+    use ∥ S ∥,
+    refine ⟨S.norm_nonneg, S.norm_nonneg, _⟩,
+    simp [S.norm_le],
+  end }
+
+def map_bounded_cocone {F : J ⥤ C} (S : limits.bounded_cocone F) :
+  limits.bounded_cocone (F ⋙ G) :=
+{ cocone := G.map_cocone S.cocone,
   bounded := begin
     use ∥ S ∥,
     refine ⟨S.norm_nonneg, S.norm_nonneg, _⟩,
