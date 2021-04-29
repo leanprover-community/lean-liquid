b86ad91dc1ddc5deb6d26ebd5acb9210f3836e25
Adam Topaz <github@adamtopaz.com>
2021-04-29 13:49:43 -0600

    Add definition of bounded additive functor

---
+++ src/for_mathlib/seminormed_category/functor.lean
@@ -0,0 +1,15 @@
+import data.real.basic
+import for_mathlib.seminormed_category.basic
+import category_theory.preadditive.additive_functor
+
+namespace category_theory
+
+namespace functor
+
+class bounded {C D : Type*} [category C] [category D] [semi_normed_category C] [semi_normed_category D]
+  (F : C ⥤ D) extends functor.additive F : Prop :=
+(bounded : ∃ c : ℝ, ∀ {X Y : C} (f : X ⟶ Y), ∥ F.map f ∥ ≤ c * ∥ f ∥)
+
+end functor
+
+end category_theory
