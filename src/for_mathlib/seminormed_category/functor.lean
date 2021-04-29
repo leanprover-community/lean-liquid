dc1a868f5d1f0e37a1e31a476478d1f140c5ed59
Adam Topaz <github@adamtopaz.com>
2021-04-29 14:07:25 -0600

    More api

--- src/for_mathlib/seminormed_category/functor.lean
+++ src/for_mathlib/seminormed_category/functor.lean
@@ -6,9 +6,24 @@ namespace category_theory
 
 namespace functor
 
+def bounded_by {C D : Type*} [category C] [category D] [semi_normed_category C]
+  [semi_normed_category D] (F : C ⥤ D) (c : ℝ) : Prop :=
+  ∀ {X Y : C} (f : X ⟶ Y), ∥ F.map f ∥ ≤ c * ∥ f ∥
+
 class bounded_additive {C D : Type*} [category C] [category D] [semi_normed_category C]
   [semi_normed_category D] (F : C ⥤ D) extends functor.additive F : Prop :=
-(bounded : ∃ c : ℝ, ∀ {X Y : C} (f : X ⟶ Y), ∥ F.map f ∥ ≤ c * ∥ f ∥)
+(bounded [] : ∃ c : ℝ, F.bounded_by c)
+
+noncomputable def norm {C D : Type*} [category C] [category D] [semi_normed_category C]
+  [semi_normed_category D] (F : C ⥤ D) [bounded_additive F] : ℝ :=
+Inf {a | F.bounded_by a}
+
+lemma norm_nonneg {C D : Type*} [category C] [category D] [semi_normed_category C]
+  [semi_normed_category D] (F : C ⥤ D) [bounded_additive F] : 0 ≤ F.norm := sorry
+
+lemma le_norm {C D : Type*} [category C] [category D] [semi_normed_category C]
+  [semi_normed_category D] {X Y : C} (F : C ⥤ D) [bounded_additive F] (f : X ⟶ Y) :
+  ∥ F.map f ∥ ≤ F.norm * ∥ f ∥ := sorry
 
 end functor
 
--- src/for_mathlib/seminormed_category/limits.lean
+++ src/for_mathlib/seminormed_category/limits.lean
@@ -1,15 +1,17 @@
 import data.real.basic
 import for_mathlib.seminormed_category.basic
 import category_theory.limits.has_limits
+import for_mathlib.seminormed_category.functor
 
 namespace category_theory
 
-namespace limits
-
 universes v u
-
 variables {C : Type u} [category.{v} C] [semi_normed_category C]
+variables {D : Type*} [category.{v} D] [semi_normed_category D] (G : C ⥤ D)
+  [functor.bounded_additive G]
 variables {J : Type v} [small_category J]
+
+namespace limits
 
 namespace cone
 
@@ -18,7 +20,7 @@ def bounded_by {F : J ⥤ C} (S : cone F) (c : ℝ) : Prop :=
 
 end cone
 
-structure bounded_cone (F : J ⥤ C) extends cone F :=
+structure bounded_cone (F : J ⥤ C) :=
 (cone : cone F)
 (bounded : ∃ c, cone.bounded_by c)
 
@@ -47,5 +49,21 @@ end
 end bounded_cone
 
 end limits
+
+namespace functor
+
+def map_bounded_cone {F : J ⥤ C} (S : limits.bounded_cone F) :
+  limits.bounded_cone (F ⋙ G) :=
+{ cone := G.map_cone S.cone,
+  bounded := begin
+    use G.norm * ∥ S ∥,
+    intros j,
+    refine le_trans (G.le_norm _) _,
+    have h : 0 ≤ G.norm := G.norm_nonneg,
+    have h2 : ∥ S.cone.π.app j ∥ ≤ ∥ S ∥ := S.norm_le _,
+    exact mul_le_mul_of_nonneg_left h2 h
+  end }
+
+end functor
 
 end category_theory
