ed2808e29c21459f81d29f2413b6750eabc1587c
Adam Topaz <github@adamtopaz.com>
2021-05-03 20:06:33 -0600

    Add docstrings

--- src/for_mathlib/Profinite/locally_constant.lean
+++ src/for_mathlib/Profinite/locally_constant.lean
@@ -9,8 +9,9 @@ open opposite locally_constant category_theory
 
 universes u
 
-variables (M : NormedGroup.{u}) (X : Profinite.{u})
+variables {M : NormedGroup.{u}} {X : Profinite.{u}}
 
+/-- Construct a discrete quotient from a locally constant function. -/
 def lc_to_discrete_quotient (f : locally_constant X M) :
   discrete_quotient X :=
 { rel := λ a b, f a = f b,
@@ -18,13 +19,14 @@ def lc_to_discrete_quotient (f : locally_constant X M) :
   clopen := λ x, ⟨is_locally_constant _ _,
     ⟨by {erw ← set.preimage_compl, apply is_locally_constant _ _}⟩⟩ }
 
+/-- Construct a locally constant function on the discrete quotient associated to `f`. -/
 def lc_desc (f : locally_constant X M) :
-  locally_constant (lc_to_discrete_quotient _ _ f) M :=
+  locally_constant (lc_to_discrete_quotient f) M :=
 { to_fun := λ i, quotient.lift_on' i f (by tauto),
   is_locally_constant := λ U, is_open_discrete _ }
 
-lemma lc_factors (f : locally_constant X M) : (lc_desc _ _ f) ∘
-  (lc_to_discrete_quotient _ _ f).proj = f := rfl
+lemma lc_factors (f : locally_constant X M) : (lc_desc f) ∘
+  (lc_to_discrete_quotient f).proj = f := rfl
 
 abbreviation diagram : NormedGroup ⥤ (discrete_quotient X)ᵒᵖ ⥤ NormedGroup :=
 LocallyConstant ⋙ (whiskering_left _ _ _).obj (X.diagram ⋙ Fintype_to_Profinite).op
