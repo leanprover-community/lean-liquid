4e58a4e76e0dfb6b250383688d633ab3f956ea4d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-05-10 00:29:07 +0100

    (int_grading_lemma): more progress

--- src/for_mathlib/int_grading_lemma.lean
+++ src/for_mathlib/int_grading_lemma.lean
@@ -37,9 +37,13 @@ begin
   exact not_le.mp hn,
 end
 
--- Brasca lemma, PRed in #7445
-lemma ft_iff_fg {R : Type*} {M : Type*} [comm_ring R] [add_comm_monoid M] [nontrivial R] :
-  add_monoid.fg M ↔ algebra.finite_type R (add_monoid_algebra R M) := sorry
+
+lemma exist_fg_gens {R : Type*} [ring R] {M : Type*} [add_comm_group M] [module R M]
+  {S : set M} (hS : (submodule.span R S).fg) :
+  ∃ T : finset M, (T : set M) ⊆ S ∧ submodule.span R (T : set M)= submodule.span R S :=
+begin
+  sorry
+end
 
 theorem nonnegative_subalgebra_fg_over_zero_subalgebra_of_int_grading_of_noeth
   (R : Type*) [comm_ring R] [is_noetherian_ring R] (Gᵢ : ℤ → add_subgroup R)
@@ -48,10 +52,59 @@ theorem nonnegative_subalgebra_fg_over_zero_subalgebra_of_int_grading_of_noeth
 (direct_sum.has_add_subgroup_decomposition.nonneg_piece_subring_of_int_grading.algebra R Gᵢ)
 :=
 begin
-  -- First prove Rₐ is a Noetherian R₀-module (see grading_zero_subring file)
-  -- then run through proof in pdf
+  -- see pdf for proof details
+  haveI h : ∀ a : ℤ, is_noetherian (Gᵢ 0) (Gᵢ a) :=
+    component_submodule.component_submodule_noetherian Gᵢ,
+  let S := ⋃ (n > 0), (Gᵢ n : set R),
+  let I : submodule R R := submodule.span R S,
+  have hI : I.fg := is_noetherian.noetherian I,
+  rcases exist_fg_gens hI with ⟨T, hTS, (heq : submodule.span R (T : set R) = I)⟩,
+  -- T is finite so it contained in `⋃_{0<n≤N} R_n` for some large `N`
+  -- this could be abstracted out
+  have HN : ∃ N : ℤ, (T : set R) ⊆ ⋃ n ∈ set.Ioc 0 N, (Gᵢ n : set R),
+  { -- proof by induction on size of T
+    classical, revert hTS, apply finset.induction_on T,
+    { intros, exact ⟨37, by simp⟩ },
+    { rintros a s - hS has,
+      cases hS (set.subset.trans (finset.subset_insert a s) has) with N hN,
+      have haS : a ∈ S := has (finset.mem_insert_self _ _),
+      replace haS := set.mem_bUnion_iff.1 haS, -- why rw no work?
+      rcases haS with ⟨M, (hM : 1 ≤ M), haM⟩,
+      use max M N,
+      intros x hx,
+      replace hx := finset.mem_insert.1 hx,
+      rcases hx with (rfl | hxs),
+      { rw set.mem_bUnion_iff,
+        use M,
+        refine ⟨_, haM⟩,
+        -- obviously
+        simp, split, linarith, left, refl },
+      { specialize hN hxs,
+        rw set.mem_bUnion_iff at ⊢ hN,
+        rcases hN with ⟨A, hA, h⟩,
+        refine ⟨A, _, h⟩,
+        rw [set.mem_Ioc] at ⊢ hA,
+        cases hA with hA1 hA2,
+        exact ⟨hA1, hA2.trans (le_max_right _ _)⟩ } } },
+  -- generating set is T union generators of Gᵢ n for 0≤n≤N
+  -- will need finset.bUnion (union of finsets over a finset)
+  choose fgset fgpf using λ z, is_noetherian.noetherian (⊤ : submodule (Gᵢ 0) (Gᵢ z)),
+  have nonneg_in : ∀ z : ℤ, 0 ≤ z → (Gᵢ z : set R) ⊆ nonneg_piece_subring_of_int_grading Gᵢ,
+  { rintros z hz x (hxz : x ∈ Gᵢ z) a (ha : ¬ (0 ≤ a)),
+    rw mem_piece_iff_single_support at hxz,
+    apply hxz,
+    rintro rfl,
+    exact ha hz },
+  have S_in_nonneg : S ⊆ nonneg_piece_subring_of_int_grading Gᵢ,
+  { intros s hsS,
+    replace hsS := set.mem_bUnion_iff.1 hsS,
+    rcases hsS with ⟨z, (hz : 1 ≤ z), hsz⟩,
+    exact nonneg_in z (zero_le_one.trans hz) hsz },
+  -- want finset ↥(nonneg_piece_subring_of_int_grading Gᵢ)
   sorry
 end
+
+-- note : we now have `add_monoid_algebra.finite_type_iff_fg`
 
 end has_add_subgroup_decomposition
 
