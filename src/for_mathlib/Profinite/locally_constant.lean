1a663dbde396ebc0701c15ff912e45e62b17c7f1
Scott Morrison <scott.morrison@gmail.com>
2021-05-15 19:04:03 +1000

    cleanup

--- src/for_mathlib/Profinite/locally_constant.lean
+++ src/for_mathlib/Profinite/locally_constant.lean
@@ -1,29 +1,26 @@
-import for_mathlib.Profinite.functorial_limit
-import locally_constant.NormedGroup
+import topology.discrete_quotient
+import topology.category.Profinite
+import normed_group.NormedGroup
 
-noncomputable theory
-
-namespace NormedGroup
-
-open opposite locally_constant category_theory
+namespace locally_constant
 
 universes u
 
 variables {M : NormedGroup.{u}} {X : Profinite.{u}}
 
-/-- Construct a disccrete quotient from a locally constant function. -/
-def lc_to_discrete_quotient (f : locally_constant X M) : discrete_quotient X :=
+/-- Construct a discrete quotient from a locally constant function. -/
+def to_discrete_quotient (f : locally_constant X M) : discrete_quotient X :=
 { rel := λ a b, f a = f b,
   equiv := ⟨by tauto, by tauto, λ a b c h1 h2, by rw [h1, h2]⟩,
   clopen := λ x, ⟨is_locally_constant _ _,
     ⟨by {erw ← set.preimage_compl, apply is_locally_constant _ _ }⟩⟩ }
 
-/-- Construct a locally constant function on the discrete quotient assocaited to `f`. -/
-def lc_desc (f : locally_constant X M) : locally_constant (lc_to_discrete_quotient f) M :=
+/-- Construct a locally constant function on the discrete quotient associated to `f`. -/
+def desc (f : locally_constant X M) : locally_constant (f.to_discrete_quotient) M :=
 { to_fun := λ i, quotient.lift_on' i f (by tauto),
   is_locally_constant := λ U, is_open_discrete _ }
 
-lemma lc_factors (f : locally_constant X M) : (lc_desc f) ∘ (lc_to_discrete_quotient f).proj = f :=
+lemma factors (f : locally_constant X M) : f.desc ∘ f.to_discrete_quotient.proj = f :=
 rfl
 
-end NormedGroup
+end locally_constant
