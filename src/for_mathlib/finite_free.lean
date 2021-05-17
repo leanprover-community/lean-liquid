700922204489f19a8ae4772bb5f76339be00b203
Scott Morrison <scott.morrison@gmail.com>
2021-05-17 14:33:40 +1000

    two finite free sorries

--- src/for_mathlib/finite_free.lean
+++ src/for_mathlib/finite_free.lean
@@ -147,27 +147,38 @@ end
 /-- The rank of a finite free abelian group. -/
 noncomputable def rank (ha : finite_free A) : ℕ := fintype.card ha.basis_type
 
+-- move?
 noncomputable
 def equiv_fin {ι : Type*} [fintype ι] (b : _root_.basis ι ℤ A) :
-  A ≃ₗ[ℤ] (fin $ fintype.card ι) → ℤ :=
-b.repr.trans $ (finsupp.lcongr (fintype.equiv_fin ι) (linear_equiv.refl ℤ ℤ)).trans $
-  finsupp.linear_equiv_fun_on_fintype ℤ
+  A ≃ₗ[ℤ] fin (fintype.card ι) → ℤ :=
+(b.reindex (fintype.equiv_fin ι)).equiv_fun
 
 lemma rank_eq {ι : Type*} [fintype ι] (b : _root_.basis ι ℤ A) (ha : finite_free A) :
   ha.rank = fintype.card ι :=
 eq_of_fin_equiv ℤ $ (equiv_fin ha.basis).symm.trans (equiv_fin b)
 
+noncomputable
+def equiv_fin_rank (ha : finite_free A) :
+  A ≃ₗ[ℤ] fin (ha.rank) → ℤ :=
+equiv_fin (ha.basis)
+
 variable {ha}
 
 /-- A rank zero abelian group has at most one element (yeah I know...). -/
-lemma rank_zero (h0 : ha.rank = 0) : subsingleton A := subsingleton.intro
+lemma rank_zero (h0 : ha.rank = 0) : subsingleton A :=
 begin
-  sorry
+  apply (ha.equiv_fin_rank).to_equiv.subsingleton_iff.mpr,
+  rw h0,
+  apply_instance,
 end
 
 lemma rank_dual (ha : finite_free A) : ha.dual.rank = ha.rank :=
 begin
-  sorry
+  have d := ha.dual,
+  rcases ha with ⟨ι, hι, ⟨b⟩⟩,
+  resetI, classical,
+  exact (rank_eq (b.dual_basis.map add_monoid_hom_lequiv_linear_map.symm) d).trans
+    (rank_eq b _).symm,
 end
 
 lemma congr_iso {B : Type} [add_comm_group B] (hab : A ≃+ B) (ha : finite_free A) :
