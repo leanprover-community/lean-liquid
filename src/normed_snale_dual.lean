560a3313015a6ba28d074c3b82f205fd01a88423
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-05-18 14:10:13 +0200

    typo

--- src/normed_snale_dual.lean
+++ src/normed_snale_dual.lean
@@ -16,15 +16,15 @@ def system_of_complexes.is_kernel {M M' : system_of_complexes} (f : M ⟶ M') : 
 
 variables (M M' N : system_of_complexes.{u}) (f : N ⟶ M) (g : M ⟶ M')
 
-lemma weak_normed_snake_dual {k k' k'' K K' K'' r₁ r₂ : ℝ≥0}
+lemma weak_normed_snake_dual {k k' K K' r₁ r₂ : ℝ≥0}
   [hk : fact (1 ≤ k)] [hk' : fact (1 ≤ k')] [hk'' : fact (1 ≤ k'')]
   {m : ℕ} {c₀ : ℝ≥0}
   (hM : M.is_weak_bounded_exact k K (m+1) c₀)
   (hM' : M'.is_weak_bounded_exact k' K' (m+1) c₀)
   (hM'_adm : M'.admissible)
-  (hg : ∀ c i (x : M (k'' * c) i), ∥g x∥ ≤ r₁ * ∥x∥)
-  (Hg : ∀ (c : ℝ≥0) [fact (c₀ ≤ c)] (i : ℕ) (hi : i ≤ m+1+1) (y : M' (k'' * c) i),
-    ∃ (x : M (k'' * c) i), g x = y ∧ ∥x∥ ≤ r₂ * ∥x∥)
+  (hg : ∀ c i (x : M c i), ∥g x∥ ≤ r₁ * ∥x∥)
+  (Hg : ∀ (c : ℝ≥0) [fact (c₀ ≤ c)] (i : ℕ) (hi : i ≤ m+1+1) (y : M' c i),
+    ∃ (x : M c i), g x = y ∧ ∥x∥ ≤ r₂ * ∥x∥)
   (hg : ∀ c i, (f.apply : N c i ⟶ M c i).range = g.apply.ker)
   (hgker : system_of_complexes.is_kernel f) :
-  N.is_weak_bounded_exact (k''*k*k') (K'*(K*K'' + 1)) m c₀ := sorry
+  N.is_weak_bounded_exact (k*k') (K'*(K + 1)) m c₀ := sorry
