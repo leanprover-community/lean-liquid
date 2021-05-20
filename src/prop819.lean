d54f6d058f9e6b3160578d2a83b48aacc0a5bb69
Adam Topaz <github@adamtopaz.com>
2021-05-20 17:25:18 -0600

    Kill sorry

--- src/prop819.lean
+++ src/prop819.lean
@@ -28,13 +28,52 @@ abbreviation FLC : cochain_complex SemiNormedGroup ℕ :=
   (((cosimplicial_object.augmented.whiskering _ _).obj (LCC.{u u}.obj M)).obj
   F.augmented_cech_nerve.right_op).to_cocomplex
 
+--⊢ cosimplicial_object.δ
+--      (functor.right_op F.cech_nerve ⋙ (curry.obj (uncurry.obj LocallyConstant ⋙ Completion)).obj M)
+--      k =
+--    Completion.map (cosimplicial_object.δ (functor.right_op F.cech_nerve ⋙ LocallyConstant.obj M) k)
+
+lemma FLC_iso_helper {x y : simplex_category} (f : x ⟶ y) :
+  (F.cech_nerve.right_op ⋙ LCC.obj M).map f =
+  Completion.map ((F.cech_nerve.right_op ⋙ LocallyConstant.obj M).map f) :=
+begin
+  change Completion.map _ = _,
+  congr' 1,
+  dsimp [uncurry],
+  erw locally_constant.map_hom_id,
+  change 𝟙 _ ≫ _ = _,
+  rw category.id_comp,
+end
+
 def FLC_iso : strict_iso ((Completion.map_homological_complex _).obj (FL F M)) (FLC F M) :=
 { iso := homological_complex.iso_of_components (λ i,
     match i with
     | 0 := eq_to_iso rfl
     | n+1 := eq_to_iso rfl
     end) begin
-      sorry
+      rintro (_|i) (_|j) h; rcases h with _|⟨i,w⟩; ext; dsimp [FLC_iso._match_1];
+        split_ifs with hh hh,
+      { simp only [category.id_comp, category.comp_id, Completion_map_apply],
+        dsimp only [cosimplicial_object.augmented.to_cocomplex_d,
+          cosimplicial_object.augmented.drop, comma.snd, cosimplicial_object.whiskering,
+          whiskering_right, cosimplicial_object.coboundary, functor.const_comp, LCC],
+        simpa },
+      { exfalso,
+        apply hh,
+        refl },
+      { simp only [category.id_comp, category.comp_id, Completion_map_apply],
+        dsimp only [cosimplicial_object.augmented.to_cocomplex_d,
+          cosimplicial_object.augmented.drop, comma.snd, cosimplicial_object.whiskering,
+          whiskering_right, cosimplicial_object.coboundary, LCC],
+        rw [← Completion_map_apply, Completion.map_sum],
+        congr,
+        funext k,
+        rw [Completion.map_gsmul],
+        congr' 1,
+        apply FLC_iso_helper },
+      { exfalso,
+        apply hh,
+        refl }
     end,
   is_strict := λ i, { strict_hom' := λ a, by { cases i; refl } } }
 
