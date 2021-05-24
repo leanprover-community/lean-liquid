66d430ef843940f7dd351327431aa496e379b9af
Adam Topaz <github@adamtopaz.com>
2021-05-23 22:22:53 -0600

    Clean up arrows

--- src/for_mathlib/Profinite/arrow_limit.lean
+++ src/for_mathlib/Profinite/arrow_limit.lean
@@ -14,10 +14,14 @@ open discrete_quotient
 /-- A diagram of arrows construction from discrete quotients of F.left. -/
 @[simps]
 def arrow_diagram : discrete_quotient F.left ⥤ arrow Profinite :=
-{ obj := λ S, S.make_arrow surj,
+{ obj := λ S,
+  { left := Profinite.of S,
+    right := Profinite.of $ S.make F.hom surj,
+    hom := ⟨discrete_quotient.map (S.make_le_comap _ _),
+      discrete_quotient.map_continuous _⟩ },
   map := λ S T f,
   { left := ⟨of_le $ le_of_hom f⟩,
-    right := ⟨of_le $ make_right_mono surj S T $ le_of_hom f⟩ } }.
+    right := ⟨of_le $ make_right_mono F.hom surj S T $ le_of_hom f⟩ } }
 
 lemma arrow_diagram_surjective (S : discrete_quotient F.left) :
   function.surjective ((arrow_diagram F surj).obj S).hom :=
@@ -33,13 +37,18 @@ instance (S : discrete_quotient F.left) : arrow.split ((arrow_diagram F surj).ob
   is_splitting' := begin
     ext x,
     have := classical.some_spec (arrow_diagram_surjective F surj S x),
-    simp [this],
+    erw this,
+    simp,
   end }
 
 /-- A cone which is a limit expressing an arrow as a limit. -/
 @[simps]
 def arrow_cone : limits.cone (arrow_diagram F surj) :=
 { X := F,
-  π := { app := λ S, S.make_hom surj } }
+  π :=
+  { app := λ S,
+    { left := ⟨S.proj, S.proj_continuous⟩,
+      right := ⟨(S.make _ surj).proj,
+        (S.make _ surj).proj_continuous⟩ } } }
 
 end Profinite
--- src/for_mathlib/Profinite/compat_discrete_quotient.lean
+++ src/for_mathlib/Profinite/compat_discrete_quotient.lean
@@ -5,30 +5,23 @@ import data.setoid.partition
 
 open category_theory
 
-/-- A compatible system of discrete quotients. -/
-@[nolint has_inhabited_instance]
-structure compat_discrete_quotient (f : arrow Profinite) :=
-(left : discrete_quotient f.left)
-(right : discrete_quotient f.right)
-(compat' [] : discrete_quotient.le_comap f.hom.continuous left right . obviously)
-
-restate_axiom compat_discrete_quotient.compat'
-
 namespace discrete_quotient
 
-variables {f : arrow Profinite} (surj : function.surjective f.hom)
+universe u
+variables {X Y : Profinite.{u}} (f : X ⟶ Y) (surj : function.surjective f)
+
 
 /-- The relation defining the largest quotient of f.right compatible with S. -/
-inductive make_rel (S : discrete_quotient f.left) : f.right → f.right → Prop
-| of (x y : f.left) (h : S.rel x y) : make_rel (f.hom x) (f.hom y)
-| trans {x y z : f.right} : make_rel x y → make_rel y z → make_rel x z
+inductive make_rel (S : discrete_quotient X) : Y → Y → Prop
+| of (x y : X) (h : S.rel x y) : make_rel (f x) (f y)
+| trans {x y z : Y} : make_rel x y → make_rel y z → make_rel x z
 
-lemma make_rel_impl (S : discrete_quotient f.left) (x y : f.left) :
-  S.rel x y → S.make_rel (f.hom x) (f.hom y) := make_rel.of _ _
+lemma make_rel_impl (S : discrete_quotient X) (x y : X) :
+  S.rel x y → S.make_rel f (f x) (f y) := make_rel.of _ _
 
 include surj
 
-lemma make_rel_equiv (S : discrete_quotient f.left) : equivalence S.make_rel :=
+lemma make_rel_equiv (S : discrete_quotient X) : equivalence (S.make_rel f) :=
 begin
   refine ⟨_,_,_⟩,
   { intro x,
@@ -47,37 +40,37 @@ begin
 end
 
 /-- The setoid assoc. to make_rel. -/
-def make_setoid (S : discrete_quotient f.left) : _root_.setoid f.right :=
-⟨S.make_rel, S.make_rel_equiv surj⟩
+def make_setoid (S : discrete_quotient X) : _root_.setoid Y :=
+⟨S.make_rel f, S.make_rel_equiv f surj⟩
 
 /-- The quotient of make_rel. -/
 @[nolint has_inhabited_instance]
-def make_quotient (S : discrete_quotient f.left) : Type* := quotient (S.make_setoid surj)
+def make_quotient (S : discrete_quotient X) : Type* := quotient (S.make_setoid f surj)
 
 /-- The projection onto make_rel. -/
-def make_proj (S : discrete_quotient f.left) : f.right → S.make_quotient surj :=
+def make_proj (S : discrete_quotient X) : Y → S.make_quotient f surj :=
   quotient.mk'
 
-lemma make_proj_comm (S : discrete_quotient f.left) :
-  S.make_proj surj ∘ f.hom = (quotient.map' f.hom $ by exact make_rel_impl _) ∘ S.proj :=
+lemma make_proj_comm (S : discrete_quotient X) :
+  S.make_proj f surj ∘ f = (quotient.map' f $ by exact make_rel_impl _ _) ∘ S.proj :=
 by {ext, refl}
 
 variable (f)
 
 -- A surjective map of compact Hausdorff spaces is a quotient map
 -- TODO: This certainly belongs in mathlib, if not already there...
-lemma quotient_map : quotient_map f.hom :=
+lemma quotient_map : quotient_map f :=
 begin
   rw quotient_map_iff,
   refine ⟨surj,_⟩,
   intro S,
   split,
   { intro hS,
-    exact is_open.preimage f.hom.continuous hS },
+    exact is_open.preimage f.continuous hS },
   { intro hS,
     rw ← is_closed_compl_iff at *,
     rw ← set.preimage_compl at hS,
-    have : Sᶜ = f.hom '' (f.hom ⁻¹' Sᶜ),
+    have : Sᶜ = f '' (f ⁻¹' Sᶜ),
     { ext,
       split,
       { intro h,
@@ -86,55 +79,55 @@ begin
       { rintro ⟨y,h,rfl⟩,
         exact h } },
     rw this,
-    apply Profinite.is_closed_map f.hom,
+    apply Profinite.is_closed_map f,
     assumption }
 end
 
-variable {f}
-
 /-- Given a discrete quotient S of f.left, this is the compatible quotient
  of f where f.right is as large as possible. -/
-def make (S : discrete_quotient f.left) : discrete_quotient f.right :=
-{ rel := make_rel S,
-  equiv := S.make_rel_equiv surj,
+def make (S : discrete_quotient X) : discrete_quotient Y :=
+{ rel := S.make_rel f,
+  equiv := S.make_rel_equiv f surj,
   clopen := begin
     intros x,
-    have : set_of (S.make_rel x) = S.make_proj surj ⁻¹' {S.make_proj surj x},
+    have : set_of (S.make_rel f x) = S.make_proj f surj ⁻¹' {S.make_proj f surj x},
     { dsimp [make_proj],
       ext,
       simp only [set.mem_preimage, set.mem_singleton_iff, quotient.eq', set.mem_set_of_eq],
       refine ⟨λ h, setoid.symm' _ h, λ h, setoid.symm' _ h⟩ },
     rw this,
-    letI : topological_space (S.make_quotient surj) := ⊥,
-    haveI : discrete_topology (S.make_quotient surj) := ⟨rfl⟩,
-    suffices : continuous (S.make_proj surj),
+    letI : topological_space (S.make_quotient f surj) := ⊥,
+    haveI : discrete_topology (S.make_quotient f surj) := ⟨rfl⟩,
+    suffices : continuous (S.make_proj f surj),
     { split,
       apply is_open.preimage this trivial,
       apply is_closed.preimage this ⟨trivial⟩ },
     rw (quotient_map f surj).continuous_iff,
-    rw S.make_proj_comm surj,
+    rw S.make_proj_comm f surj,
     apply continuous.comp,
     continuity,
     exact S.proj_continuous,
   end }
 
-lemma make_le_comap (S : discrete_quotient f.left) : le_comap f.hom.continuous S (S.make surj) :=
-make_rel_impl _
+lemma make_le_comap (S : discrete_quotient X) : le_comap f.continuous S (S.make f surj) :=
+make_rel_impl _ _
 
+/-
 /-- make as an arrow. -/
-def make_arrow (S : discrete_quotient f.left) : arrow Profinite :=
+def make_arrow (S : discrete_quotient X) : arrow Profinite :=
 { left := Profinite.of S,
   right := Profinite.of (S.make surj),
   hom := ⟨discrete_quotient.map (S.make_le_comap surj)⟩ }
 
 /-- the canonical morphism of arrows to make. -/
-def make_hom (S : discrete_quotient f.left) : f ⟶ S.make_arrow surj :=
+def make_hom (S : discrete_quotient X) : f ⟶ S.make_arrow surj :=
 { left := ⟨S.proj, S.proj_continuous⟩,
   right := ⟨(S.make surj).proj, (S.make surj).proj_continuous⟩ }
+-/
 
-lemma make_right_le (S : discrete_quotient f.left) (T : discrete_quotient f.right)
-  (compat : le_comap f.hom.continuous S T) :
-  (S.make surj) ≤ T :=
+lemma make_right_le (S : discrete_quotient X) (T : discrete_quotient Y)
+  (compat : le_comap f.continuous S T) :
+  (S.make f surj) ≤ T :=
 begin
   intros x y h,
   induction h with a b hab a b c _ _ h1 h2,
@@ -143,8 +136,8 @@ begin
   apply T.trans _ _ _ h1 h2,
 end
 
-lemma make_right_mono (S1 S2 : discrete_quotient f.left) (h : S1 ≤ S2) :
-  S1.make surj ≤ S2.make surj :=
+lemma make_right_mono (S1 S2 : discrete_quotient X) (h : S1 ≤ S2) :
+  S1.make f surj ≤ S2.make f surj :=
 begin
   intros x y h,
   induction h,
@@ -154,6 +147,5 @@ begin
   apply make_rel.trans,
   assumption',
 end
-
 
 end discrete_quotient
