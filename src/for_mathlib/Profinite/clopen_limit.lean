ea432120780139f715e596b49ff7946f8704d40a
Adam Topaz <github@adamtopaz.com>
2021-05-25 13:01:01 -0600

    Reduce to Top

--- src/for_mathlib/Profinite/clopen_limit.lean
+++ src/for_mathlib/Profinite/clopen_limit.lean
@@ -18,17 +18,64 @@ def created_cone : limits.cone F :=
 def created_cone_is_limit : limits.is_limit (created_cone F) :=
   lifted_limit_is_limit _
 
-def cone_iso (hC : is_limit C) : C.X ≅ (created_cone F).X :=
-hC.cone_point_unique_up_to_iso $ created_cone_is_limit _
+def cone_iso (hC : is_limit C) : C ≅ (created_cone F) :=
+hC.unique_up_to_iso $ created_cone_is_limit _
 
-def cone_iso_Top (hC : is_limit C) :
-  Profinite_to_Top.obj C.X ≅ (Top.limit_cone $ F ⋙ Profinite_to_Top).X :=
-let A := lifted_limit_maps_to_original (Top.limit_cone_is_limit $ F ⋙ Profinite_to_Top) in
-Profinite_to_Top.map_iso (cone_iso _ _ hC) ≪≫ (cones.forget _).map_iso A
+def cone_point_iso (hC : is_limit C) : C.X ≅ (created_cone F).X :=
+(cones.forget _).map_iso $ cone_iso _ _ hC
+
+def created_iso : Profinite_to_Top.map_cone (created_cone F) ≅
+  (Top.limit_cone $ F ⋙ Profinite_to_Top) :=
+lifted_limit_maps_to_original _
+
+def created_point_iso : Profinite_to_Top.obj (created_cone F).X ≅
+  (Top.limit_cone $ F ⋙ Profinite_to_Top).X := (cones.forget _).map_iso $
+created_iso _
+
+def iso_to_Top (hC : is_limit C) : Profinite_to_Top.obj C.X ≅
+  (Top.limit_cone $ F ⋙ Profinite_to_Top).X :=
+  Profinite_to_Top.map_iso (cone_point_iso _ _ hC) ≪≫ created_point_iso _
+
+def cone_homeo (hC : is_limit C) :
+  C.X ≃ₜ (Top.limit_cone $ F ⋙ Profinite_to_Top).X :=
+let FF := iso_to_Top _ _ hC in
+{ to_fun := FF.hom,
+  inv_fun := FF.inv,
+  left_inv := λ x, by simp,
+  right_inv := λ x, by simp,
+  continuous_to_fun := FF.hom.continuous,
+  continuous_inv_fun := FF.inv.continuous }
+
+lemma exists_clopen' (U : set (Top.limit_cone $ F ⋙ Profinite_to_Top).X) (hU : is_clopen U) :
+  ∃ (j : J) (V : set (F.obj j)) (hV : is_clopen V),
+  U = ((Top.limit_cone $ F ⋙ Profinite_to_Top).π.app j) ⁻¹' V := sorry
 
 /-- The existence of a clopen. -/
 theorem exists_clopen (hC : is_limit C) (U : set C.X) (hU : is_clopen U) :
-  ∃ (j : J) (V : set (F.obj j)) (hV : is_clopen V), U = (C.π.app j) ⁻¹' V := sorry
+  ∃ (j : J) (V : set (F.obj j)) (hV : is_clopen V), U = (C.π.app j) ⁻¹' V :=
+begin
+  let FF := cone_homeo _ _ hC,
+  let UU := FF.symm ⁻¹' U,
+  have hUU : is_clopen UU,
+  { split,
+    exact is_open.preimage (FF.symm.continuous) hU.1,
+    exact is_closed.preimage (FF.symm.continuous) hU.2 },
+  rcases exists_clopen' F UU hUU with ⟨j,V,hV,hJ⟩,
+  use j, use V, use hV,
+  dsimp only [UU] at hJ,
+  have : U = FF ⁻¹' (((Top.limit_cone (F ⋙ Profinite_to_Top)).π.app j) ⁻¹' V),
+  { rw [← hJ, ← set.preimage_comp],
+    simp },
+  rw this,
+  rw ← set.preimage_comp,
+  congr' 1,
+  have : C.π.app j = (cone_point_iso _ _ hC).hom ≫ (created_cone _).π.app j,
+  { have := (cone_iso _ _ hC).hom.w,
+    rw ← this,
+    refl },
+  rw this,
+  refl,
+end
 
 /-- The images of the transition maps stabilize. -/
 lemma image_stabilizes [∀ i, fintype (F.obj i)]
