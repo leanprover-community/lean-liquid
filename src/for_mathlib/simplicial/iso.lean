8fbc3c70eed9c282c5fae2611d917a4793895bb9
Johan Commelin <johan@commelin.net>
2021-05-27 18:02:51 +0000

    forgotten files

---
+++ src/for_mathlib/simplicial/iso.lean
@@ -0,0 +1,64 @@
+import algebraic_topology.simplicial_object
+
+open opposite category_theory category_theory.limits
+open simplex_category
+
+universes v u
+
+namespace category_theory
+
+namespace comma
+
+universes v₁ v₂ v₃ u₁ u₂ u₃
+variables {A : Type u₁} [category.{v₁} A]
+variables {B : Type u₂} [category.{v₂} B]
+variables {T : Type u₃} [category.{v₃} T]
+variables {L : A ⥤ T} {R : B ⥤ T}
+variables {X Y : comma L R} (f : X ⟶ Y)
+
+instance is_iso_of [is_iso f.left] [is_iso f.right] : is_iso f :=
+{ out := ⟨
+  { left := inv f.left,
+    right := inv f.right,
+    w' := by { dsimp,
+      simp only [is_iso.eq_comp_inv, functor.map_inv, comma_morphism.w,
+        is_iso.inv_comp_eq, category.assoc],
+      simp } },
+   by { split; ext; dsimp; simp only [is_iso.hom_inv_id, is_iso.inv_hom_id] }⟩ }
+
+end comma
+
+namespace simplicial_object
+
+variables {C : Type u} [category.{v} C]
+
+section
+
+variables {X Y : simplicial_object C} (f : X ⟶ Y)
+
+instance is_iso_of [hf : ∀ n, is_iso (f.app n)] : is_iso f :=
+{ out :=
+  begin
+    let g : Y ⟶ X :=
+    { app := λ n, inv (f.app n),
+      naturality' := λ m n φ, _ },
+    { refine ⟨g, _, _⟩;
+      { ext n,
+        simp only [nat_trans.id_app, nat_trans.comp_app, is_iso.hom_inv_id, is_iso.inv_hom_id], } },
+    { simp only [is_iso.inv_hom_id_assoc, nat_trans.naturality, is_iso.comp_inv_eq, category.assoc] }
+  end }
+
+end
+
+namespace augmented
+
+variables {X Y : augmented C} (f : X ⟶ Y)
+
+instance is_iso_of [∀ n, is_iso (f.left.app n)] [is_iso f.right] : is_iso f :=
+infer_instance
+
+end augmented
+
+end simplicial_object
+
+end category_theory
---
+++ src/for_mathlib/wide_pullback.lean
@@ -0,0 +1,54 @@
+import category_theory.limits.shapes.wide_pullbacks
+import category_theory.concrete_category
+import topology.category.Profinite
+.
+
+universe variables v u
+
+open category_theory category_theory.limits category_theory.limits.wide_pullback function
+
+namespace Profinite
+namespace wide_pullback
+
+lemma ext {B : Profinite} {ι : Type*} {X : ι → Profinite} {f : Π (j : ι), X j ⟶ B}
+  [has_wide_pullback B X f]
+  (x y : wide_pullback B X f) (h₀ : base f x = base f y) (h : ∀ j, π f j x = π f j y) :
+  x = y :=
+begin
+  let gx : of punit ⟶ wide_pullback B X f := ⟨const _ x, continuous_const⟩,
+  let gy : of punit ⟶ wide_pullback B X f := ⟨const _ y, continuous_const⟩,
+  suffices : gx = gy,
+  { show gx punit.star = gy punit.star, rw this },
+  apply wide_pullback.hom_ext,
+  { intro j, ext1, exact h j, },
+  { ext1, exact h₀ }
+end
+
+lemma ext_iff {B : Profinite} {ι : Type*} {X : ι → Profinite} {f : Π (j : ι), X j ⟶ B}
+  [has_wide_pullback B X f]
+  (x y : wide_pullback B X f) :
+  x = y ↔ (base f x = base f y ∧ ∀ j, π f j x = π f j y) :=
+begin
+  refine ⟨_, _⟩,
+  { rintro rfl, exact ⟨rfl, λ _, rfl⟩ },
+  { rintro ⟨h₀, h⟩, apply ext x y h₀ h },
+end
+
+lemma ext' {B : Profinite} {ι : Type*} [hι : nonempty ι]
+  {X : ι → Profinite} {f : Π (j : ι), X j ⟶ B} [has_wide_pullback B X f]
+  (x y : wide_pullback B X f) (h : ∀ j, π f j x = π f j y) :
+  x = y :=
+begin
+  apply ext x y _ h,
+  unfreezingI { obtain ⟨j⟩ := hι },
+  simp only [← π_arrow f j, coe_comp, comp, h],
+end
+
+lemma ext_iff' {B : Profinite} {ι : Type*} [nonempty ι]
+  {X : ι → Profinite} {f : Π (j : ι), X j ⟶ B} [has_wide_pullback B X f]
+  (x y : wide_pullback B X f) :
+  x = y ↔ (∀ j, π f j x = π f j y) :=
+by { refine ⟨_, ext' x y⟩, rintro rfl _, refl }
+
+end wide_pullback
+end Profinite
