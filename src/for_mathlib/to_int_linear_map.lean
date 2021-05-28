0b5fedc9c53dec285b34aad6533993cdc472667a
Johan Commelin <johan@commelin.net>
2021-05-28 10:35:56 +0000

    forgotten file

---
+++ src/for_mathlib/to_int_linear_map.lean
@@ -0,0 +1,12 @@
+import algebra.module
+.
+
+namespace add_monoid_hom
+
+variables (M₁ M₂ : Type*) [add_comm_group M₁] [add_comm_group M₂]
+
+lemma to_int_linear_map_injective :
+  function.injective (@to_int_linear_map M₁ M₂ _ _) :=
+by { intros f g h, ext, exact linear_map.congr_fun h x }
+
+end add_monoid_hom
