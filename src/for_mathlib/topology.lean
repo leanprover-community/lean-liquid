46af00508bf1c8c9f73c5597ea766aa51aa38470
Adam Topaz <github@adamtopaz.com>
2021-05-27 18:29:54 -0600

    Finish exists_discrete_quotient

--- src/for_mathlib/topology.lean
+++ src/for_mathlib/topology.lean
@@ -6,6 +6,15 @@ open_locale classical
 
 open topological_space
 
+lemma is_closed.sdiff {X : Type*} [topological_space X] {S T : set X} :
+  is_closed S → is_open T → is_closed (S \ T) :=
+begin
+  intros h1 h2,
+  rw (show S \ T = S ∩ Tᶜ, by refl),
+  apply is_closed.inter h1,
+  simpa using h2,
+end
+
 lemma is_closed_bUnion' {ι X : Type*} [topological_space X] :
   Π (F : finset ι) (Vs : Π (i : ι) (hi : i ∈ F), set X)
   (hVs : ∀ (i : ι) (hi : i ∈ F), is_closed (Vs i hi)),
