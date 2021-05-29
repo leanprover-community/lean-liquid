7df53f14035262158b44e44a4675d26adb633ece
Scott Morrison <scott.morrison@gmail.com>
2021-05-29 12:08:51 +1000

    .

--- src/for_mathlib/SemiNormedGroup.lean
+++ src/for_mathlib/SemiNormedGroup.lean
@@ -1,12 +1,13 @@
 import normed_group.SemiNormedGroup
 
+-- PR'd as #7740
+
 namespace SemiNormedGroup
 
 open category_theory
 
 universes u
 
--- PR'd as #7740
 noncomputable instance has_zero_object : limits.has_zero_object SemiNormedGroup.{u} :=
 { zero := 0,
   unique_to := λ X,
@@ -16,8 +17,7 @@ noncomputable instance has_zero_object : limits.has_zero_object SemiNormedGroup.
   { default := 0,
     uniq := λ f, by ext } }
 
--- Do we have this?
-lemma coe_comp_apply (A B C : SemiNormedGroup) (f : A ⟶ B) (g : B ⟶ C)
-  (a : A) : (f ≫ g) a = g (f a) := rfl
+@[simp] lemma coe_comp {M N K : SemiNormedGroup} (f : M ⟶ N) (g : N ⟶ K) :
+  ((f ≫ g) : M → K) = g ∘ f := rfl
 
 end SemiNormedGroup
