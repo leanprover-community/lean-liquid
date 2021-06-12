055004f04ccd46878fc19f6b84005cc6f52f99ae
Patrick Massot <patrickmassot@free.fr>
2021-06-12 21:49:08 +0200

    Remove useless simp attribute
    Not clear it should be a simp lemma, and the attribute was never used

--- src/for_mathlib/coe_nat_abs.lean
+++ src/for_mathlib/coe_nat_abs.lean
@@ -2,8 +2,7 @@ import analysis.normed_space.basic
 
 open_locale nnreal
 
--- move this
-@[simp] lemma nnreal.coe_nat_abs (n : ℤ) : ↑n.nat_abs = nnnorm n :=
+lemma nnreal.coe_nat_abs (n : ℤ) : (n.nat_abs : ℝ≥0) = nnnorm n :=
 nnreal.eq $
 calc ((n.nat_abs : ℝ≥0) : ℝ)
     = ↑(n.nat_abs : ℤ) : by simp only [int.cast_coe_nat, nnreal.coe_nat_cast]
