0c9e38ac38360be221cea7bc73d0bd2f67db65d2
Johan Commelin <johan@commelin.net>
2021-06-25 08:58:11 +0000

    update README

--- src/liquid.lean
+++ src/liquid.lean
@@ -24,18 +24,17 @@ open_locale nnreal -- enable the notation `ℝ≥0` for the nonnegative real num
 
 open category_theory ProFiltPseuNormGrpWithTinv polyhedral_lattice opposite
 
+variables (r r' : ℝ≥0)
+variables [fact (0 < r)] [fact (0 < r')] [fact (r < r')] [fact (r < 1)] [fact (r' < 1)]
+variables (BD : breen_deligne.package) (κ : ℕ → ℝ≥0)
+variables [BD.data.very_suitable r r' κ] [∀ (i : ℕ), fact (0 < κ i)]
+
+include r r' BD κ
 
 /-- A mix of Theorems 9.4 and 9.5 in [Analytic] -/
-theorem first_target
-  (r r' : ℝ≥0)
-  [fact (0 < r)] [fact (0 < r')] [fact (r < r')] [fact (r < 1)] [fact (r' < 1)]
-  (BD : breen_deligne.package) (κ : ℕ → ℝ≥0)
-  [BD.data.very_suitable r r' κ] [∀ (i : ℕ), fact (0 < κ i)] :
-  ∀ m : ℕ,
-  ∃ (k K : ℝ≥0) [fact (1 ≤ k)],
-  ∃ c₀ : ℝ≥0,
-  ∀ (S : Type) [fintype S],
-  ∀ (V : SemiNormedGroup.{0}) [normed_with_aut r V],
+theorem first_target :
+  ∀ m : ℕ, ∃ (k K : ℝ≥0) [fact (1 ≤ k)] c₀ : ℝ≥0,
+  ∀ (S : Type) [fintype S] (V : SemiNormedGroup.{0}) [normed_with_aut r V],
     ​((BD.data.system κ r V r').obj (op $ of r' (Mbar r' S))).is_weak_bounded_exact k K m c₀ :=
 begin
   intro m,
@@ -73,8 +72,5 @@ The proof reduces to `thm95''` (a variant of Theorem 9.5).
   `≤ k`-exact in degrees `≤ m` for `c ≥ c₀` (where `c` is an index to the system of complexes).
 -/
 
-example (r r' : ℝ≥0) [fact (0 < r)] [fact (0 < r')] [fact (r < r')] [fact (r < 1)] [fact (r' < 1)]
-  (BD : breen_deligne.package) (κ : ℕ → ℝ≥0)
-  [BD.data.very_suitable r r' κ] [∀ (i : ℕ), fact (0 < κ i)] :
-  first_target_stmt r r' BD κ :=
+example : first_target_stmt r r' BD κ :=
 first_target r r' BD κ
