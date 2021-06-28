df17e3a6da3d64a11e48e0c21bd858d6bb391716
Ben Toner <bentoner@bentoner.com>
2021-06-28 16:56:17 +1000

    Run sorry-free check on first_target not thm95''

--- scripts/print_thm95_axioms.lean
+++ scripts/print_thm95_axioms.lean
@@ -1,9 +1,9 @@
 import system.io
-import thm95
+import liquid
 
 -- Surround with unique tokens to be robust against anything in the import that may product output.
 #print "BEGIN_THM95_AXIOMS"
-#print axioms thm95''
+#print axioms first_target
 #print "END_THM95_AXIOMS"
 
 meta def main : io unit := pure ()
