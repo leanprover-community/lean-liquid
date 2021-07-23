3c7f32ce8a17cf8c3a012510d4765919918edbe1
Adam Topaz <github@adamtopaz.com>
2021-07-23 10:28:58 -0600

    Add extend

--- src/for_mathlib/Profinite/extend.lean
+++ src/for_mathlib/Profinite/extend.lean
@@ -15,6 +15,7 @@ variables {C : Type u} [category.{v} C]
 -- And let `F` be a functor from `Fintype` to `C`.
   (F : Fintype.{v} ⥤ C)
 
+@[simps]
 def change_cone {X Y : Profinite} (f : X ⟶ Y) (D : cone (X.fintype_diagram ⋙ F)) :
   cone (Y.fintype_diagram ⋙ F) :=
 { X := D.X,
@@ -28,6 +29,31 @@ def change_cone {X Y : Profinite} (f : X ⟶ Y) (D : cone (X.fintype_diagram ⋙
       simp only [category.assoc, ← F.map_comp, functor.comp_map],
       congr' 2,
       ext ⟨t⟩, refl,
-    end } }
+    end } } .
+
+@[simps]
+def extend : Profinite ⥤ C :=
+{ obj := λ X, limit (X.fintype_diagram ⋙ F),
+  map := λ X Y f, limit.lift _ (change_cone _ f _),
+  map_id' := begin
+    intros X,
+    ext S,
+    dsimp,
+    simp only [limit.lift_π, coe_id, change_cone_π_app, limit.cone_π, category.id_comp],
+    erw discrete_quotient.map_id,
+    change _ ≫ F.map (𝟙 _) = _,
+    rw [F.map_id, category.comp_id],
+    congr,
+    exact S.comap_id,
+  end,
+  map_comp' := begin
+    intros X Y Z f g,
+    ext S,
+    dsimp,
+    simp only [limit.lift_π, change_cone_π_app,
+      limit.cone_π, limit.lift_π_assoc, coe_comp, category.assoc, ← F.map_comp],
+    congr,
+    exact discrete_quotient.map_comp _ _,
+  end }
 
 end Profinite
