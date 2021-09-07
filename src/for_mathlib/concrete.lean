622eabf88e0096fab888eb69a6071e8ce2a84ad9
Adam Topaz <github@adamtopaz.com>
2021-09-07 08:36:59 -0600

    Small generalization

--- src/for_mathlib/concrete.lean
+++ src/for_mathlib/concrete.lean
@@ -13,13 +13,19 @@ variables {C : Type u} [category.{v} C] [concrete_category.{v} C]
 local attribute [instance] has_coe_to_sort has_coe_to_fun
 
 /-- If `C` is a concrete category for which the forgetful functor
+preserves limits, then the type `limit G` is in bijection with the type of
+sequences compatible with the transition morphisms in the diagram. -/
+noncomputable def limit.equiv :
+  (G ⋙ forget _).sections ≃ ↥(limits.limit G) :=
+let E := limits.is_limit_of_preserves (forget C) (limits.limit.is_limit G),
+    F := limits.types.limit_cone_is_limit (G ⋙ forget C) in
+(F.cone_point_unique_up_to_iso E).to_equiv
+
+/-- If `C` is a concrete category for which the forgetful functor
 preserves limits, then we can construct a term of `limit G` in the "usual"
 way as a sequence compatible with the transition morphisms in the diagram. -/
 noncomputable def limit.mk (x : Π j : J, G.obj j) (compat : ∀ ⦃i j : J⦄ (e : i ⟶ j),
-  G.map e (x _) = x _) : ↥(limits.limit G) :=
-let E := limits.is_limit_of_preserves (forget C) (limits.limit.is_limit G),
-    F := limits.types.limit_cone_is_limit (G ⋙ forget C) in
-(F.cone_point_unique_up_to_iso E).hom ⟨x,compat⟩
+  G.map e (x _) = x _) : ↥(limits.limit G) := (limit.equiv G) ⟨x,compat⟩
 
 end concrete_category
 
