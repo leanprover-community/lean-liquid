8e718b7905327bdedc2c3bd1ace7706f025f5063
Adam Topaz <github@adamtopaz.com>
2021-09-07 08:30:12 -0600

    Add docstring

--- src/for_mathlib/concrete.lean
+++ src/for_mathlib/concrete.lean
@@ -12,7 +12,9 @@ variables {C : Type u} [category.{v} C] [concrete_category.{v} C]
 
 local attribute [instance] has_coe_to_sort has_coe_to_fun
 
-@[nolint doc_blame] --TODO add doc?
+/-- If `C` is a concrete category for which the forgetful functor
+preserves limits, then we can construct a term of `limit G` in the "usual"
+way as a sequence compatible with the transition morphisms in the diagram. -/
 noncomputable def limit.mk (x : Π j : J, G.obj j) (compat : ∀ ⦃i j : J⦄ (e : i ⟶ j),
   G.map e (x _) = x _) : ↥(limits.limit G) :=
 let E := limits.is_limit_of_preserves (forget C) (limits.limit.is_limit G),
