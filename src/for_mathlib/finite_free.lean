68bf09c7e7e9c14c1b5ea181c456444f16d91dfe
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-09-16 09:48:43 +0200

    fix build

--- src/for_mathlib/finite_free.lean
+++ src/for_mathlib/finite_free.lean
@@ -6,30 +6,15 @@ open_locale big_operators
 
 variables (R M : Type*) [ring R] [add_comm_group M] [module R M]
 
-lemma module.finite.of_basis {ι : Type*} [fintype ι] (b : basis ι R M) : module.finite R M :=
-begin
-  classical,
-  refine ⟨⟨finset.univ.image b, _⟩⟩,
-  simp only [set.image_univ, finset.coe_univ, finset.coe_image, basis.span_eq],
-end
-
 section
 variables [module.finite ℤ M] [module.free ℤ M]
 
 -- generalize?
 instance : module.finite ℤ (M →+ ℤ) :=
-begin
-  classical,
-  let b := module.free.choose_basis ℤ M,
-  exact module.finite.of_basis ℤ _ (b.dual_basis.map add_monoid_hom_lequiv_int.symm),
-end
+module.finite.equiv add_monoid_hom_lequiv_int.symm
 
 -- generalize?
 instance : module.free ℤ (M →+ ℤ) :=
-begin
-  classical,
-  let b := module.free.choose_basis ℤ M,
-  exact module.free.of_basis (b.dual_basis.map add_monoid_hom_lequiv_int.symm),
-end
+module.free.of_equiv add_monoid_hom_lequiv_int.symm
 
 end
