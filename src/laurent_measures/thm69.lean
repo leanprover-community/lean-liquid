d80b24b334da641cd89f006f5269762af0fd16ad
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2021-10-01 10:08:30 +0200

    removed commented lines

--- src/laurent_measures/thm69.lean
+++ src/laurent_measures/thm69.lean
@@ -12,48 +12,6 @@ noncomputable theory
 open set filter function classical finset nat
 open_locale topological_space classical nnreal big_operators
 
--- section surjectivity
-
--- parameter (x : ℝ≥0)
--- variables (y : ℝ≥0) --(N : ℕ)
-
--- def N : ℕ := ⌈(x⁻¹ : ℝ)⌉₊
-
--- lemma N_inv_le : x ≥ 1 / N := sorry
-
-
--- --The minimal integer such that the corresponding coefficient in the Laurent series for y is ≠ 0
--- def deg : ℤ := ⌊(log y) / (log x)⌋
-
--- lemma xpow_le : x ^ (deg y) ≤ y := sorry
-
--- lemma deg_is_min : ∀ k < deg y, x ^ k > y := sorry
-
--- def a (m : ℤ) := ⌊ (y / x ^ m : ℝ)⌋₊
-
--- lemma a_bdd : a y (deg y) < N  := sorry
-
--- lemma y_mul_xpow_le : ((a y (deg y) : ℝ≥0) * x ^ (deg y)) ≤ y := sorry
-
--- def z (m : ℤ) := y - (a y m) * x ^ m
-
--- /--Given the bound L (eventually L = deg y), `step m` is the pair whose first element is the
--- (m+L)-th coefficient
--- -/
--- def step (L : ℤ) (m : ℕ) : ℕ × ℝ≥0 := (a y (L + m), z y (L + m))
-
--- noncomputable def A : ℕ → ℕ × ℝ≥0
--- | 0         := step y (deg y) 0
--- | (m + 1)   := step (A m).2 (deg y) (m + 1)--let z' := (A m).2, c := n y + m + 1 in (a z' c, z z' c)
-
--- lemma deg_increasing (k : ℕ) : deg (A y (k + 1)).2 > deg (A y k).2 := sorry
-
--- def coeff : ℤ → ℕ := λ k, if k < deg y then 0 else (A y (k + deg y ).to_nat).1
-
--- lemma surj_on_nonneg : has_sum (λ k : ℤ, (coeff y k : ℝ≥0) * x ^ k ) y := sorry
-
--- end surjectivity
-
 def laurent_measures.to_Rfct (r : ℝ≥0) : --[fact (r < 1)] :
   (laurent_measures r (Fintype.of punit)) → (ℤ → ℤ) := λ ⟨F, _⟩, (F punit.star)
 
