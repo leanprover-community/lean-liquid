154d33a78100e393ccad577feb9fd03714efac8b
Adam Topaz <github@adamtopaz.com>
2021-11-18 12:22:02 -0700

    kill sorry

--- src/for_mathlib/sheaf.lean
+++ src/for_mathlib/sheaf.lean
@@ -8,6 +8,12 @@ variables {C : Type*} [category C] (J : grothendieck_topology C)
 variables {D : Type*} [category D]
 
 lemma is_sheaf_of_iso {F G : Cᵒᵖ ⥤ D} (η : F ≅ G) (hG : is_sheaf J G) : is_sheaf J F :=
-sorry
+begin
+  intros T X S hS,
+  let e : F ⋙ coyoneda.obj (opposite.op T) ≅ G ⋙ coyoneda.obj (opposite.op T) :=
+    iso_whisker_right η _,
+  apply presieve.is_sheaf_for_iso e.symm,
+  apply hG _ _ hS,
+end
 
 end category_theory.presheaf
