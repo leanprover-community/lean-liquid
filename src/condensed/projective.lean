e4abe584bd9153acc62b60d18d9aefa227140834
Adam Topaz <github@adamtopaz.com>
2021-11-25 12:04:02 -0700

    More work toward projective resolutiosn

--- src/condensed/projective.lean
+++ src/condensed/projective.lean
@@ -2,6 +2,7 @@ import category_theory.yoneda
 import condensed.basic
 import condensed.is_proetale_sheaf
 import algebra.category.Group.adjunctions
+import for_mathlib.SheafOfTypes_sheafification
 --import algebra.category.Group.filtered_colimits
 
 --import category_theory.sites.sheafification
@@ -54,50 +55,3 @@ def Profinite.to_Condensed (T : Profinite.{u}) : CondensedSet :=
         dsimp,
         apply Profinite.π_descend } }
   end } .
-
-namespace CondensedAb
-
-noncomputable
-def free_on (S : CondensedSet.{u}) : Condensed.{u} Ab.{u+1} :=
-(presheaf_to_Sheaf proetale_topology _).obj $
-  (SheafOfTypes_to_presheaf _).obj S ⋙ AddCommGroup.free
-
-def forget : Condensed.{u} Ab.{u+1} ⥤ CondensedSet :=
-  Condensed.forget_to_CondensedType
-
-noncomputable
-def lift {S : CondensedSet.{u}} {T : Condensed.{u} Ab.{u+1}}
-  (η : S ⟶ forget.obj T) :
-  free_on S ⟶ T :=
-proetale_topology.sheafify_lift
-{ app := λ X, free_abelian_group.lift $ η.app X,
-  naturality' := begin
-    intros X Y f,
-    apply free_abelian_group.lift.ext,
-    intros t,
-    dsimp,
-    simp only [free_abelian_group.lift.of, comp_apply,
-      free_abelian_group.map_of, AddCommGroup.free_map_coe],
-    change ((S.1.map f) ≫ η.app Y) _ = _,
-    rw η.naturality,
-    refl,
-  end } T.2 .
-
--- We need the fact that sheafification for Ab-valued presheaves arises from
--- sheafification of Type-valued presheaves.
-def incl (S : CondensedSet.{u}) : S ⟶ forget.obj (free_on S) := sorry
-
-@[simp]
-lemma incl_lift {S : CondensedSet.{u}} {T : Condensed.{u} Ab.{u+1}}
-  (η : S ⟶ forget.obj T) : incl S ≫ forget.map (lift η) = η := sorry
-
-lemma lift_unique {S : CondensedSet.{u}} {T : Condensed.{u} Ab.{u+1}}
-  (η : S ⟶ forget.obj T) (g : free_on S ⟶ T) (h : incl S ≫ forget.map g = η) :
-  g = lift η := sorry
-
-lemma hom_ext {S : CondensedSet.{u}} {T : Condensed.{u} Ab.{u+1}}
-  (f g : free_on S ⟶ T) (h : incl S ≫ forget.map f = incl S ≫ forget.map g) :
-  f = g :=
-by rw [lift_unique (incl S ≫ forget.map f) f rfl, lift_unique (incl S ≫ forget.map g) g rfl, h]
-
-end CondensedAb
