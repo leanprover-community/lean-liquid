abab4bd67419037b48d2f5cc2bde4097763243cb
Adam Topaz <github@adamtopaz.com>
2022-01-12 13:47:49 -0700

    change hom

--- src/condensed/extr.lean
+++ src/condensed/extr.lean
@@ -21,24 +21,26 @@ structure ExtrDisc :=
 
 namespace ExtrDisc
 
-@[ext]
-structure hom (X Y : ExtrDisc) :=
-mk :: (val : X.val ⟶ Y.val)
-
 @[simps]
 instance : category ExtrDisc :=
-{ hom := hom,
-  id := λ X, ⟨𝟙 _⟩,
-  comp := λ X Y Z f g, ⟨f.val ≫ g.val⟩ }
+{ hom := λ X Y, X.val ⟶ Y.val,
+  id := λ X, 𝟙 _,
+  comp := λ X Y Z f g, f ≫ g }
 
 @[simps]
 def _root_.ExtrDisc_to_Profinite : ExtrDisc ⥤ Profinite :=
 { obj := val,
-  map := λ X Y f, f.val }
+  map := λ X Y f, f }
 
 instance : concrete_category ExtrDisc.{u} :=
 { forget := ExtrDisc_to_Profinite ⋙ forget _,
   forget_faithful := ⟨⟩ }
+
+instance : has_coe_to_sort ExtrDisc Type* :=
+concrete_category.has_coe_to_sort _
+
+instance {X Y : ExtrDisc} : has_coe_to_fun (X ⟶ Y) (λ f, X → Y) :=
+⟨λ f, f⟩
 
 instance (X : ExtrDisc) : projective X.val := X.cond
 
@@ -46,33 +48,25 @@ example (X : ExtrDisc) : projective (ExtrDisc_to_Profinite.obj X) :=
 by { dsimp, apply_instance }
 
 noncomputable
-def split {X Y : ExtrDisc} (f : X ⟶ Y) (hf : function.surjective f.val) :
+def split {X Y : ExtrDisc} (f : X ⟶ Y) (hf : function.surjective f) :
   Y ⟶ X :=
 begin
-  have : epi f.val, by rwa Profinite.epi_iff_surjective f.val,
+  let f' : X.val ⟶ Y.val := f,
+  have : epi f', by  rwa Profinite.epi_iff_surjective f',
   resetI,
-  choose g h using projective.factors (𝟙 Y.val) f.val,
+  choose g h using projective.factors (𝟙 Y.val) f,
   exact ⟨g⟩,
 end
 
 @[simp, reassoc]
 lemma splitting_is_splitting {X Y : ExtrDisc} (f : X ⟶ Y)
-  (hf : function.surjective f.val) : split f hf ≫ f = 𝟙 _ :=
+  (hf : function.surjective f) : split f hf ≫ f = 𝟙 _ :=
 begin
-  have : epi f.val, by rwa Profinite.epi_iff_surjective f.val,
+  let f' : X.val ⟶ Y.val := f,
+  have : epi f', by  rwa Profinite.epi_iff_surjective f',
   resetI,
-  ext1,
-  exact (projective.factors (𝟙 Y.val) f.val).some_spec,
+  exact (projective.factors (𝟙 Y.val) f).some_spec,
 end
-
-instance : has_coe_to_sort ExtrDisc Type* :=
-concrete_category.has_coe_to_sort _
-
-instance {X Y : ExtrDisc} : has_coe_to_fun (X ⟶ Y) (λ f, X → Y) :=
-concrete_category.has_coe_to_fun
-
-@[simp]
-lemma coe_fun_eq {X Y : ExtrDisc} (f : X ⟶ Y) : ⇑(f.val) = f := rfl
 
 instance (X : ExtrDisc) : topological_space X :=
 show topological_space X.val, by apply_instance
@@ -98,7 +92,7 @@ lemma free.ι_apply {α : Type u} (a : α) : free.ι α a = (pure a : ultrafilte
 
 noncomputable
 def free.lift {X : ExtrDisc.{u}} {α : Type u} (f : α → X) : free α ⟶ X :=
-⟨⟨ultrafilter.extend f, continuous_ultrafilter_extend _⟩⟩
+⟨ultrafilter.extend f, continuous_ultrafilter_extend _⟩
 
 @[simp]
 lemma free.lift_apply {X : ExtrDisc.{u}} {α : Type u} (f : α → X) (F : free α) :
@@ -124,12 +118,11 @@ begin
   letI hh : topological_space α := ⊥,
   have : dense_range (free.ι α) := dense_range_pure,
   rw ← free.ι_lift f at h,
-  ext : 2,
+  ext1,
   have := this.equalizer _ _ h,
   erw this,
-  refl,
-  exact g.val.continuous,
-  exact (free.lift f).val.continuous,
+  exact g.continuous,
+  exact (free.lift f).continuous,
 end
 
 @[ext]
