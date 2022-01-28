3135f29a997ed07c750aabd1423ce33b94c787f9
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2022-01-28 22:01:37 +0100

    advances on summable_convolution

--- src/laurent_measures/aux_lemmas.lean
+++ src/laurent_measures/aux_lemmas.lean
@@ -1,6 +1,7 @@
 -- import topology.algebra.infinite_sum
 import data.finset.nat_antidiagonal
 import analysis.normed_space.basic
+import analysis.specific_limits
 
 open metric --finset --filter
 open_locale nnreal classical big_operators topological_space
@@ -23,16 +24,17 @@ end equivalences
 section summable
 
 lemma goofy {r : ℝ≥0} (f : ℤ → ℤ) (hf : summable (λ n, ∥ f n ∥ * r ^ n)) (b : ℕ)
-: (λ n : ℕ, (2 * r : ℝ) ^ n * ∥∑' (x : ℕ), (1 / 2 : ℝ) ^ (n + x + 1 : ℤ) * (f (n + x + 1 : ℤ))∥) b ≤
+: (λ n : ℕ, (2 * r : ℝ) ^ n * ∥∑' (x : ℕ), (1 / 2 : ℝ) ^ (n + 1 + x : ℤ) * (f (n + 1 + x : ℤ))∥) b ≤
   (λ n : ℕ, (2 * r : ℝ) ^ n * ∥∑' (x : ℕ), (1 / 2 : ℝ) ^ (x + 1) * (f (x + 1))∥) b:=
 begin
   sorry,
 end
 
 lemma aux_pos_terms {r : ℝ≥0} (f : ℤ → ℤ) (n : ℕ) : 0 ≤ (2 * r : ℝ) ^ n *
-  ∥∑' (x : ℕ), (1 / 2 : ℝ) ^ (n + x + 1) * ↑(f (n + 1 + x))∥ := sorry
+  ∥∑' (x : ℕ), (1 / 2 : ℝ) ^ (n + 1 + x) * ↑(f (n + 1 + x))∥ := sorry
 
-lemma summable_convolution {r : ℝ≥0} (hr₀: 0 < r) (f : ℤ → ℤ) (d : ℤ)
+
+lemma summable_convolution {r : ℝ≥0} (hr₀: 0 < r) (hr₂ : 1 / 2 < r) (f : ℤ → ℤ) (d : ℤ)
   (hf : summable (λ n, ∥ f n ∥ * r ^ n)) (hd : ∀ n : ℤ, n < d → f n = 0) :
   summable (λ n : ℤ, (1 / 2) * ∥ tsum (λ i : ℕ, ((f (n + 1 + i)) : ℝ) * (1 / 2) ^ i) ∥ * r ^ n) :=
 begin
@@ -48,7 +50,7 @@ begin
     simp_rw [mul_comm, ← normed_field.norm_mul, ← tsum_mul_left, ← mul_assoc],
     rw ← half_norm,
     simp_rw [← (pow_succ (1 / 2 : ℝ) _)],
-    convert_to summable (λ (n : ℕ), ((2 : ℝ) * r) ^ n * ∥∑' (x : ℕ), (1 / 2 : ℝ) ^ (n + x + 1 : ℤ)
+    convert_to summable (λ (n : ℕ), ((2 : ℝ) * r) ^ n * ∥∑' (x : ℕ), (1 / 2 : ℝ) ^ (n + 1 + x : ℤ)
       * (f (n + 1 + x))∥),
     { funext n,
       nth_rewrite_rhs 0 [mul_pow],
@@ -64,58 +66,22 @@ begin
       rw [neg_one_mul, int.of_nat_eq_coe, half_norm, ← normed_field.norm_zpow,
         ← normed_field.norm_mul ((1 / 2 : ℝ) ^ (- ↑n)) _, ← half_norm],
       simp_rw [← tsum_mul_left, ← mul_assoc, ← zpow_add₀ $ one_div_ne_zero $ @two_ne_zero ℝ _ _, add_assoc,
-        neg_add_cancel_left],
+        neg_add_cancel_left, add_comm _ 1],
       refl },
-      -- let g := λ b : ℕ, (2 * r : ℝ) ^ b * ∥∑' (x : ℕ), (1 / 2 : ℝ) ^ (↑x + 1) * f (1 + ↑x)∥,
-      have := @summable_of_nonneg_of_le _ _ _ _ (goofy f hf),
-      simp only at this,
-      apply this,
-      apply summable_of_nonneg_of_le,
-      { intro b, exact aux_pos_terms f b},
+      apply summable_of_nonneg_of_le _ (goofy f hf),
+      { have temp : ∥ (2 * r : ℝ) ∥ < 1, sorry,
+        apply summable.mul_right,
+        exact summable_geometric_of_norm_lt_1 temp,
+        --refine (summable_geometric_of_norm_lt_1 _).mul_right,
+      -- apply geom
+        },--intro b, exact aux_pos_terms f b},
       { intro b,
         have : (0 : ℝ) < (2 * ↑r) ^ b,
         { apply pow_pos,
           apply mul_pos,
-          simp only [zero_lt_bit0, zero_lt_one],
-          simpa only [nnreal.coe_pos],},
-      simp only [one_div] },
-      -- squeeze_simp [(mul_le_mul_left this).mpr (goofy f hf b)],
-      -- have fine := (mul_le_mul_left this).mpr (goofy f hf b),
-      -- simp only at fine },
-
-
-
-      suffices : summable (λ (n : ℕ), (2 * ↑r) ^ n * ∥∑' (x : ℕ), (1 / 2) ^ (↑x + 1) *
-        (f (1 + ↑x))∥),
-      sorry,-- **[FAE]** this is a reduction to a subtype, and it has already been proven
-      --somewhere as aux lemma
-
-
-      have H1 : summable (λ x : ℕ, (1 / 2 : ℝ) ^ (x + 1 : ℤ) * (f (1 + x) : ℝ)), sorry,
-      replace H1 := H1.has_sum,
-      have H2 : summable (λ n : ℕ, (2 * r : ℝ) ^ n), sorry,
-      replace H2 := H2.has_sum,
-      have H3 := H2.mul_eq H1,
-      --hf.has_sum.mul_eq hg.has_sum hfg.has_sum
-
-      -- **[FAE]** Now it is the product of a convergent geometric series and a convergent gadget
-    -- have h_bdd : ∀ n : ℕ, ∥ tsum (λ i : ℕ, (1 / 2) ^ i * (f (n + 1 + i))) ∥ ≤
-    -- ∥ tsum (λ i : ℕ, (1 / 2) ^ i * (f (1 + i))) ∥,
-    -- {},
-    -- replace h_bdd : ∀ n : ℕ, ∥ tsum (λ i : ℕ, (r : ℝ) ^ n * ( 1 / 2) * (1 / 2) ^ i * (f (n + 1 + i))) ∥ ≤
-    --   ∥ (r : ℝ) ^ n * ( 1 / 2) * tsum (λ i : ℕ, (1 / 2) ^ i * (f (1 + i))) ∥, sorry,
-    -- replace h_bdd : ∀ n : ℕ, (r : ℝ) ^ n * ( 1 / 2) * ∥ tsum (λ i : ℕ, (1 / 2) ^ i * (f (n + 1 + i))) ∥ ≤ (r : ℝ) ^ n * ( 1 / 2) * ∥ tsum (λ i : ℕ, (1 / 2) ^ i * (f (1 + i))) ∥, sorry,
-    -- apply summable_of_nonneg_of_le _ h_bdd,
-    -- apply @summable.mul_right _ _ _ _ _ (λ n : ℕ, (r : ℝ) ^ n * ( 1 / 2))
-    -- (∥ ∑' (i : ℕ), (1 / 2) ^ i * (f (1 + i)) ∥),
-    -- apply summable.mul_right,
-    -- sorry,--this is just the sum of the geometric series
-    -- sorry,--trivial, product of positive gadgets
-  },
-
-
-
-  -- }
+          simp only [zero_lt_bit0, zero_lt_one, nnreal.coe_pos],
+          simpa only [nnreal.coe_pos] },
+      exact aux_pos_terms f b }},
 end
 
 end summable
