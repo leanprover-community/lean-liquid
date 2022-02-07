e3c1b14327d69342198fd3d5a85b551ff1d10be7
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2022-02-07 18:31:31 +0100

    some trials

--- src/laurent_measures/ses.lean
+++ src/laurent_measures/ses.lean
@@ -1,18 +1,22 @@
-import laurent_measures.functor
+-- import laurent_measures.functor
 import laurent_measures.thm69
-
+-- import data.real.basic
 
 /-
 This files introduces the maps `Θ`, `Φ` and `Ψ` are the "measurifications" of `θ`, `ϕ` and `ψ` from
 `laurent_measures.thm69`, they are morphisms in the right category.
 
 We then prove in **???** that `θ_ϕ_exact` of `laurent_measures.thm69` becomes a short exact sequence
-in the category **???**.-/
+in the category **???**.
+-/
 
 
 noncomputable theory
 
 namespace laurent_measures_ses
+
+open nnreal laurent_measures
+open_locale big_operators nnreal
 
 section homs
 
@@ -21,9 +25,21 @@ parameter {p : ℝ≥0}
 def r : ℝ≥0 := (1 / 2) ^ (p:ℝ)
 
 variables [fact(0 < p)] [fact (p < 1)]
+variables [fact (0 < r)]
 variable {S : Fintype}
 
+local notation `ℳ` := real_measures p
+local notation `ℒ` := laurent_measures r
 
-example : profinitely_filtered_pseudo_normed_group_with_Tinv r (ℒ S) := by library_search
+example : profinitely_filtered_pseudo_normed_group_with_Tinv r (ℒ S) :=
+begin
+  apply_instance,
+end
+
+-- begin
+
+-- end
 
 end homs
+
+end laurent_measures_ses
