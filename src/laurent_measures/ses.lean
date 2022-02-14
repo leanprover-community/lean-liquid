cc1222ffed5c6626e297ad747a403e84bf17beef
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2022-02-14 16:32:53 +0100

    Phi as cfpng_hom

--- src/laurent_measures/ses.lean
+++ src/laurent_measures/ses.lean
@@ -3,7 +3,7 @@ import laurent_measures.thm69
 -- import data.real.basic
 
 /-
-This files introduces the maps `Θ`, `Φ` and `Ψ` are the "measurifications" of `θ`, `ϕ` and `ψ` from
+This files introduces the maps `Θ`, `Φ` (***and `Ψ` ???***), which are the "measurifications" of `θ`, `ϕ` (*** and `ψ` ???***)
 `laurent_measures.thm69`, they are morphisms in the right category.
 
 We then prove in **???** that `θ_ϕ_exact` of `laurent_measures.thm69` becomes a short exact sequence
@@ -13,9 +13,11 @@ in the category **???**.
 
 noncomputable theory
 
+universe u
+
 namespace laurent_measures_ses
 
-open nnreal laurent_measures pseudo_normed_group
+open nnreal laurent_measures pseudo_normed_group comphaus_filtered_pseudo_normed_group comphaus_filtered_pseudo_normed_group_hom
 open_locale big_operators nnreal
 
 section homs
@@ -30,6 +32,89 @@ variable {S : Fintype}
 
 local notation `ℳ` := real_measures p
 local notation `ℒ` := laurent_measures r
+
+variables {M₁ M₂ : Type u} [comphaus_filtered_pseudo_normed_group M₁] [comphaus_filtered_pseudo_normed_group M₂]
+
+def cfpng_hom_add (f g : comphaus_filtered_pseudo_normed_group_hom M₁ M₂) : (comphaus_filtered_pseudo_normed_group_hom M₁ M₂) :=
+begin
+  apply mk_of_bound (f.to_add_monoid_hom + g.to_add_monoid_hom) (f.bound.some + g.bound.some),
+  intro c,
+  refine ⟨_, _⟩,
+  { intros x hx,
+      simp only [comphaus_filtered_pseudo_normed_group_hom.coe_mk],
+      simp only [add_monoid_hom.add_apply, coe_to_add_monoid_hom],
+      convert pseudo_normed_group.add_mem_filtration (f.bound.some_spec hx) (g.bound.some_spec hx),
+      rw add_mul, },
+  let f₀ : filtration M₁ c → filtration M₂ (f.bound.some * c) := λ x, ⟨f x, f.bound.some_spec x.2⟩,
+  have hf₀ : continuous f₀ := f.continuous _ (λ x, rfl),
+  let g₀ : filtration M₁ c → filtration M₂ (g.bound.some * c) := λ x, ⟨g x, g.bound.some_spec x.2⟩,
+  have hg₀ : continuous g₀ := g.continuous _ (λ x, rfl),
+  simp only [add_monoid_hom.add_apply, coe_to_add_monoid_hom],
+  haveI : fact ((f.bound.some * c + g.bound.some * c) ≤ ((f.bound.some + g.bound.some) * c) ) := fact.mk (le_of_eq (add_mul _ _ _).symm),
+  let ι : filtration M₂ (f.bound.some * c + g.bound.some * c) → filtration M₂ ((f.bound.some + g.bound.some) * c) := cast_le,
+  have hι : continuous ι := continuous_cast_le _ _,
+  let S₀ : filtration M₂ (f.bound.some * c) × filtration M₂ (g.bound.some * c) → filtration M₂ (f.bound.some * c + g.bound.some * c) := λ x, ⟨x.fst + x.snd, add_mem_filtration x.fst.2 x.snd.2⟩,
+  have hS₀ := continuous_add' (f.bound.some * c) (g.bound.some * c),
+  exact hι.comp (hS₀.comp (continuous.prod_mk hf₀ hg₀)),
+end
+
+def cfpng_hom_neg (f : comphaus_filtered_pseudo_normed_group_hom M₁ M₂) : (comphaus_filtered_pseudo_normed_group_hom M₁ M₂) :=
+begin
+  apply mk_of_bound (- f.to_add_monoid_hom) (f.bound.some),
+  intro c,
+  refine ⟨_, _⟩,
+  { intros x hx,
+    simp only [comphaus_filtered_pseudo_normed_group_hom.coe_mk],
+    convert pseudo_normed_group.neg_mem_filtration (f.bound.some_spec hx) },
+  let f₀ : filtration M₁ c → filtration M₂ (f.bound.some * c) := λ x, ⟨f x, f.bound.some_spec x.2⟩,
+  have hf₀ : continuous f₀ := f.continuous _ (λ x, rfl),
+  exact (continuous_neg' _).comp hf₀,
+end
+
+-- def cfpng_hom_sub (f g : comphaus_filtered_pseudo_normed_group_hom M₁ M₂) : (comphaus_filtered_pseudo_normed_group_hom M₁ M₂) :=
+-- begin
+--   apply mk_of_bound (f.to_add_monoid_hom - g.to_add_monoid_hom) (f.bound.some + g.bound.some),
+--   intro c,
+--   refine ⟨_, _⟩,
+--   { intros x hx,
+--       simp only [comphaus_filtered_pseudo_normed_group_hom.coe_mk],
+--       simp only [add_monoid_hom.sub_apply, coe_to_add_monoid_hom],
+--       convert pseudo_normed_group.sub_mem_filtration (f.bound.some_spec hx) (g.bound.some_spec hx),
+--       rw add_mul, },
+--   let f₀ : filtration M₁ c → filtration M₂ (f.bound.some * c) := λ x, ⟨f x, f.bound.some_spec x.2⟩,
+--   have hf₀ : continuous f₀ := f.continuous _ (λ x, rfl),
+--   let g₀ : filtration M₁ c → filtration M₂ (g.bound.some * c) := λ x, ⟨g x, g.bound.some_spec x.2⟩,
+--   have hg₀ : continuous g₀ := g.continuous _ (λ x, rfl),
+--   simp only [add_monoid_hom.sub_apply, coe_to_add_monoid_hom],
+--   haveI : fact ((f.bound.some * c + g.bound.some * c) ≤ ((f.bound.some + g.bound.some) * c) ) := fact.mk (le_of_eq (add_mul _ _ _).symm),
+--   let ι : filtration M₂ (f.bound.some * c + g.bound.some * c) → filtration M₂ ((f.bound.some + g.bound.some) * c) := cast_le,
+--   have hι : continuous ι := continuous_cast_le _ _,
+--   let S₀ : filtration M₂ (f.bound.some * c) × filtration M₂ (g.bound.some * c) → filtration M₂ (f.bound.some * c + g.bound.some * c) := λ x, ⟨x.fst + x.snd, add_mem_filtration x.fst.2 x.snd.2⟩,
+--   have hS₀ := continuous_add' (f.bound.some * c) (g.bound.some * c),
+--   exact hι.comp (hS₀.comp (continuous.prod_mk hf₀ hg₀)),
+-- end
+
+-- def cfpng_hom_nsmul (f : comphaus_filtered_pseudo_normed_group_hom M₁ M₂) : (comphaus_filtered_pseudo_normed_group_hom M₁ M₂) := sorry
+
+
+instance : add_comm_group (comphaus_filtered_pseudo_normed_group_hom M₁ M₂) :=
+{ add := cfpng_hom_add,
+  add_assoc := by {intros, ext, apply add_assoc},
+  zero := 0,
+  zero_add := by {intros, ext, apply zero_add},
+  add_zero := by {intros, ext, apply add_zero},
+  neg := cfpng_hom_neg,
+  add_left_neg := by {intros, ext, apply add_left_neg},
+  add_comm := by {intros, ext, apply add_comm} }
+
+def Φ : comphaus_filtered_pseudo_normed_group_hom (ℒ S) (ℒ S) := 2 • shift (-1) - id
+
+lemma Φ_eq_ϕ (F : ℒ S) : Φ F = ϕ F := rfl
+
+
+
+
+#exit
 
 example : comphaus_filtered_pseudo_normed_group_with_Tinv r (ℒ S) :=
 begin
@@ -56,7 +141,10 @@ begin
   apply @subtype.topological_space (S → ℝ) _ Pi.topological_space,
 end
 
--- #help options
+
+
+
+#exit
 
 instance : comphaus_filtered_pseudo_normed_group_with_Tinv r (ℳ S) :=
 { topology := infer_instance,
