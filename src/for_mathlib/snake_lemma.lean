1349cb315c805ee1eaf36352f098ae15466d0e26
adomani <adomani@gmail.com>
2022-02-16 19:19:50 +0100

    new names?

--- src/for_mathlib/snake_lemma.lean
+++ src/for_mathlib/snake_lemma.lean
@@ -866,7 +866,8 @@ begin
 end .
 
 instance : is_iso hD.cokernel_to_top_right_kernel_to_right_kernel :=
-abelian.is_iso_of_mono_of_epi _
+is_iso_of_mono_of_epi (cokernel_to_top_right_kernel_to_right_kernel hD)
+--abelian.is_iso_of_mono_of_epi _
 
 def bottom_left_cokernel_to : cokernel ((1,0) ⟶[D] (2,1)) ⟶ D.obj (2,2) :=
 cokernel.desc _ (_ ⟶[D] _)
@@ -936,7 +937,8 @@ begin
 end
 
 instance : is_iso hD.left_cokernel_to_kernel_bottom_left_cokernel_to :=
-abelian.is_iso_of_mono_of_epi _
+is_iso_of_mono_of_epi (left_cokernel_to_kernel_bottom_left_cokernel_to hD)
+--abelian.is_iso_of_mono_of_epi _
 
 def δ_aux : cokernel hD.to_top_right_kernel ⟶ kernel hD.bottom_left_cokernel_to :=
 cokernel.desc _ (kernel.lift _ (kernel.ι _ ≫ (_ ⟶[D] _) ≫ cokernel.π _) begin
@@ -977,7 +979,8 @@ begin
 end
 
 instance : is_iso hD.to_kernel :=
-abelian.is_iso_of_mono_of_epi _
+is_iso_of_mono_of_epi (to_kernel hD)
+--abelian.is_iso_of_mono_of_epi _
 
 def cokernel_to : cokernel ((1,0) ⟶[D] (2,0)) ⟶ D.obj (3,0) :=
 cokernel.desc _ (_ ⟶[D] _) (hD.col_exact₂ _).1
@@ -997,7 +1000,8 @@ begin
 end
 
 instance : is_iso hD.cokernel_to :=
-abelian.is_iso_of_mono_of_epi _
+is_iso_of_mono_of_epi (cokernel_to hD)
+--abelian.is_iso_of_mono_of_epi _
 
 def δ : D.obj (0,2) ⟶ D.obj (3,0) :=
   hD.to_kernel ≫ inv hD.cokernel_to_top_right_kernel_to_right_kernel ≫  -- <-- this is an iso
