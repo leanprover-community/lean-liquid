c5e6a5e650c669994702e4977b45be271a9cf83c
adomani <adomani@gmail.com>
2022-02-16 16:19:21 +0100

    Revert "update name: neg_mul_eq_neg_mul_symm --> neg_mul + symm"
    This reverts commit 3a79b82619216be9a41fef2df6b26d6c480cac9c.
    
    I changed the names of two lemmas, since they were unknown on my copy
    but the old names worked on the remote repository.  For this reason,
    I reverted the changes.

--- src/polyhedral_lattice/int.lean
+++ src/polyhedral_lattice/int.lean
@@ -17,9 +17,9 @@ lemma int.sum_units_to_nat_smul (n : ℤ) :
   ∑ (i : units ℤ), int.to_nat (i * n) • (i : ℤ) = n :=
 begin
   rw [units_int.univ, finset.sum_insert], swap, dec_trivial,
-  simp only [mul_one, int.nat_cast_eq_coe_nat, one_mul, nsmul_eq_mul,
-    units.coe_one, units.coe_neg_one, finset.sum_singleton,
-    ← sub_eq_add_neg, int.to_nat_sub_to_nat_neg, neg_mul, mul_neg],
+  simp only [neg_mul_eq_neg_mul_symm, mul_one, int.nat_cast_eq_coe_nat, one_mul, nsmul_eq_mul,
+    units.coe_one, units.coe_neg_one, mul_neg_eq_neg_mul_symm, finset.sum_singleton,
+    ← sub_eq_add_neg, int.to_nat_sub_to_nat_neg],
 end
 
 instance int.polyhedral_lattice : polyhedral_lattice ℤ :=
@@ -30,6 +30,6 @@ instance int.polyhedral_lattice : polyhedral_lattice ℤ :=
     refine ⟨λ e, int.to_nat (e * n), (int.sum_units_to_nat_smul _).symm, _⟩,
     simp only [int.norm_coe_units, mul_one, nat.cast_one, one_mul, units_int.univ],
     show ∥n∥ = ((1 * n).to_nat) + (↑(((-1) * n).to_nat) + 0),
-    simp only [neg_mul, add_zero, one_mul],
+    simp only [neg_mul_eq_neg_mul_symm, add_zero, one_mul],
     exact (int.to_nat_add_to_nat_neg_eq_norm _).symm,
   end }
