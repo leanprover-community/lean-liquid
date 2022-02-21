cd99a48f5ed2dd8b1b5b06c33d8fb0d0126b78ef
Adam Topaz <github@adamtopaz.com>
2022-02-21 13:41:50 -0700

    five term exact seq

--- src/for_mathlib/derived/homological.lean
+++ src/for_mathlib/derived/homological.lean
@@ -45,6 +45,15 @@ begin
   apply homological_functor.cond F _ ((rotate_distinguished_triangle T).mp hT),
 end
 
+lemma five_term_exact_seq {A : Type*} [category A] [abelian A] (F : C ⥤ A) [F.additive]
+  [homological_functor F] (T : triangle C) (hT : T ∈ dist_triang C):
+  exact_seq A [F.map T.inv_rotate.mor₁, F.map T.mor₁, F.map T.mor₂, F.map T.mor₃] :=
+begin
+  apply exact_seq.cons _ _ _ _ (four_term_exact_seq F _ hT),
+  apply homological_functor.cond F,
+  apply inv_rot_of_dist_triangle _ _ hT,
+end
+
 lemma complete_distinguished_triangle_morphism'
   (T₁ T₂ : triangle C)
   (h₁ : T₁ ∈ dist_triang C)
