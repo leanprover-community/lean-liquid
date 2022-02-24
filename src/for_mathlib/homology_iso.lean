233245f923f2b39c88ce3cf9b48bf0df9217706d
Adam Topaz <github@adamtopaz.com>
2022-02-24 12:25:30 -0700

    homology iso

---
+++ src/for_mathlib/homology_iso.lean
@@ -0,0 +1,103 @@
+import for_mathlib.homotopy_category_pretriangulated
+import for_mathlib.homology
+
+namespace category_theory
+
+open category_theory.limits
+
+variables {A : Type*} [category A] [abelian A]
+  (X : cochain_complex A ℤ)
+
+variables (i j k : ℤ) (hij : (complex_shape.up ℤ).rel i j)
+  (hjk : (complex_shape.up ℤ).rel j k)
+
+section
+
+include hjk
+
+noncomputable theory
+
+@[simps]
+def kernel_d_from_iso :
+  kernel (X.d_from j) ≅ kernel (X.d j k) :=
+{ hom := kernel.lift _ (kernel.ι _) begin
+    apply_fun (λ e, e ≫ (X.X_next_iso hjk).inv),
+    swap,
+    { intros u v h,
+      apply_fun (λ e, e ≫ (X.X_next_iso hjk).hom) at h,
+      simpa using h },
+    dsimp,
+    rw [category.assoc, ← X.d_from_eq hjk],
+    simp,
+  end,
+  inv := kernel.lift _ (kernel.ι _) begin
+    rw X.d_from_eq hjk,
+    simp,
+  end,
+  hom_inv_id' := by { ext, simp },
+  inv_hom_id' := by { ext, simp } }
+
+include hij
+
+@[reassoc]
+lemma kernel_lift_comp_kernel_d_from_iso_hom :
+  kernel.lift (X.d_from j) (X.d_to j) (by simp) ≫
+  (kernel_d_from_iso X j k hjk).hom =
+  kernel.lift _ (X.d_to _) (by simp [X.d_to_eq hij]) :=
+by { ext, dsimp, simp }
+
+lemma kernel_lift_eq :
+  kernel.lift (X.d j k) (homological_complex.d_to X j)
+    (by simp [X.d_to_eq hij]) =
+  (X.X_prev_iso hij).hom ≫ kernel.lift _ _ (X.d_comp_d i j k) :=
+begin
+  ext,
+  dsimp,
+  simp [X.d_to_eq hij],
+end
+
+end
+
+noncomputable
+def homology_iso (i j k : ℤ) (hij : (complex_shape.up ℤ).rel i j)
+  (hjk : (complex_shape.up ℤ).rel j k) :
+(homology_functor A _ j).obj X ≅ homology _ _ (X.d_comp_d i j k) :=
+{ hom := homology.desc' _ _ _ ((kernel_d_from_iso _ _ k hjk).hom ≫
+    homology.π' _ _ _)
+  begin
+    rw kernel_lift_comp_kernel_d_from_iso_hom_assoc _ _ _ _ hij hjk,
+    rw kernel_lift_eq _ _ _ _ hij hjk,
+    simp,
+  end,
+  inv := homology.desc' _ _ _
+    ((kernel_d_from_iso _ _ k hjk).inv ≫ homology.π' _ _ _)
+  begin
+    have := kernel_lift_comp_kernel_d_from_iso_hom X i j k hij hjk,
+    rw ← iso.eq_comp_inv at this,
+    rw ← category.assoc,
+    rw kernel_lift_eq _ _ _ _ hij hjk at this,
+    rw category.assoc at this,
+    rw ← iso.inv_comp_eq at this,
+    rw ← this,
+    simp,
+  end,
+  hom_inv_id' := begin
+    ext,
+    simp,
+    let f := _, show _ = f,
+    simp only [← category.assoc _ _ f],
+    convert category.id_comp _,
+    ext,
+    simp,
+  end,
+  inv_hom_id' := begin
+    ext,
+    simp,
+    let f := _, show _ = f,
+    simp only [← category.assoc _ _ f],
+    convert category.id_comp _,
+    ext,
+    simp,
+  end }
+
+end category_theory
