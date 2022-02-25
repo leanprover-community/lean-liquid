9d9519c55df760452db890c6e5c0a97b85f9062f
adomani <adomani@gmail.com>
2022-02-25 13:38:21 +0100

    remove instance and prove inequality nnnorm_add_le

--- src/Lbar/finsupp_instance.lean
+++ src/Lbar/finsupp_instance.lean
@@ -11,19 +11,6 @@ universes u v
 
 section families_of_add_comm_groups
 
--- `apply_instance` works.  Having this instance explicitly, allows `finsupp_add_group` to work.
-instance add_comm_group_finsupp {α β γ : Type*} [add_comm_group γ] : add_comm_group (α → β →₀ γ) :=
-pi.add_comm_group
-
---  Without the explicit instance `add_comm_group_finsupp`, this does not work
-instance finsupp_add_group (S : Type*) : add_comm_group (S → ℤ →₀ ℝ) := by apply_instance
-
-/--  A function from a `Fintype` is automatically a `finsupp`, when the target has a zero. -/
-def finsupp_of_fintype_domain {α : Type*} [has_zero α] {S : Fintype} (F : S → α) : S →₀ α :=
-{ support            := (set.finite.of_fintype {s | F s ≠ 0}).to_finset,
-  to_fun             := F,
-  mem_support_to_fun := by simp }
-
 lemma finset.sum_add {α β : Type*} [add_comm_monoid β] {F G : α → β} (s : finset α) :
   ∑ x in s, (F x + G x) = ∑ x in s, F x + ∑ x in s, G x :=
 begin
@@ -35,9 +22,6 @@ end
 
 variables {S : Fintype} {α : Type*}
 
-instance fintype.sum_nnnorm [has_nnnorm α] : has_nnnorm (S → α) :=
-{ nnnorm := λ F, ∑ s, ∥F s∥₊ }
-
 instance sum_nnnorm [has_nnnorm α] : has_nnnorm (S → α) :=
 { nnnorm := λ F, ∑ b, ∥F b∥₊ }
 
@@ -47,7 +31,7 @@ lemma sum_nnnorm_def [has_nnnorm α] (F : S → α) : ∥F∥₊ = ∑ b, ∥F b
 lemma sum_nnnorm_add_le [semi_normed_group α] (F G : S → α) :
   ∥F + G∥₊ ≤ ∥F∥₊ + ∥G∥₊ :=
 show ∑ s, ∥F s + G s∥₊ ≤ ∑ s, ∥F s∥₊ + ∑ s, ∥G s∥₊, from
-le_trans (sum_le_sum (λ i hi, nnnorm_add_le _ _)) (finset.sum_add _).le
+le_trans (sum_le_sum (λ i hi, nnnorm_add_le _ _)) univ.sum_add.le
 
 end families_of_add_comm_groups
 
@@ -55,11 +39,9 @@ lemma add_zero_dists {α β : Type*} [decidable_eq α] [add_zero_class β] {l : 
   (h : x l + y l + z l = 0) (hl : l ∈ x.support) :
   l ∈ y.support ∪ z.support :=
 begin
-  contrapose hl,
-  simp only [mem_support_iff, coe_sub, pi.sub_apply, ne.def, not_not, mem_union] at hl ⊢,
-  push_neg at hl,
-  cases hl with h1 h2,
-  rwa [h1, h2, add_zero, add_zero] at h,
+  simp only [mem_support_iff, ne.def, mem_union] at hl ⊢,
+  contrapose! hl,
+  simpa only [hl, add_zero] using h,
 end
 
 lemma dists {α β : Type*} [decidable_eq α] [add_group β] {l : α} {x y z : α →₀ β}
@@ -101,9 +83,9 @@ by simp [has_norm.norm, has_dist.dist]
 lemma nnnorm_triangle {r : ℝ≥0} (x y z : r.normed) : ∥x - z∥₊ ≤ ∥x - y∥₊ + ∥y - z∥₊ :=
 begin
   unfold has_nnnorm.nnnorm,
-  rw [sum_subset (subset_union_left _ _ : _ ⊆ (x - y).support ∪ (y - z).support),
-    sum_subset (subset_union_right _ _ : _ ⊆ (x - y).support ∪ (y - z).support),
-    sum_subset (λ l hl, dists hl : _ ⊆ (_ - y).support ∪ _), ← finset.sum_add],
+  rw [sum_subset (subset_union_left _ _  : _ ⊆ (x - y).support ∪ (y - z).support),
+      sum_subset (subset_union_right _ _ : _ ⊆ (x - y).support ∪ (y - z).support),
+    sum_subset (λ l hl, dists hl : _ ⊆ (x - y).support ∪ _), ← finset.sum_add],
   { refine sum_le_sum (λ j hj, _),
     rw ← add_mul,
     refine mul_le_mul_of_nonneg_right _ (zero_le _),
@@ -112,17 +94,6 @@ begin
   repeat
   { intros k _ h, convert zero_mul _, simpa only [mem_support_iff, not_not, norm_eq_zero] using h }
 end
-
-/-  This instance, in particular, provides a `pseudo_metric_space` instance to `r.normed`. -/
-instance (r : ℝ≥0) : semi_normed_group r.normed :=
-{ norm          := coe ∘ has_nnnorm.nnnorm,
-  dist          := λ F G, ∥F - G∥₊,
-  dist_self     := λ F, by simp only [sub_self, nnnorm_zero, nonneg.coe_zero],
-  dist_comm     := λ F G, by simp only [dist, nnnorm_sub],
-  dist_triangle := λ x y z, nnnorm_triangle _ _ _,
-  edist_dist    := λ x y, by simp only [subtype.coe_eta, ennreal.of_real_coe_nnreal],
-  dist_eq       := λ x y, by simp only,
-  ..(infer_instance : add_comm_group _) }
 
 end nnreal.normed
 
@@ -137,40 +108,53 @@ variables {r : ℝ≥0} {S : Fintype}
 
 instance : inhabited (invpoly r S) := ⟨0⟩
 
-instance (r : ℝ≥0) (S : Fintype) : has_nnnorm (invpoly r S) :=
+instance : has_nnnorm (invpoly r S) :=
 @sum_nnnorm S r.normed ⟨λ F, ∑ x in F.support, ∥F x∥₊ * r⁻¹ ^ x⟩
 
 @[simp]
-lemma nnnorm_zero {r : ℝ≥0} {S : Fintype} : ∥(0 : invpoly r S)∥₊ = 0 :=
+lemma nnnorm_zero : ∥(0 : invpoly r S)∥₊ = 0 :=
 by simp only [sum_nnnorm_def, pi.zero_apply, sum_const_zero, nnreal.normed.nnnorm_zero]
 
 @[simp]
-lemma nnnorm_neg {r : ℝ≥0} {S : Fintype} (F : invpoly r S) :
+lemma nnnorm_neg (F : invpoly r S) :
   ∥-F∥₊ = ∥F∥₊ :=
 by simp only [sum_nnnorm_def, pi.neg_apply, nnreal.normed.nnnorm_neg]
 
-lemma nnnorm_sub {r : ℝ≥0} {S : Fintype} (F G : invpoly r S) :
+lemma nnnorm_sub (F G : invpoly r S) :
   ∥F - G∥₊ = ∥G - F∥₊ :=
 by rw [← nnnorm_neg (F - G), neg_sub]
 
-instance {r : ℝ≥0} {S : Fintype} : topological_space (invpoly r S) :=
-by simpa only [invpoly] using preorder.topology _
+--  There is some duplication between this lemma and `nnnorm_triangle`.
+--  They can probably be unified.
+lemma nnnorm_add_le (F G : invpoly r S) :
+  ∥F + G∥₊ ≤ ∥F∥₊ + ∥G∥₊ :=
+begin
+  simp only [sum_nnnorm_def, pi.add_apply],
+  rw ← finset.sum_add,
+  refine sum_le_sum (λ s hs, _),
+  simp only [has_nnnorm.nnnorm, finsupp.coe_add, pi.add_apply],
+  rw [sum_subset (subset_union_left _ _  : _ ⊆ (F s).support ∪ (G s).support),
+      sum_subset (subset_union_right _ _ : _ ⊆ (F s).support ∪ (G s).support),
+      sum_subset ((λ k hk, _) : _ ⊆ (F s).support ∪ (G s).support), ← finset.sum_add],
+  { refine sum_le_sum (λ j hj, _),
+    rw ← add_mul,
+    refine mul_le_mul_of_nonneg_right _ (zero_le _),
+    exact nnreal.coe_le_coe.mp (norm_add_le _ _) },
+  repeat
+  { intros k _ h, convert zero_mul _, simpa only [mem_support_iff, not_not, norm_eq_zero] using h },
+  { simp only [mem_union, mem_support_iff, ne.def, finsupp.coe_add, pi.add_apply] at ⊢ hk,
+    contrapose! hk,
+    simp only [hk, add_zero] }
+end
 
---  There is some awkwardness in getting the fact that the `nnnorm` instances coincide.
---  you can see this in the `convert sum_nnnorm_add_le F G` step.
 instance (S : Fintype) (r : ℝ≥0) : pseudo_normed_group (invpoly r S) :=
-{ to_add_comm_group   := by refine invpoly.add_comm_group r S,
+{ to_add_comm_group   := invpoly.add_comm_group r S,
   filtration          := λ c, {F : invpoly r S | ∥F∥₊ ≤ c},
-  filtration_mono     := λ c d cd x hx, by { rw set.mem_set_of_eq at hx ⊢, exact hx.trans cd },
-  zero_mem_filtration := λ c,
-    by { simp only [set.mem_set_of_eq, nnnorm_zero, zero_le'] },
+  filtration_mono     := λ c d cd x (hx : ∥x∥₊ ≤ c), hx.trans cd,
+  zero_mem_filtration := λ c, by simp only [set.mem_set_of_eq, nnnorm_zero, zero_le'],
   neg_mem_filtration  := λ c F hF, by simpa only [set.mem_set_of_eq, nnnorm_neg],
-  add_mem_filtration  := λ c d F G hF hG, begin
-      simp only [sum_nnnorm_def, set.mem_set_of_eq, pi.add_apply, finsupp.coe_add],
-      refine le_trans _ (add_le_add hF hG),
-      convert sum_nnnorm_add_le F G,
-      { ext s, simp only [pi.add_apply, _root_.coe_nnnorm], refl },
-      repeat { unfold invpoly.has_nnnorm, congr, ext, refl },
-    end }
+  add_mem_filtration  := λ c d F G hF hG, show ∑ s, ∥F s + G s∥₊ ≤ c + d, by
+  { refine le_trans _ (add_le_add hF hG),
+    exact nnnorm_add_le F G } }
 
 end invpoly
