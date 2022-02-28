26a01dabc8065049ad928894f6feceb8661deb21
adomani <adomani@gmail.com>
2022-02-28 05:20:17 +0100

    doc-string nnnorm_add_class

--- src/Lbar/nnnorm_add_class.lean
+++ src/Lbar/nnnorm_add_class.lean
@@ -1,43 +1,19 @@
 import analysis.normed_space.basic
 
 /-!
-Let `α` be a type with the structure of an additive commutative group and
-a non-negative norm `∥_∥₊`.
+#  The typeclass `nnnorm_add_class α`
 
-In this file, we introduce
-* the typeclass `nnnorm_add_class α`,
-* a construction of `pseudo_normed_group` using the "standard filtration" `λ c, {z : α | ∥z∥₊ ≤ c}`.
+In this file, we introduce the typeclass `nnnorm_add_class α`.
 
-##  The typeclass `nnnorm_add_class α`
-Given an additive commutative group `α` with a `nnnorm`, the typeclass `nnnorm_add_class α`
-requires to prove the identities:
+Let `α` be a type with a non-negative norm `∥_∥₊`, a zero `0`, an addition `+` and a negation `-`.
+
+The typeclass `nnnorm_add_class α` requires to prove the identities:
 * the nnnorm of `0` is `0` : `∥0∥₊ = 0`;
 * the nnnorm of `-x` is equal to the nnnorm of `x`: `∥- x∥₊ = ∥x∥₊`;
 * the nnnorm of a sum is at most the sum of the nnnorms: `∥x + y∥₊ ≤ ∥x∥₊ + ∥y∥₊`.
-
-##  The standard filtration `c : ℝ≥0 ↦ {z : α | ∥z∥₊ ≤ c}`
-Let `α` be a nnnormed type.  We use the naming convention `std_flt` for the function `ℝ≥0 → set α`
-taking a non-negative real number `c` to the subset of all the terms of `α` of nnnorm at most `c`:
-```
-ℝ≥0 → set α
-  c ↦ {z : α | ∥z∥₊ ≤ c}.
-```
-
-The main construction is `std_flt.to_pseudo_normed_group` asserting that the typeclass
-`nnnorm_add_class α` (i.e. `α` is an additive commutative group with a compatible `∥_∥₊`),
-gives rise to a `pseudo_normed_group α` under the standard filtration.
 -/
 
 open_locale nnreal
-
-lemma mem_union_support_of_mem_support_add {α β : Type*} [add_zero_class β] [decidable_eq α] {k : α}
-  (F G : α →₀ β) (hk : k ∈ (F + G).support) :
-  k ∈ F.support ∪ G.support :=
-begin
-  simp only [finset.mem_union, finsupp.mem_support_iff, finsupp.coe_add, pi.add_apply] at ⊢ hk,
-  contrapose! hk,
-  simp only [hk, add_zero],
-end
 
 variables (α : Type*) [has_nnnorm α]
 
@@ -45,8 +21,7 @@ variables (α : Type*) [has_nnnorm α]
 * the nnnorm of `0` is `0`;
 * the nnnorm of `-x` is equal to the nnnorm of `x`;
 * the nnnorm of a sum is at most the sum of the nnnorms.
-The class assumes `add_comm_group`, since this is what is required for `pseudo_normed_group`.
--/
+The class only assumes that `α` has `0, +, -`. -/
 @[ancestor add_comm_group has_nnnorm]
 class nnnorm_add_class [has_zero α] [has_add α] [has_neg α] : Prop :=
 (nnn_zero   : ∥(0 : α)∥₊ = 0)
