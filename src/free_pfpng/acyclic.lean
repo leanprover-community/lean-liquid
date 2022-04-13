a28ffa195458d172e12b2a0dec867862a2acead2
Johan Commelin <johan@commelin.net>
2022-04-13 15:14:48 +0000

    new file normed_to_cond

--- src/free_pfpng/acyclic.lean
+++ src/free_pfpng/acyclic.lean
@@ -1,6 +1,7 @@
 import free_pfpng.main
 import condensed.acyclic
 import prop819
+import normed_to_cond
 .
 
 noncomputable theory
@@ -15,32 +16,6 @@ variables (S : Profinite.{u})
 variables (V : SemiNormedGroup.{u}) [complete_space V]
 
 set_option pp.universes true
-
--- move me
-instance SemiNormedGroup.forget₂ : has_forget₂ SemiNormedGroup.{u} Ab.{u} :=
-{ forget₂ :=
-  { obj := λ V, AddCommGroup.of V,
-    map := λ V W f, f.to_add_monoid_hom,
-    map_id' := λ V, rfl,
-    map_comp' := λ _ _ _ f g, rfl },
-  forget_comp := rfl }
-
--- move me
-instance SemiNormedGroup.forget₂_additive :
-  (forget₂ SemiNormedGroup.{u} Ab.{u}).additive :=
-{ map_add' := by { intros, refl } }
-
-section LCC
--- maybe move this section
-
-def LCC : Profinite.{u}ᵒᵖ ⥤ Ab.{u} :=
-SemiNormedGroup.LCC.obj V ⋙ forget₂ _ _
-
-def LCC_iso_Cond_of_top_ab :
-  LCC.{u} V ≅ Condensed.of_top_ab.presheaf.{u} V :=
-sorry
-
-end LCC
 
 namespace category_theory
 
