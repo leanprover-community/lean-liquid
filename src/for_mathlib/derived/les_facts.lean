f3fe349b588581257915fb0dbe22de7d0b084c6a
Johan Commelin <johan@commelin.net>
2022-04-14 06:02:38 +0000

    wip

--- src/for_mathlib/derived/les_facts.lean
+++ src/for_mathlib/derived/les_facts.lean
@@ -6,9 +6,35 @@ noncomputable theory
 
 universes v u
 
-open category_theory homotopy_category opposite
+open category_theory category_theory.triangulated homotopy_category opposite
+open bounded_homotopy_category
 
 variables {C : Type u} [category.{v} C] [abelian C] [enough_projectives C]
+
+namespace bounded_homotopy_category
+
+variables (i : ℤ) (A : triangle (bounded_homotopy_category C)) (B : bounded_homotopy_category C)
+
+def Ext.map₁ : ((Ext i).obj (op A.obj₂)).obj B ⟶ ((Ext i).obj (op A.obj₁)).obj B :=
+((Ext i).map A.mor₁.op).app B
+
+def Ext.map₂ : ((Ext i).obj (op A.obj₃)).obj B ⟶ ((Ext i).obj (op A.obj₂)).obj B :=
+((Ext i).map A.mor₂.op).app B
+
+def Ext.δ_iso : ((Ext i).obj (op A.obj₁)).obj B ≅ ((Ext (i + 1)).obj (op (A.obj₁⟦(1:ℤ)⟧))).obj B :=
+sorry
+
+def Ext.δ :
+  ((Ext i).obj (op A.obj₁)).obj B ⟶ ((Ext (i+1)).obj (op A.obj₃)).obj B :=
+(Ext.δ_iso i A B).hom ≫ ((Ext (i+1)).map A.mor₃.op).app B
+
+set_option pp.universes true
+
+lemma Ext.five_term_exact_seq :
+  exact_seq Ab.{v} [Ext.map₁ i A B, Ext.map₂ i A B, Ext.δ i A B, Ext.map₁ (i+1) A B] :=
+sorry
+
+end bounded_homotopy_category
 
 lemma is_zero_iff_epi_and_is_iso
   {A₁ A₂ A₃ : C} (f : A₁ ⟶ A₂) (g : A₂ ⟶ A₃) (B : C) (h : short_exact f g) :
