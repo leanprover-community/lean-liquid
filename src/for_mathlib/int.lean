2dabbd0a5901fa92bee8e4404b29b7f9dea6e519
YaelDillies <yael.dillies@gmail.com>
2022-04-27 10:41:46 +0100

    Profinite.pmz_to_free_pfpng_epi_aux

--- src/for_mathlib/int.lean
+++ src/for_mathlib/int.lean
@@ -1,6 +1,8 @@
-import tactic
+import data.sign
 
-@[elab_as_eliminator] protected lemma _root_.int.induction_on_iff {p : ℤ → Prop}
+namespace int
+
+@[elab_as_eliminator] protected lemma induction_on_iff {p : ℤ → Prop}
   (i : ℤ) (hz : p 0) (h : ∀ i : ℤ, p i ↔ p (i + 1)) : p i :=
 begin
   induction i using int.induction_on with i IH i IH,
@@ -8,3 +10,13 @@ begin
   { rwa ← h },
   { rwa [h, sub_add_cancel], }
 end
+
+@[simp] lemma sign_eq_sign (n : ℤ) : n.sign = _root_.sign n :=
+begin
+  obtain ((_ | _) | _) := n,
+  { exact congr_arg coe sign_zero.symm },
+  { exact congr_arg coe (sign_pos $ int.succ_coe_nat_pos _).symm },
+  { exact congr_arg coe (_root_.sign_neg $ neg_succ_lt_zero _).symm }
+end
+
+end int
