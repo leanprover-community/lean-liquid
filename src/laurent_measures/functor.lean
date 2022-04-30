fe8c590aa808a2b911237dd1a0d2ab761b2b1620
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-04-30 16:23:49 +0100

    Fintype.Laurent_measures

--- src/laurent_measures/functor.lean
+++ src/laurent_measures/functor.lean
@@ -11,26 +11,35 @@ open_locale nnreal
 
 open category_theory
 
+section Laurent_measures_def
+
+open laurent_measures
+
+/-- `Laurent_measures r'` is an explicit functor from finite types to the category of profinitely
+  exhaustively-filtered pseudo-normed groups with an action of T⁻¹, and strict morphisms. It sends
+  `S` to the Laurent measures on `S` with bound `r'`. -/
+@[simps] def Fintype.Laurent_measures (r : ℝ≥0) [fact (0 < r)] :
+  Fintype.{u} ⥤ ProFiltPseuNormGrpWithTinv₁.{u} r :=
+{ obj := λ S, ⟨laurent_measures r S, λ F, ⟨∥F∥₊, le_rfl⟩⟩,
+  map := λ S T f, map_hom f,
+  map_id' := λ S, by { ext1, simp only [map_hom, map_id], refl, },
+  map_comp' := λ S S' S'' f g, by { ext1, simp only [map_hom, map_comp], refl } }
+
+end Laurent_measures_def
+
 namespace laurent_measures
 
 variables (r' : ℝ≥0) [fact (0 < r')]
 
 open ProFiltPseuNormGrpWithTinv₁
 
-/-- `fintype_functor r'` is an explicit functor from finite types to the category of profinitely
-  exhaustively-filtered pseudo-normed groups with an action of T⁻¹, and strict morphisms. It sends
-  `S` to the Laurent measures on `S` with bound `r'`. -/
-@[simps] def fintype_functor : Fintype.{u} ⥤ ProFiltPseuNormGrpWithTinv₁.{u} r' :=
-{ obj := λ S, ⟨laurent_measures r' S, λ F, ⟨∥F∥₊, le_rfl⟩⟩,
-  map := λ S T f, map_hom f,
-  map_id' := λ S, by { ext1, simp only [map_hom, map_id], refl, },
-  map_comp' := λ S S' S'' f g, by { ext1, simp only [map_hom, map_comp], refl } }
+open Fintype
 
 @[simps] def profinite : Profinite.{u} ⥤ ProFiltPseuNormGrpWithTinv₁.{u} r' :=
-Profinite.extend (fintype_functor.{u} r')
+Profinite.extend (Laurent_measures.{u} r')
 
 @[simps] def profiniteCH : Profinite.{u} ⥤ CompHausFiltPseuNormGrp₁.{u} :=
-Profinite.extend (fintype_functor.{u} r' ⋙ to_CompHausFiltPseuNormGrp₁.{u} r')
+Profinite.extend (Laurent_measures.{u} r' ⋙ to_CompHausFiltPseuNormGrp₁.{u} r')
 
 def profinite_comp_to_CompHausFiltPseuNormGrp₁ :
   laurent_measures.profinite.{u} r' ⋙ to_CompHausFiltPseuNormGrp₁.{u} r' ≅
