75f2d03a5293781e074fc3cb213b019b22be82da
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-04-30 17:05:48 +0100

    ses compiling

--- src/Lbar/ses.lean
+++ src/Lbar/ses.lean
@@ -450,12 +450,12 @@ to its positive terms in `Tℤ[[T]]`. -/
   end }
 
 @[simps]
-def to_Lbar_nat_trans : laurent_measures.Fintype.Laurent_measures r' ⟶ Lbar.Fintype.Laurent_measures r' :=
+def to_Lbar_nat_trans : Fintype.Laurent_measures r' ⟶ Lbar.Laurent_measures r' :=
 { app := λ S, to_Lbar_hom r' S,
   naturality' := λ S₁ S₂ f, begin
     ext,
     simp only [Fintype.Laurent_measures_map, category_theory.comp_apply, to_Lbar_hom_to_fun, to_Lbar_to_fun,
-      Lbar.Fintype.Laurent_measures_map_to_fun, Lbar.map_to_fun, map_hom, map_apply,
+      Lbar.Laurent_measures_map_to_fun, Lbar.map_to_fun, map_hom, map_apply,
       comphaus_filtered_pseudo_normed_group_with_Tinv_hom.coe_mk],
     split_ifs, { simp only [finset.sum_const_zero], }, { refl }
   end }
