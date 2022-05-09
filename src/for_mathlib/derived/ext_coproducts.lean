0cd5ac632fe18560e3db5b77550cce845533c6a1
Adam Topaz <github@adamtopaz.com>
2022-05-09 17:24:32 -0600

    coprods

--- src/for_mathlib/derived/ext_coproducts.lean
+++ src/for_mathlib/derived/ext_coproducts.lean
@@ -1,4 +1,5 @@
 import for_mathlib.derived.K_projective
+import category_theory.limits.preserves.limits
 
 open category_theory
 open category_theory.limits
@@ -10,12 +11,37 @@ open_locale zero_object
 
 namespace homotopy_category
 
+noncomputable
 def coproduct_iso {α : Type v} (X : α → cochain_complex A ℤ) (i) :
-  (sigma_obj X).X i ≅ sigma_obj (λ a : α, (X a).X i) := sorry
+  (sigma_obj X).X i ≅ sigma_obj (λ a : α, (X a).X i) :=
+(category_theory.preserves_colimit_iso (homological_complex.eval A
+  (complex_shape.up ℤ) i) (discrete.functor X)) ≪≫
+  has_colimit.iso_of_nat_iso
+  (nat_iso.of_components (λ _, iso.refl _) begin
+    rintros i _ ⟨⟨⟨⟩⟩⟩,
+    simp,
+  end)
 
-lemma coproduct_iso_inv_f {α : Type v} (X : α → cochain_complex A ℤ) (i) (Y)
-  (f : sigma_obj X ⟶ Y) (a):
-  sigma.ι _ a ≫ (coproduct_iso X i).inv ≫ f.f i = (sigma.ι _ a ≫ f).f i := sorry
+@[simp, reassoc]
+lemma coproduct_ι_coproduct_iso_inv {α : Type v} (X : α → cochain_complex A ℤ) (i) (a) :
+  sigma.ι _ a ≫ (coproduct_iso X i).inv = ((sigma.ι X a : _ ⟶ _)).f i :=
+begin
+  dsimp [coproduct_iso, has_colimit.iso_of_nat_iso,
+    is_colimit.map, preserves_colimit_iso, is_colimit.cocone_point_unique_up_to_iso],
+  simp, dsimp, simp,
+end
+
+@[simp, reassoc]
+lemma coproduct_ι_coproduct_iso_hom {α : Type v} (X : α → cochain_complex A ℤ) (i) (a : α) :
+  (sigma.ι X a : _ ⟶ _).f i ≫ (coproduct_iso X i).hom = sigma.ι _ a :=
+begin
+  dsimp [coproduct_iso, has_colimit.iso_of_nat_iso,
+    is_colimit.map, preserves_colimit_iso, is_colimit.cocone_point_unique_up_to_iso],
+  slice_lhs 0 1
+  { erw (is_colimit_of_preserves (homological_complex.eval A (complex_shape.up ℤ) i)
+    (colimit.is_colimit (discrete.functor X))).fac },
+  simp, dsimp, simp,
+end
 
 noncomputable
 def homotopy_coprod {α : Type v} (X : α → cochain_complex A ℤ) (Y)
@@ -34,16 +60,27 @@ def homotopy_coprod {α : Type v} (X : α → cochain_complex A ℤ) (Y)
     intros i,
     rw ← cancel_epi (coproduct_iso X i).inv,
     apply colimit.hom_ext, intros a,
-    simp_rw [coproduct_iso_inv_f, (h a).comm, preadditive.comp_add,
-      coproduct_iso_inv_f],
-    congr' 1,
-    sorry
+    simp only [coproduct_ι_coproduct_iso_inv_assoc, homological_complex.cochain_complex_d_next,
+      homological_complex.cochain_complex_prev_d, category.assoc, preadditive.comp_add,
+      homological_complex.hom.comm_assoc, coproduct_ι_coproduct_iso_hom_assoc,
+      colimit.ι_desc, cofan.mk_ι_app, colimit.ι_desc_assoc],
+    have := (h a).comm i,
+    dsimp at this,
+    rw this,
+    simp only [homological_complex.cochain_complex_d_next,
+      homological_complex.cochain_complex_prev_d],
   end }
 
 lemma homotopic_coprod {α : Type v} (X : α → cochain_complex A ℤ) (Y)
   (f g : sigma_obj X ⟶ Y)
   (h : ∀ a : α, homotopic _ _ (sigma.ι _ a ≫ f) (sigma.ι _ a ≫ g)) :
-  homotopic _ _ f g := sorry
+  homotopic _ _ f g :=
+begin
+  constructor,
+  apply homotopy_coprod,
+  intros a,
+  exact (h a).some,
+end
 
 -- Move this
 lemma homotopic_of_quotient_map_eq {X Y : cochain_complex A ℤ}
@@ -53,38 +90,45 @@ begin
   erw quotient.functor_map_eq_iff at h, assumption,
 end
 
+noncomputable
+def colimit_cofan {α : Type v} (X : α → homotopy_category A (complex_shape.up ℤ)) :
+  cofan X :=
+cofan.mk
+((quotient _ _).obj $ sigma_obj (λ a, (X a).as))
+(λ a, (quotient _ _).map $ sigma.ι _ a)
+
+noncomputable
+def is_colimit_cofan {α : Type v} (X : α → homotopy_category A (complex_shape.up ℤ)) :
+  is_colimit (colimit_cofan X) :=
+{ desc := λ S, (quotient _ _).map $ sigma.desc $ λ a, (S.ι.app a).out,
+  fac' := begin
+    intros S j,
+    dsimp,
+    erw [← (quotient A (complex_shape.up ℤ)).map_comp, colimit.ι_desc],
+    dsimp [quotient],
+    simp,
+  end,
+  uniq' := begin
+    intros S m hm,
+    let mm := m.out,
+    have : (quotient _ _).map mm = m, by simp,
+    rw ← this,
+    apply quot.sound,
+    apply quotient.comp_closure.of,
+    apply homotopic_coprod,
+    intros a,
+    specialize hm a, rw ← this at hm, dsimp at hm,
+    erw [← (quotient A (complex_shape.up ℤ)).map_comp] at hm,
+    erw colimit.ι_desc,
+    dsimp,
+    have : S.ι.app a = (quotient _ _).map (S.ι.app a).out, by simp,
+    rw this at hm,
+    apply homotopic_of_quotient_map_eq,
+    exact hm
+  end }
+
 instance {α : Type v} (X : α → homotopy_category A (complex_shape.up ℤ)) :
   has_coproduct X :=
-{ exists_colimit := nonempty.intro $
-  { cocone := cofan.mk
-      ((quotient _ _).obj $ sigma_obj (λ a, (X a).as))
-      (λ a, (quotient _ _).map $ sigma.ι _ a),
-    is_colimit :=
-    { desc := λ S, (quotient _ _).map $ sigma.desc $ λ a, (S.ι.app a).out,
-      fac' := by sorry ; begin
-        intros S j,
-        dsimp,
-        erw [← (quotient A (complex_shape.up ℤ)).map_comp, colimit.ι_desc],
-        dsimp [quotient],
-        simp,
-      end,
-      uniq' := begin
-        intros S m hm,
-        let mm := m.out,
-        have : (quotient _ _).map mm = m, by simp,
-        rw ← this,
-        apply quot.sound,
-        apply quotient.comp_closure.of,
-        apply homotopic_coprod,
-        intros a,
-        specialize hm a, rw ← this at hm, dsimp at hm,
-        erw [← (quotient A (complex_shape.up ℤ)).map_comp] at hm,
-        erw colimit.ι_desc,
-        dsimp,
-        have : S.ι.app a = (quotient _ _).map (S.ι.app a).out, by simp,
-        rw this at hm,
-        apply homotopic_of_quotient_map_eq,
-        exact hm
-      end } } }
+{ exists_colimit := nonempty.intro $ ⟨colimit_cofan _, is_colimit_cofan _⟩ }
 
 end homotopy_category
