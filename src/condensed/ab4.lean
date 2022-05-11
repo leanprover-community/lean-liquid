3eeaf669ec354673d17acb313bddeab93118ccbf
Adam Topaz <github@adamtopaz.com>
2022-05-11 14:51:50 -0600

    annoying

--- src/condensed/ab4.lean
+++ src/condensed/ab4.lean
@@ -2,6 +2,7 @@ import for_mathlib.ab4
 import condensed.extr.equivalence
 import category_theory.limits.preserves.limits
 import category_theory.adjunction.evaluation
+import algebra.group.ulift
 
 .
 
@@ -21,7 +22,72 @@ has_limits_of_has_limits_creates_limits
 instance : has_colimits_of_size.{u} Ab.{u+1} :=
 has_colimits_of_size_shrink.{u u (u+1) (u+1)} Ab.{u+1}
 
-instance : AB4 Ab.{u+1} := sorry
+def Ab.pt {X : Ab.{u}} (x : X) :
+  AddCommGroup.of (ulift.{u} ℤ) ⟶ X :=
+{ to_fun := λ a, zmultiples_add_hom _ x a.down,
+  map_zero' := by simp,
+  map_add' := λ a b, by rw [ulift.add_down, add_monoid_hom.map_add] }
+
+lemma Ab.pt_eq {X : Ab.{u}} (x : X) :
+  Ab.pt x ⟨1⟩ = x := by { dsimp [Ab.pt], simp }
+
+lemma AddCommGroup.injective_of_mono' {X Y : Ab.{u}} (f : X ⟶ Y) [mono f] :
+  function.injective f :=
+begin
+  intros a b h,
+  rw [← Ab.pt_eq a, ← Ab.pt_eq b] at *,
+  simp_rw ← comp_apply at h,
+  congr' 1,
+  sorry
+end
+
+open_locale classical
+
+noncomputable
+def Ab.cofan {α : Type (u+1)} (X : α → Ab.{u+1}) :
+  cofan X :=
+cofan.mk
+(AddCommGroup.of $ Π₀ x, X x)
+(λ a, dfinsupp.single_add_hom (λ x, X x) a)
+
+noncomputable
+def Ab.is_colimit_cofan {α : Type (u+1)} (X : α → Ab.{u+1}) :
+  is_colimit (Ab.cofan X) :=
+{ desc := λ S, dfinsupp.lift_add_hom
+    (λ i, let e : X i ⟶ S.X := S.ι.app i in e),
+  fac' := sorry,
+  uniq' := sorry }
+
+instance : AB4 Ab.{u+1} :=
+begin
+  constructor,
+  introsI α X Y f hf,
+  let t := _, change mono t,
+  let eX : (∐ λ (a : α), X a) ≅ (Ab.cofan X).X :=
+    (colimit.is_colimit _).cocone_point_unique_up_to_iso (Ab.is_colimit_cofan X),
+  let eY : (∐ λ (a : α), Y a) ≅ (Ab.cofan Y).X :=
+    (colimit.is_colimit _).cocone_point_unique_up_to_iso (Ab.is_colimit_cofan Y),
+  let q : (Ab.cofan X).X ⟶ (Ab.cofan Y).X :=
+    (Ab.is_colimit_cofan X).desc ⟨(Ab.cofan Y).X,
+    λ a, f a ≫ (Ab.cofan Y).ι.app a, _⟩,
+  swap, { sorry },
+  haveI : mono q,
+  { apply concrete_category.mono_of_injective,
+    rintros (u v : Π₀ x, X x) h, ext w,
+    dsimp [q, Ab.is_colimit_cofan, Ab.cofan] at h,
+    apply_fun (λ e, (e : Π₀ w, Y w) w) at h,
+    simp_rw dfinsupp.sum_add_hom_apply at h,
+    apply_fun f w,
+    swap, { sorry },
+    let q : Π i, Y i → Π₀ i, Y i := dfinsupp.single,
+    change u.sum (λ i, q i ∘ f i) w = v.sum (λ i, q i ∘ f i) w at h,
+    -- convert h,
+    sorry
+  },
+  suffices : t = eX.hom ≫ q ≫ eY.inv,
+  { rw this, apply_instance },
+  sorry
+end
 
 instance {C : Type (u+1)} [category.{u} C] : AB4 (C ⥤ Ab.{u+1}) :=
 begin
