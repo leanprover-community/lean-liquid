87d35c9060916c7bcca22806cf46813b146db7a9
Adam Topaz <github@adamtopaz.com>
2022-05-11 13:46:00 -0600

    almost there with AB4

--- src/condensed/ab4.lean
+++ src/condensed/ab4.lean
@@ -1,4 +1,83 @@
 import for_mathlib.ab4
 import condensed.extr.equivalence
+import category_theory.limits.preserves.limits
+import category_theory.adjunction.evaluation
 
 .
+
+universe u
+
+open category_theory
+open category_theory.limits
+
+instance : has_colimits (ExtrSheafProd.{u} Ab.{u+1}) :=
+has_colimits_of_has_colimits_creates_colimits
+(Condensed_ExtrSheafProd_equiv _).inverse
+
+instance : has_limits (ExtrSheafProd.{u} Ab.{u+1}) :=
+has_limits_of_has_limits_creates_limits
+(Condensed_ExtrSheafProd_equiv _).inverse
+
+instance : has_colimits_of_size.{u} Ab.{u+1} := sorry
+
+instance : AB4 Ab.{u+1} := sorry
+
+instance {C : Type (u+1)} [category.{u} C] : AB4 (C ⥤ Ab.{u+1}) :=
+begin
+  constructor,
+  intros α X Y f hf,
+  let t := _, change mono t,
+  rw category_theory.nat_trans.mono_iff_app_mono,
+  intros c,
+  let eX : (∐ λ (a : α), X a).obj c ≅ (∐ λ (a : α), (X a).obj c) :=
+    (is_colimit_of_preserves ((evaluation _ _).obj c)
+      (colimit.is_colimit _)).cocone_point_unique_up_to_iso (colimit.is_colimit _)
+    ≪≫ has_colimit.iso_of_nat_iso (discrete.nat_iso (λ _, iso.refl _)),
+   let eY : (∐ λ (a : α), Y a).obj c ≅ (∐ λ (a : α), (Y a).obj c) :=
+    (is_colimit_of_preserves ((evaluation _ _).obj c)
+      (colimit.is_colimit _)).cocone_point_unique_up_to_iso (colimit.is_colimit _)
+    ≪≫ has_colimit.iso_of_nat_iso (discrete.nat_iso (λ _, iso.refl _)),
+  let tt : (∐ λ (a : α), (X a).obj c) ⟶ (∐ λ (a : α), (Y a).obj c) :=
+    sigma.desc (λ a, (f a).app c ≫ sigma.ι _ a),
+  haveI : mono tt,
+  { dsimp [tt],
+    apply AB4.cond, intros a, specialize hf a,
+    rw category_theory.nat_trans.mono_iff_app_mono at hf,
+    apply hf },
+  suffices : t.app c = eX.hom ≫ tt ≫ eY.inv,
+  { rw this, apply_instance },
+  dsimp [eX, tt, t, eY],
+  apply ((is_colimit_of_preserves ((evaluation _ _).obj c) (colimit.is_colimit _))).hom_ext,
+  intros a,
+  dsimp [is_colimit.cocone_point_unique_up_to_iso],
+  simp only [category.assoc, ← nat_trans.comp_app, colimit.ι_desc],
+  slice_rhs 0 1
+  { erw ((is_colimit_of_preserves ((evaluation _ _).obj c) (colimit.is_colimit _))).fac },
+  slice_rhs 0 1
+  { erw colimit.ι_desc },
+  dsimp,
+  simp only [category.id_comp, colimit.ι_desc, cofan.mk_ι_app, category.assoc,
+    has_colimit.iso_of_nat_iso_ι_inv, discrete.nat_iso_inv_app, functor.map_cocone_ι_app,
+    colimit.cocone_ι, evaluation_obj_map],
+  dsimp, simp, apply_instance
+end
+
+noncomputable
+instance : creates_limits (ExtrSheafProd_to_presheaf.{u} Ab.{u+1}) :=
+begin
+  change creates_limits ((ExtrSheaf_ExtrSheafProd_equiv _).inverse ⋙
+    Sheaf_to_presheaf _ _),
+  apply_with category_theory.comp_creates_limits { instances := ff },
+  apply_instance,
+  exact @Sheaf.category_theory.Sheaf_to_presheaf.category_theory.creates_limits.{(u+2) u (u+1)}
+    ExtrDisc.{u} _ ExtrDisc.proetale_topology Ab.{u+1} _ _,
+end
+
+instance : AB4 (ExtrSheafProd.{u} Ab.{u+1}) :=
+AB4_of_preserves_colimits_of_reflects_limits_of_AB4
+  (ExtrSheafProd_to_presheaf _)
+
+-- TODO: Prove a lemma about transporting AB4 across an equivalence.
+instance : AB4 (Condensed.{u} Ab.{u+1}) :=
+AB4_of_preserves_colimits_of_reflects_limits_of_AB4
+    (Condensed_ExtrSheafProd_equiv.{u} Ab.{u+1}).functor
--- src/for_mathlib/ab4.lean
+++ src/for_mathlib/ab4.lean
@@ -44,6 +44,51 @@ begin
   rwa cancel_epi at h,
 end
 
+lemma AB4_of_preserves_colimits_of_reflects_limits_of_AB4
+  {A B : Type u} [category.{v} A] [category.{v} B]
+  [has_coproducts A]
+  [has_coproducts B]
+  (F : A ⥤ B)
+  [preserves_colimits F]
+  [creates_limits F]
+  [has_limits B]
+  [AB4 B] : AB4 A :=
+begin
+  constructor, introsI a X Y f hf,
+  let t := _, change mono t,
+  suffices : mono (F.map t),
+  { resetI, apply reflects_mono F },
+  let eX : F.obj (∐ λ (a : a), X a) ≅ (∐ λ a, F.obj (X a)) :=
+    (is_colimit_of_preserves F (colimit.is_colimit _)).cocone_point_unique_up_to_iso
+      (colimit.is_colimit _) ≪≫ has_colimit.iso_of_nat_iso
+      (nat_iso.of_components (λ _, iso.refl _) _),
+  swap, { rintro i _ ⟨⟨⟨⟩⟩⟩, dsimp, simp, dsimp, simp },
+  let eY : F.obj (∐ λ (a : a), Y a) ≅ (∐ λ a, F.obj (Y a)) :=
+    (is_colimit_of_preserves F (colimit.is_colimit _)).cocone_point_unique_up_to_iso
+      (colimit.is_colimit _) ≪≫ has_colimit.iso_of_nat_iso
+      (nat_iso.of_components (λ _, iso.refl _) _),
+  swap, { rintro i _ ⟨⟨⟨⟩⟩⟩, dsimp, simp, dsimp, simp },
+  let tt : (∐ λ a, F.obj (X a)) ⟶ (∐ λ a, F.obj (Y a)) :=
+    sigma.desc (λ a, F.map (f a) ≫ sigma.ι _ a),
+  haveI : mono tt,
+  { apply AB4.cond, intros a,
+    apply category_theory.preserves_mono F },
+  suffices : F.map t = eX.hom ≫ tt ≫ eY.inv,
+  { rw this, apply mono_comp },
+  apply (is_colimit_of_preserves F (colimit.is_colimit _)).hom_ext,
+  swap, apply_instance,
+  intros i,
+  erw [← F.map_comp, colimit.ι_desc, F.map_comp],
+  dsimp [eX, tt, t, eY, limits.is_colimit.cocone_point_unique_up_to_iso, is_colimit_of_preserves,
+    has_colimit.iso_of_nat_iso, is_colimit.map],
+  slice_rhs 0 1
+  { erw (is_colimit_of_preserves F (colimit.is_colimit _)).fac },
+  slice_rhs 0 1
+  { erw colimit.ι_desc },
+  dsimp [iso.refl],
+  simp,
+end
+
 -- TODO: exactness of `sigma_functor`. Mono and Epi conditions done.
 -- TODO: homology commutes with coproducts given AB4.
 -- TODO: etc...
