33ed21216d6238df8fe53dabe4c9ef8b276a0fa7
Adam Topaz <github@adamtopaz.com>
2022-05-16 11:26:46 -0600

    really fix...

--- src/for_mathlib/ab4.lean
+++ src/for_mathlib/ab4.lean
@@ -135,7 +135,7 @@ def is_colimit_homotopy_category_homology_functor_map_cocone
 
 noncomputable
 instance homotopy_category_homology_functor_preserves_coproducts
-  (M : Type*) (S : complex_shape M) (α : Type v)
+  (α : Type v)
   [abelian A] [has_coproducts A] [AB4 A] (i) :
   preserves_colimits_of_shape (discrete α)
   (homotopy_category.homology_functor A (complex_shape.up ℤ) i) :=
