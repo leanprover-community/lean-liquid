f7fd93a47a7d2d8260837c1c191544b3951285a5
Markus Himmel <markus@himmel-villmar.de>
2022-05-26 09:54:15 +0200

    mono_lift and epi_desc

--- src/for_mathlib/exact_lift_desc.lean
+++ src/for_mathlib/exact_lift_desc.lean
@@ -15,9 +15,9 @@ section lift
 
 variables (h : exact f g) [mono f] (φ : T ⟶ B) (w : φ ≫ g = 0)
 
--- SELFCONTAINED
 def mono_lift (h : exact f g) [mono f] (φ : T ⟶ B) (w : φ ≫ g = 0) : T ⟶ A :=
-abelian.mono_lift f φ sorry
+abelian.mono_lift f φ $
+  by { obtain ⟨t, rfl⟩ := kernel.lift' _ _ w, simp [kernel_comp_cokernel _ _ h] }
 
 @[reassoc] lemma mono_lift_comp : h.mono_lift φ w ≫ f = φ := abelian.mono_lift_comp f φ _
 
@@ -30,9 +30,9 @@ section desc
 
 variables (h : exact f g) [category_theory.epi g] (φ : B ⟶ T) (w : f ≫ φ = 0)
 
--- SELFCONTAINED
 def epi_desc (h : exact f g) [category_theory.epi g] (φ : B ⟶ T) (w : f ≫ φ = 0) : C ⟶ T :=
-abelian.epi_desc g φ sorry
+abelian.epi_desc g φ $
+  by { obtain ⟨t, rfl⟩ := cokernel.desc' _ _ w, simp [kernel_comp_cokernel_assoc _ _ h] }
 
 @[reassoc] lemma comp_epi_desc : g ≫ h.epi_desc φ w = φ := abelian.comp_epi_desc g φ _
 
