02a9055165efad94aec629465c1caadcb3ce0833
Johan Commelin <johan@commelin.net>
2022-05-27 15:11:02 +0000

    better inductive step

--- src/for_mathlib/exact_seq4.lean
+++ src/for_mathlib/exact_seq4.lean
@@ -8,13 +8,13 @@ open category_theory.limits
 
 variables {𝓐 : Type*} [category 𝓐] [abelian 𝓐]
 
--- SELFCONTAINED
-lemma exact_seq.is_iso_of_is_zero_of_is_zero
-  {A B C D : 𝓐} {f : A ⟶ B} {g : B ⟶ C} {h : C ⟶ D} {L : list (arrow 𝓐)}
-  (H : exact_seq 𝓐 (f::g::h::L)) (hA : is_zero A) (hD : is_zero D) :
-  is_iso g :=
-begin
-  sorry
-end
+-- -- SELFCONTAINED
+-- lemma exact_seq.is_iso_of_is_zero_of_is_zero
+--   {A B C D : 𝓐} {f : A ⟶ B} {g : B ⟶ C} {h : C ⟶ D} {L : list (arrow 𝓐)}
+--   (H : exact_seq 𝓐 (f::g::h::L)) (hA : is_zero A) (hD : is_zero D) :
+--   is_iso g :=
+-- begin
+--   admit
+-- end
 
 end category_theory
