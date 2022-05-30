24453b201c54f88bd24c060926771b429659d23e
Adam Topaz <github@adamtopaz.com>
2022-05-30 07:59:02 -0600

    unsheafified stuff

--- src/condensed/acyclic.lean
+++ src/condensed/acyclic.lean
@@ -17,11 +17,25 @@ open function (surjective)
 
 namespace condensed
 
+set_option pp.universes true
+
+def unsheafified_free_Cech' (F : arrow Profinite.{u}) :
+  chain_complex (Profinite.{u}ᵒᵖ ⥤ Ab.{u+1}) ℕ :=
+simplicial_object.augmented.to_complex $
+(((simplicial_object.augmented.whiskering _ _).obj
+  (yoneda ⋙ (whiskering_right _ _ _).obj (AddCommGroup.free ⋙ Ab.ulift.{(u+1) u}))).obj
+  F.augmented_cech_nerve)
+
 def free_Cech' (F : arrow Profinite.{u}) :
   chain_complex (Condensed.{u} Ab.{u+1}) ℕ :=
 (((simplicial_object.augmented.whiskering _ _).obj
   (Profinite_to_Condensed ⋙ CondensedSet_to_Condensed_Ab)).obj
   F.augmented_cech_nerve).to_complex
+
+def unsheafified_free_Chech (F : arrow Profinite.{u}) :
+  chain_complex (Profinite.{u}ᵒᵖ ⥤ Ab.{u+1}) ℤ :=
+(homological_complex.embed $ complex_shape.embedding.nat_down_int_down).obj
+  (unsheafified_free_Cech' F)
 
 def free_Cech (F : arrow Profinite.{u}) :
   chain_complex (Condensed.{u} Ab.{u+1}) ℤ :=
