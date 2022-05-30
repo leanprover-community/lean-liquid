9ddcac55c222d7ee58b55be0882ad26f85ac7690
Adam Topaz <github@adamtopaz.com>
2022-05-30 11:12:50 -0400

    kill some sorry

--- src/for_mathlib/chain_complex_exact.lean
+++ src/for_mathlib/chain_complex_exact.lean
@@ -15,10 +15,36 @@ variables (C : chain_complex A ℤ)
 
 /- This whole file is SELFCONTAINED -/
 
+lemma _root_.category_theory.is_zero_homology_iff_exact
+  {X Y Z : A} (f : X ⟶ Y) (g : Y ⟶ Z) (w : f ≫ g = 0) :
+  is_zero (homology f g w) ↔ exact f g :=
+begin
+  rw preadditive.exact_iff_homology_zero,
+  split,
+  { intros h,
+    use [w, h.iso_zero] },
+  { rintros ⟨w,⟨h⟩⟩,
+    apply is_zero.of_iso _ h,
+    exact is_zero_zero _ }
+end
+
 lemma exact_of_is_zero_homology
   (h : ∀ i : ℤ, is_zero (C.homology i)) :
   ∀ i j k : ℤ, (complex_shape.down ℤ).rel i j → (complex_shape.down ℤ).rel j k →
-  exact (C.d i j) (C.d j k) := sorry
+  exact (C.d i j) (C.d j k) :=
+begin
+  rintros i j k ⟨rfl⟩ ⟨rfl⟩,
+  dsimp,
+  specialize h (k+1),
+  erw _root_.category_theory.is_zero_homology_iff_exact at h,
+  let e : C.X_prev (k+1) ≅ C.X (k+1+1) :=
+    C.X_prev_iso rfl,
+  let q : C.X_next (k+1) ≅ C.X k :=
+    C.X_next_iso rfl,
+  suffices : exact (e.hom ≫ C.d (k+1+1) (k+1)) (C.d (k+1) k ≫ q.inv),
+  { simpa using this },
+  dsimp [e,q], rwa [← C.d_to_eq, ← C.d_from_eq],
+end
 
 lemma is_zero_homology_of_exact
   (h : ∀ i j k : ℤ, (complex_shape.down ℤ).rel i j → (complex_shape.down ℤ).rel j k →
