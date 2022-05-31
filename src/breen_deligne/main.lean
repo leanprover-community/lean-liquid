26e20ab6572aeeb36b944eb4c6a293fd307045b8
Johan Commelin <johan@commelin.net>
2022-05-31 10:36:54 +0000

    wip

--- src/breen_deligne/main.lean
+++ src/breen_deligne/main.lean
@@ -11,7 +11,7 @@ universes v u
 
 open_locale big_operators
 
-open category_theory category_theory.limits
+open category_theory category_theory.limits opposite
 open bounded_homotopy_category (Ext single)
 
 namespace breen_deligne
@@ -21,31 +21,70 @@ variables (BD : package)
 variables {𝓐 : Type u} [category.{v} 𝓐] [abelian 𝓐] [enough_projectives 𝓐]
 variables (F : 𝓐 ⥤ 𝓐) --[preserves_filtered_colimits F]
 
+namespace main_lemma
+
+variables (A : 𝓐) (B : 𝓐) (j : ℤ)
+
+def IH  : Prop :=
+  (∀ i ≤ j, is_zero $ ((Ext' i).obj (op A)).obj B) ↔
+  (∀ i ≤ j, is_zero $ ((Ext i).obj (op ((BD.eval F).obj A))).obj ((single _ 0).obj B))
+
+lemma IH_neg (j : ℤ) (hj : j ≤ 0) (ih : IH BD F A B j) : IH BD F A B (j - 1) :=
+begin
+  split; intros _ _ hij,
+  { apply Ext_single_right_is_zero _ _ 1 _ _ (chain_complex.bounded_by_one _),
+    linarith only [hj, hij] },
+  { apply Ext'_is_zero_of_neg, linarith only [hj, hij] }
+end
+
+lemma IH_0 : IH BD F A B 0 :=
+begin
+  apply forall_congr, intro i, apply forall_congr, intro hi0,
+  rw [le_iff_lt_or_eq] at hi0, rcases hi0 with (hi0|rfl),
+  { split; intro,
+    { apply Ext_single_right_is_zero _ _ 1 _ _ (chain_complex.bounded_by_one _),
+      linarith only [hi0] },
+    { apply Ext'_is_zero_of_neg, linarith only [hi0] } },
+  apply iso.is_zero_iff,
+  -- this can probaby be simplified further,
+  -- but ultimately, we need the assumption that `H₀((BD.eval F).obj A)` is isom to `A`
+  sorry
+end
+
+
+lemma bdd_step (j : ℤ) (hj : 0 ≤ j) (ih : IH BD F A B j) : IH BD F A B (j + 1) :=
+sorry
+
 -- This requires more hypotheses on `BD` and `F`.
 -- We'll figure them out while proving the lemma.
 -- These extra hypotheses are certainly satisfies by
 -- `BD = breen_deligne.package.eg` and
 -- `F` = "free condensed abelian group"
 -- Also missing: the condition that `A` is torsion free.
-lemma main_lemma_bdd (A : 𝓐ᵒᵖ) (B : 𝓐) (j : ℤ) :
-  (∀ i ≤ j, is_zero $ ((Ext' i).obj A).obj B) ↔
-  (∀ i ≤ j, is_zero $ ((Ext i).obj ((BD.eval F).op.obj A)).obj ((single _ 0).obj B)) :=
-sorry
+lemma bdd (A : 𝓐) (B : 𝓐) (j : ℤ) : IH BD F A B j :=
+begin
+  apply int.induction_on' j,
+  { exact IH_0 BD F A B },
+  { exact bdd_step BD F A B },
+  { exact IH_neg BD F A B, },
+end
 
-lemma main_lemma_is_zero (A : 𝓐ᵒᵖ) (B : 𝓐) :
-  (∀ i, is_zero $ ((Ext' i).obj A).obj B) ↔
-  (∀ i, is_zero $ ((Ext i).obj ((BD.eval F).op.obj A)).obj ((single _ 0).obj B)) :=
+lemma is_zero (A : 𝓐) (B : 𝓐) :
+  (∀ i, is_zero $ ((Ext' i).obj (op A)).obj B) ↔
+  (∀ i, is_zero $ ((Ext i).obj (op ((BD.eval F).obj A))).obj ((single _ 0).obj B)) :=
 begin
   split,
   { intros H j,
-    refine (main_lemma_bdd BD F A B j).mp _ j le_rfl,
+    refine (bdd BD F A B j).mp _ j le_rfl,
     intros i hij,
     apply H },
   { intros H j,
-    refine (main_lemma_bdd BD F A B j).mpr _ j le_rfl,
+    refine (bdd BD F A B j).mpr _ j le_rfl,
     intros i hij,
     apply H }
 end
+
+end main_lemma
 
 section
 
@@ -66,7 +105,7 @@ begin
   rw [← f.op_unop, ← endomorphisms.Ext'_is_zero_iff' A B f.unop g, (BD.eval F).op_map, f.op_unop],
   dsimp,
   rw [← endomorphisms.Ext_is_zero_iff'],
-  refine (main_lemma_is_zero BD F.map_endomorphisms _ _).trans _,
+  refine (main_lemma.is_zero BD F.map_endomorphisms _ _).trans _,
   apply forall_congr, intro i,
   apply iso.is_zero_iff,
   refine functor.map_iso _ _ ≪≫ iso.app (functor.map_iso _ _) _,
