3a8f92085b73823a972c3e305abfb75e0936f74b
Adam Topaz <github@adamtopaz.com>
2022-06-04 07:04:09 -0400

    wip

--- src/condensed/tensor.lean
+++ src/condensed/tensor.lean
@@ -1,4 +1,6 @@
 import condensed.adjunctions
+import condensed.extr.equivalence
+import linear_algebra.tensor_product
 
 noncomputable theory
 
@@ -7,6 +9,21 @@ universes u
 open category_theory
 
 namespace condensed
+
+open_locale tensor_product
+
+def tensor_ExtrSheafProd_presheaf (M : ExtrDisc.{u}ᵒᵖ ⥤ Ab.{u+1}) (A : Ab.{u+1}) :
+  ExtrDisc.{u}ᵒᵖ ⥤ Ab.{u+1} :=
+{ obj := λ S, AddCommGroup.of $ M.obj S ⊗[ℤ] A,
+  map := λ S T f, (tensor_product.map (M.map f).to_int_linear_map $
+    linear_map.id).to_add_monoid_hom,
+  map_id' := sorry,
+  map_comp' := sorry }
+
+def tensor_ExtrSheafProd (M : ExtrSheafProd.{u} Ab.{u+1}) (A : Ab.{u+1}) :
+  ExtrSheafProd.{u} Ab.{u+1} :=
+{ val := tensor_ExtrSheafProd_presheaf M.val A,
+  cond := sorry } -- tensor product commutes with direct sums.
 
 /-- This is the functor that sends `A : Ab` to `M ⊗ A`,
 where `M` is a fixed condensed abelian group. -/
