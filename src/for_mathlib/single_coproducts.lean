26c986e47d420a18e094ffa23cad0fa86d7d8545
Johan Commelin <johan@commelin.net>
2022-06-06 16:49:23 +0000

    fix one sorry

--- src/for_mathlib/single_coproducts.lean
+++ src/for_mathlib/single_coproducts.lean
@@ -11,10 +11,30 @@ variables {A : Type u} [category.{v} A] [abelian A]
 namespace bounded_homotopy_category
 
 instance uniformly_bounded_single {α : Type v} (i : ℤ) (X : α → A) :
-  uniformly_bounded (λ a : α, (single A i).obj (X a)) := sorry
+  uniformly_bounded (λ a : α, (single A i).obj (X a)) :=
+begin
+  refine ⟨⟨i+1, λ a k hk, _⟩⟩,
+  dsimp only [single, homotopy_category.single, functor.comp_obj, function.comp_app,
+    homotopy_category.quotient_obj_as, homological_complex.single],
+  rw if_neg,
+  { exact is_zero_zero _ },
+  { rintro rfl, linarith only [hk] }
+end
 
-instance preserves_coproducts_single {α : Type v} (i : ℤ) :
-  preserves_colimits_of_shape (discrete α) (single A i) := sorry
+instance preserves_coproducts_single {α : Type v}
+  [has_coproducts_of_shape α A] [has_coproducts_of_shape α (bounded_homotopy_category A)]
+  (i : ℤ) :
+  preserves_colimits_of_shape (discrete α) (single A i) :=
+begin
+  sorry
+  -- refine preserves_coproducts_aux _ _ _,
+  -- { intro X,
+  --   refine ⟨_, sigma.desc (λ a, (single A i).map (sigma.ι _ _)), _, _⟩,
+  --   { sorry },
+  --   { sorry },
+  --   { sorry }, },
+  -- { intros X a, sorry },
+end
 
 variables [enough_projectives A]
 
