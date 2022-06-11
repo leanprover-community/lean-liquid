4a6c093d07031816f1660c06883d523755e8a81c
Adam Topaz <github@adamtopaz.com>
2022-06-10 19:09:49 -0600

    some more api

--- src/for_mathlib/exact_functor.lean
+++ src/for_mathlib/exact_functor.lean
@@ -1,10 +1,37 @@
 import category_theory.abelian.homology
+import for_mathlib.abelian_category
 
 namespace category_theory
 
 universes v v' u u'
 variables {A : Type u} {B : Type u'} [category.{v} A] [category.{v'} B]
   [abelian A] [abelian B] (F : A ⥤ B) [functor.additive F]
+
+lemma exact.mono_desc {X Y Z : A} {f : X ⟶ Y} {g : Y ⟶ Z} (e : exact f g) :
+  mono (limits.cokernel.desc _ _ e.w) :=
+abelian.category_theory.limits.cokernel.desc.category_theory.mono f g e --WAT?
+
+lemma exact.epi_lift {X Y Z : A} {f : X ⟶ Y} {g : Y ⟶ Z} (e : exact f g) :
+  epi (limits.kernel.lift _ _ e.w) :=
+limits.kernel.lift.epi e
+
+lemma exact.epi_of_exact_zero_right {X Y Z : A} {f : X ⟶ Y} (e : exact f (0 : Y ⟶ Z)) :
+  epi f :=
+begin
+  rw abelian.epi_iff_cokernel_π_eq_zero,
+  rw abelian.exact_iff at e,
+  cases e with e1 e2,
+  simpa using e2,
+end
+
+lemma exact.mono_of_exact_zero_left {X Y Z : A} {f : Y ⟶ Z} (e : exact (0 : X ⟶ Y) f) :
+  mono f :=
+begin
+  rw abelian.mono_iff_kernel_ι_eq_zero,
+  rw abelian.exact_iff at e,
+  cases e with e1 e2,
+  simpa using e2,
+end
 
 namespace functor
 
@@ -22,7 +49,39 @@ def cokernel_comparison {X Y : A} (f : X ⟶ Y) :
 cokernel.desc _ (F.map $ cokernel.π _) $
   by rw [← F.map_comp, limits.cokernel.condition, F.map_zero]
 
-instance {X Y : A} (f : X ⟶ Y)  : is_iso (F.cokernel_comparison f) := sorry
+open_locale zero_object
+
+instance epi_of_epi_of_exact {X Y : A} (f : X ⟶ Y)
+  [epi f] : epi (F.map f) :=
+begin
+  have : category_theory.exact f (0 : _ ⟶ 0) := exact_epi_zero f,
+  replace this := exact.cond F _ _ this,
+  simp at this,
+  apply this.epi_of_exact_zero_right,
+end
+
+instance mono_of_mono_of_exact {X Y : A} (f : X ⟶ Y)
+  [mono f] : mono (F.map f) :=
+begin
+  have : category_theory.exact (0 : X ⟶ _) f := exact_zero_left_of_mono X,
+  replace this := exact.cond F _ _ this,
+  simp at this,
+  apply this.mono_of_exact_zero_left,
+end
+
+instance {X Y : A} (f : X ⟶ Y)  : is_iso (F.cokernel_comparison f) :=
+begin
+  have : category_theory.exact (F.map f) (F.map (cokernel.π f)),
+  { apply exact.cond, exact abelian.exact_cokernel f },
+  dsimp [cokernel_comparison],
+  apply_with is_iso_of_mono_of_epi { instances := ff },
+  apply_instance,
+  apply this.mono_desc,
+  constructor, intros Z a b h,
+  apply_fun (λ e, cokernel.π _ ≫ e) at h,
+  simp at h,
+  rwa cancel_epi at h,
+end
 
 def kernel_comparison {X Y : A} (f : X ⟶ Y) :
   F.obj (kernel f) ⟶ kernel (F.map f) :=
