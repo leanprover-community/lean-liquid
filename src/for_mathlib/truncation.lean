328a3985740ac4a7039b1fb9061c303fff3dbfb8
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-06-15 17:13:09 +0100

    (for_mathlib/truncation): back down to 1 sorry -- boss level ;-)

--- src/for_mathlib/truncation.lean
+++ src/for_mathlib/truncation.lean
@@ -604,6 +604,20 @@ def kernel_comp_is_iso {X Y Z : 𝓐} (f : X ⟶ Y) (g : Y ⟶ Z) [is_iso g] :
 def kernel_iso_assoc {A B C D : 𝓐} (f : A ⟶ B) (g : B ⟶ C) (h : C ⟶ D) :
   kernel (f ≫ g ≫ h) ≅ kernel ((f ≫ g) ≫ h) := kernel_iso_of_eq (by rw category.assoc)
 
+@[simp] lemma mono_comp_iso_iff_mono {V : Type*} [category V] {A B C : V} (e : A ≅ B) (f : B ⟶ C) :
+  mono (e.hom ≫ f) ↔ mono f :=
+begin
+  split,
+  { introI h,
+    have := mono_comp e.inv (e.hom ≫ f),
+    simpa using this, },
+  { apply mono_comp, },
+end
+
+@[simp] lemma mono_comp_is_iso_iff_mono {V : Type*} [category V] {A B C : V} (e : A ⟶ B) [is_iso e]
+  (f : B ⟶ C) : mono (e ≫ f) ↔ mono f :=
+mono_comp_iso_iff_mono (as_iso e) _
+
 lemma ι_succ_to_imker_ex_π {i n : ℤ} : epi (kernel.lift ((to_imker C (i + 1)).f n)
   ((ι_succ C i).f n) (ι_succ.comp_to_imker_zero C)) :=
 begin
@@ -695,11 +709,59 @@ begin
           { exact hn.elim rfl, }, },
         { exact h.elim rfl, }, } }, }
 end
+.
+
+lemma mono_of_epi_of_comp_mono {A B C : 𝓐} (f : A ⟶ B) (g : B ⟶ C) [epi f] [mono (f ≫ g)] :
+  mono g :=
+begin
+  haveI : mono f := mono_of_mono f g,
+  haveI : is_iso f := is_iso_of_mono_of_epi _,
+  exact (mono_comp_is_iso_iff_mono f g).mp infer_instance,
+end
+
+lemma ι_succ_to_imker_ι_ex_aux {n : ℤ} : mono (cokernel.desc ((ι_succ C n).f n) ((to_imker C (n + 1)).f n) (ι_succ.comp_to_imker_zero C)) :=
+begin
+  sorry
+end
 
 lemma ι_succ_to_imker_ι_ex {i n : ℤ} : mono (cokernel.desc ((ι_succ C i).f n)
   ((to_imker C (i + 1)).f n) (ι_succ.comp_to_imker_zero C)) :=
 begin
-  sorry
+  by_cases h : n = i,
+  { subst h,
+--    delta ι_succ map_of_le, dsimp,
+    apply ι_succ_to_imker_ι_ex_aux, },
+  { by_cases hni : n < i,
+    { apply mono_of_source_iso_zero,
+      apply is_zero.iso_zero,
+      apply @is_zero_cokernel_of_epi _ _ _ _ _ _ _,
+      dunfold ι_succ,
+      dunfold ι_succ map_of_le, dsimp only,
+      simp [dif_pos hni],
+      apply_instance, },
+    { by_cases hni1 : n = i + 1, -- or aomwrhibf
+      { subst hni1,
+        suffices : mono ((to_imker C (i + 1)).f (i + 1)),
+        { rw ← cokernel.π_desc ((ι_succ C i).f (i + 1)) ((to_imker C (i + 1)).f (i + 1)) (ι_succ.comp_to_imker_zero C) at this,
+          exact @mono_of_epi_of_comp_mono _ _ _ _ _ _ _ _ _ this, },
+        delta to_imker,
+        dsimp only,
+        rw dif_neg (show i + 1 ≠ i + 1 - 1, by linarith),
+        rw dif_pos rfl,
+        simp,
+        apply_instance,
+      },
+      { apply mono_of_source_iso_zero,
+        apply is_zero.iso_zero,
+        apply @is_zero_cokernel_of_epi _ _ _ _ _ _ _,
+        apply epi_of_target_iso_zero,
+        apply is_zero.iso_zero,
+        apply is_zero_X_of_lt,
+        apply lt_of_not_lt_of_ne _ hni1,
+        push_neg at hni ⊢,
+        obtain (hni | rfl) := lt_or_eq_of_le hni,
+        { linarith },
+        { exact h.elim rfl } } } }
 end
 
 def ι_succ_to_imker_has_homology_zero {i n : ℤ} :
