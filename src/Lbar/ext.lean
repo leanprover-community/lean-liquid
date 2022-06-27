d45add48fb03180b6e0ed17c6cb4c85caa5f8982
Johan Commelin <johan@commelin.net>
2022-06-27 13:10:15 +0000

    wip

--- src/Lbar/ext.lean
+++ src/Lbar/ext.lean
@@ -50,12 +50,94 @@ begin
   { apply free_acyclic.{u} _ V i hi },
 end
 
+open_locale zero_object
+
+section
+
+variables {𝓐 : Type*} [category 𝓐] [abelian 𝓐]
+variables {ι : Type*} {c : complex_shape ι}
+variables (C : homological_complex 𝓐 c)
+
+def chain_complex_nat_has_homology_0
+  (C : chain_complex 𝓐 ℕ) :
+  has_homology (C.d 1 0) (0 : _ ⟶ 0) (C.homology 0) :=
+sorry
+
+def homological_complex_has_homology (i j k : ι) (hij : c.rel i j) (hjk : c.rel j k) :
+  has_homology (C.d i j) (C.d j k) (C.homology j) :=
+sorry
+
+abbreviation chain_complex_nat_has_homology {𝓐 : Type*} [category 𝓐] [abelian 𝓐]
+  (C : chain_complex 𝓐 ℕ) (n : ℕ) :
+  has_homology (C.d (n+1+1) (n+1)) (C.d (n+1) n) (C.homology (n+1)) :=
+homological_complex_has_homology C (n+1+1) (n+1) n rfl rfl
+
+abbreviation cochain_complex_int_has_homology {𝓐 : Type*} [category 𝓐] [abelian 𝓐]
+  (C : cochain_complex 𝓐 ℤ) (n : ℤ) :
+  has_homology (C.d n (n+1)) (C.d (n+1) (n+1+1)) (C.homology (n+1)) :=
+homological_complex_has_homology C n (n+1) (n+1+1) rfl rfl
+
+end
+
+def homology_embed_iso {𝓐 : Type*} [category 𝓐] [abelian 𝓐]
+  (C : chain_complex 𝓐 ℕ) : Π (n : ℕ),
+  ((homological_complex.embed complex_shape.embedding.nat_down_int_up).obj C).homology (-n) ≅
+  C.homology n
+| 0 :=
+begin
+  refine has_homology.iso _ (chain_complex_nat_has_homology_0 C),
+  let C' := (homological_complex.embed complex_shape.embedding.nat_down_int_up).obj C,
+  exact cochain_complex_int_has_homology C' (-(1:ℕ):ℤ),
+end
+| 1 :=
+begin
+  refine has_homology.iso _ (chain_complex_nat_has_homology C 0),
+  let C' := (homological_complex.embed complex_shape.embedding.nat_down_int_up).obj C,
+  exact cochain_complex_int_has_homology C' (-(1+1:ℕ):ℤ),
+end
+| (n+1+1) :=
+begin
+  refine has_homology.iso _ (chain_complex_nat_has_homology C (n+1)),
+  let C' := (homological_complex.embed complex_shape.embedding.nat_down_int_up).obj C,
+  exact cochain_complex_int_has_homology C' (-(n+1+1+1:ℕ):ℤ),
+end
+
+def map_homological_complex_embed
+  {𝓐 𝓑 : Type*} [category 𝓐] [abelian 𝓐] [category 𝓑] [abelian 𝓑]
+  (F : 𝓐 ⥤ 𝓑) [F.additive] :
+  (homological_complex.embed complex_shape.embedding.nat_down_int_up) ⋙
+  F.map_homological_complex _ ≅
+  F.map_homological_complex _ ⋙
+  (homological_complex.embed complex_shape.embedding.nat_down_int_up) :=
+nat_iso.of_components (λ C, homological_complex.hom.iso_of_components
+  (λ n, by { rcases n with ((_|n)|n),
+    { exact iso.refl _ },
+    { apply functor.map_zero_object },
+    { exact iso.refl _ }, })
+  begin
+    rintro i j (rfl : _ = _), rcases i with (n|(_|n)),
+    { apply is_zero.eq_of_tgt, apply is_zero_zero, },
+    { erw [category.id_comp, category.comp_id], refl, },
+    { erw [category.id_comp, category.comp_id], refl, },
+  end)
+begin
+  intros C₁ C₂ f, ext ((_|n)|n) : 2,
+  { rw [homological_complex.comp_f, homological_complex.comp_f],
+    dsimp, erw [category.id_comp, category.comp_id], refl, },
+  { apply is_zero.eq_of_tgt, apply is_zero_zero, },
+  { rw [homological_complex.comp_f, homological_complex.comp_f],
+    dsimp, erw [category.id_comp, category.comp_id], refl, },
+end
+
 -- this needs to be functorial in `c`
 def ExtQprime_iso_aux_system_obj (c : ℝ≥0) (n : ℕ) :
   ((Ext n).obj (op $ (QprimeFP r' BD κ M).obj c)).obj ((single _ 0).obj V.to_Cond) ≅
   ((aux_system r' BD ⟨M⟩ (SemiNormedGroup.ulift.{u+1}.obj V) κ).to_AbH n).obj (op c) :=
 Ext_compute_with_acyclic _ _ (ExtQprime_iso_aux_system_aux r' BD κ M V c) _ ≪≫
+begin
+  refine (homology_functor _ _ (-n:ℤ)).map_iso _ ≪≫ _;
   sorry
+end
 
 attribute [reassoc] Ext_compute_with_acyclic_naturality
 
