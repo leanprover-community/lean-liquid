64a7e5d697c18edf2e699832ffef7c32de69c382
Johan Commelin <johan@commelin.net>
2022-07-08 06:46:13 +0000

    wip

--- src/condensed/filtered_colimits_commute_with_finite_limits.lean
+++ src/condensed/filtered_colimits_commute_with_finite_limits.lean
@@ -61,7 +61,60 @@ adjunction.right_adjoint_preserves_limits CondensedSet_presheaf_adjunction
 
 section
 
+noncomputable theory
+
 -- set_option pp.universes true
+
+def _root_.category_theory.functor.map_limit {C D J : Type*}
+  [category C] [category D] [small_category J]
+  (G : C ⥤ D) (F : J ⥤ C)
+  [has_limit F] [has_limit (F ⋙ G)] [preserves_limit F G] :
+  G.obj (limit F) ≅ limit (F ⋙ G) :=
+is_limit.cone_point_unique_up_to_iso
+  (is_limit_of_preserves G (limit.is_limit _))
+  (limit.is_limit _)
+
+def _root_.category_theory.functor.map_colimit {J C D : Type*}
+  [small_category J] [category C] [category D]
+  (G : C ⥤ D) (F : J ⥤ C)
+  [has_colimit F] [has_colimit (F ⋙ G)] [preserves_colimit F G] :
+  G.obj (colimit F) ≅ colimit (F ⋙ G) :=
+is_colimit.cocone_point_unique_up_to_iso
+  (is_colimit_of_preserves G (colimit.is_colimit _))
+  (colimit.is_colimit _)
+
+@[simp, reassoc]
+lemma _root_.category_theory.functor.ι_map_colimit_inv {J C D : Type*}
+  [small_category J] [category C] [category D]
+  (G : C ⥤ D) (F : J ⥤ C)
+  [has_colimit F] [has_colimit (F ⋙ G)] [preserves_colimit F G]
+  (j : J) :
+  colimit.ι (F ⋙ G) j ≫ (G.map_colimit F).inv = G.map (colimit.ι F j) :=
+by simp only [category_theory.functor.map_colimit, functor.map_cocone_ι_app,
+    colimit.comp_cocone_point_unique_up_to_iso_inv, colimit.cocone_ι]
+
+def colimit_comp_iso {J K C D : Type*} [small_category J] [small_category K]
+  [category C] [category D] [has_colimits_of_shape J D]
+  (F : J ⥤ K ⥤ C) (G : C ⥤ D) [has_colimit F]
+  [∀ k, has_colimit (F.flip.obj k)] [∀ k, preserves_colimit (F.flip.obj k) G]
+  [∀ k, preserves_colimit F ((category_theory.evaluation K C).obj k)]
+  [∀ k, has_colimit (F ⋙ (category_theory.evaluation K C).obj k)] :
+  colimit F ⋙ G ≅ F.flip ⋙ ((whiskering_right _ _ _).obj G) ⋙ colim :=
+begin
+  refine nat_iso.of_components _ _,
+  { intro k,
+    refine G.map_iso _ ≪≫ G.map_colimit _,
+    refine ((category_theory.evaluation _ _).obj k).map_colimit _, },
+  { intros k₁ k₂ f,
+    rw [← iso.inv_comp_eq, ← category.assoc, ← iso.eq_comp_inv],
+    ext j,
+    dsimp,
+    simp only [category.assoc, category_theory.functor.ι_map_colimit_inv_assoc, colimit.ι_map_assoc,
+      whisker_right_app, functor.flip_map_app],
+    simp only [← functor.map_comp], congr' 1,
+    sorry }
+end
+.
 
 lemma is_iso_colim_to_lim_component (S : Profinite.{u}ᵒᵖ) :
   is_iso ((colim_to_lim F).val.app S) :=
@@ -74,14 +127,12 @@ begin
   let VS := CondensedSet_to_presheaf.{u} ⋙ (category_theory.evaluation.{u u+1 u+1 u+2} Profinite.{u}ᵒᵖ (Type (u+1))).obj S,
   let F' := uncurry.{u+1 u+1}.obj F ⋙ VS,
   let f := colimit_limit_to_limit_colimit F',
-  let f' := colimit_limit_iso (curry.{u+1 u+1}.obj F'), -- another version of `f`?
   let e₁ : (colimit (limit F)).val.obj S ≅ colimit (curry.obj (category_theory.prod.swap J K ⋙ F') ⋙ lim),
   { sorry },
   let e₂ : (limit (colimit F.flip)).val.obj S ≅ limit (curry.obj F' ⋙ colim),
-  { refine is_limit.cone_point_unique_up_to_iso
-      (is_limit_of_preserves VS (limit.is_limit $ colimit F.flip))
-      (limit.is_limit _) ≪≫ _,
+  { refine VS.map_limit (colimit F.flip) ≪≫ _,
     refine limits.lim.map_iso _,
+    refine (colimit_comp_iso _ _) ≪≫ _,
     sorry },
   suffices : (colim_to_lim F).val.app S = e₁.hom ≫ f ≫ e₂.inv,
   { rw [this, is_iso_iff_is_iso_comp_left, is_iso_iff_is_iso_comp_right], apply_instance },
