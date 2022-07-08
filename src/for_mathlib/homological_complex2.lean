24c302558d32dddf45af7e9ab81336b6c57f8719
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-07-08 16:39:23 +0200

    some little progress on functor_eval_homology_nat_iso

--- src/for_mathlib/homological_complex2.lean
+++ src/for_mathlib/homological_complex2.lean
@@ -90,4 +90,26 @@ def eval_functor_homology_iso (F : X ⥤ homological_complex 𝒜 c) (i) :
 iso_whisker_right (eval_functor_equiv.unit_iso.app F) (homology_functor 𝒜 c i)
   ≪≫ (functor_eval_homology_iso ((@eval_functor _ X 𝒜 _ _ _ c).obj F) i).symm
 
+/- do we have this elsewhere? -/
+def _root_.category_theory.functor.right_comp {C D : Type*} (X : Type*) [category C] [category D]
+  [category X] (F : C ⥤ D) : (X ⥤ C) ⥤ (X ⥤ D) :=
+{ obj := λ G, G ⋙ F,
+  map := λ G₁ G₂ φ, φ ◫ 𝟙 F,
+  map_id' := λ G, by { ext, simpa only [nat_trans.hcomp_id_app, nat_trans.id_app,
+    category_theory.functor.map_id], },
+  map_comp' := λ G₁ G₂ G₃ φ ψ, begin
+    ext,
+    simp only [nat_trans.hcomp_id_app, nat_trans.comp_app, functor.map_comp],
+  end }
+
+def functor_eval_homology_nat_iso (i : ι) :
+  homology_functor (X ⥤ 𝒜) c i ≅ functor_eval.flip ⋙ (homology_functor 𝒜 c i).right_comp _ :=
+nat_iso.of_components
+(λ G, functor_eval_homology_iso G i)
+(λ G₁ G₂ φ, begin
+  sorry,
+  -- use functor.naturality_homology_functor_iso,
+end)
+
+
 end homological_complex
