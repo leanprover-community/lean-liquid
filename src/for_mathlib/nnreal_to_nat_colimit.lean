29f693bbe518090cc1df7d76838706323d1187bc
Adam Topaz <github@adamtopaz.com>
2022-07-10 05:52:31 -0600

    wip

--- src/for_mathlib/nnreal_to_nat_colimit.lean
+++ src/for_mathlib/nnreal_to_nat_colimit.lean
@@ -9,49 +9,39 @@ open category_theory
 
 namespace category_theory.limits
 
-variables (c : ℝ≥0) [fact (0 < c)]
+variables (ι : ℕ →o ℝ≥0) (hι : ∀ r : ℝ≥0, ∃ n : ℕ, r ≤ ι n)
 
-lemma exists_int_mul (r : ℝ≥0) : ∃ n : ℕ, r ≤ ↑n * c :=
-begin
-  refine ⟨nat.ceil (r / c), _⟩,
-  transitivity r / c * c,
-  { rw div_mul_cancel,
-    exact ne_of_gt (fact.out _) },
-  { exact (mul_le_mul_right (fact.out _)).mpr (nat.le_ceil _) },
-end
+def find_nat (r : ℝ≥0) : ℕ := (hι r).some
 
-def find_int_mul (r : ℝ≥0) : ℕ := (exists_int_mul c r).some
-
-lemma find_int_mul_spec (r : ℝ≥0) : r ≤ ↑(find_int_mul c r) * c :=
-(exists_int_mul c r).some_spec
+lemma find_nat_spec (r : ℝ≥0) : r ≤ ι (find_nat ι hι r) :=
+(hι r).some_spec
 
 def nat_to_nnreal : ℕ ⥤ ℝ≥0 :=
-{ obj := λ i, ↑i * c,
-  map := λ i j h, hom_of_le $
-    mul_le_mul (by exact_mod_cast h.le) (le_refl _) (zero_le _) (zero_le _) }
+{ obj := λ i, ι i,
+  map := λ i j h, hom_of_le $ ι.mono h.le }
 
 universes v u
 variables {C : Type u} [category.{v} C]
 
 def restrict_diagram (F : as_small.{v} ℝ≥0 ⥤ C) :
   as_small.{v} ℕ ⥤ C :=
-(as_small.down ⋙ nat_to_nnreal c ⋙ as_small.up) ⋙ F
+(as_small.down ⋙ nat_to_nnreal ι ⋙ as_small.up) ⋙ F
 
 def restrict_cocone {F : as_small.{v} ℝ≥0 ⥤ C} (S : cocone F) :
-  cocone (restrict_diagram c F) :=
+  cocone (restrict_diagram ι F) :=
 S.whisker _
 
 def is_colimit_restrict_cocone {F : as_small.{v} ℝ≥0 ⥤ C} (S : cocone F) (hS : is_colimit S) :
-  is_colimit (restrict_cocone c S) :=
+  is_colimit (restrict_cocone ι S) :=
 { desc := λ T, hS.desc ⟨T.X,
-  { app := λ r, F.map (as_small.up.map $ hom_of_le $ find_int_mul_spec c (ulift.down r)) ≫
-        T.ι.app (ulift.up (find_int_mul c (ulift.down r))),
+  { app := λ r, F.map (as_small.up.map $ hom_of_le $ find_nat_spec ι hι (ulift.down r)) ≫
+        T.ι.app (ulift.up (find_nat ι hι (ulift.down r))),
     naturality' := begin
       intros r s f,
       dsimp,
-      let m := find_int_mul c (ulift.down r) ⊔ find_int_mul c (ulift.down s),
-      let r' := as_small.up.obj (find_int_mul c (ulift.down r)),
-      let s' := as_small.up.obj (find_int_mul c (ulift.down s)),
+      let m := find_nat ι hι (ulift.down r) ⊔ find_nat ι hι (ulift.down s),
+      let r' := as_small.up.obj (find_nat ι hι (ulift.down r)),
+      let s' := as_small.up.obj (find_nat ι hι (ulift.down s)),
       let m' := as_small.up.obj m,
       let ιr : r' ⟶ m' := as_small.up.map (hom_of_le $ le_sup_left),
       let ιs : s' ⟶ m' := as_small.up.map (hom_of_le $ le_sup_right),
@@ -66,8 +56,8 @@ def is_colimit_restrict_cocone {F : as_small.{v} ℝ≥0 ⥤ C} (S : cocone F) (
     dsimp [restrict_cocone],
     rw hS.fac,
     dsimp,
-    let r := (nat_to_nnreal c).obj (ulift.down j),
-    let k := find_int_mul c r,
+    let r := (nat_to_nnreal ι).obj (ulift.down j),
+    let k := find_nat ι hι r,
     let l := (ulift.down j) ⊔ k,
     let k' : as_small.{v} ℕ := as_small.up.obj k,
     let l' : as_small.{v} ℕ := as_small.up.obj l,
@@ -82,7 +72,7 @@ def is_colimit_restrict_cocone {F : as_small.{v} ℝ≥0 ⥤ C} (S : cocone F) (
   uniq' := begin
     intros W m hm,
     apply hS.hom_ext, intros j,
-    specialize hm (as_small.up.obj (find_int_mul c (ulift.down j))),
+    specialize hm (as_small.up.obj (find_nat ι hι (ulift.down j))),
     dsimp [restrict_cocone] at hm,
     rw hS.fac,
     dsimp,
