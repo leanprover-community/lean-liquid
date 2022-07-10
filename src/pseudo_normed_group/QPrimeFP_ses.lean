e1c737f6adf43c54e7fc91080bd12ac2ea2d0825
Adam Topaz <github@adamtopaz.com>
2022-07-10 05:52:38 -0600

    wip

---
+++ src/pseudo_normed_group/QPrimeFP_ses.lean
@@ -0,0 +1,39 @@
+import pseudo_normed_group.FP2
+import condensed.adjunctions
+import free_pfpng.acyclic
+import for_mathlib.derived.ext_coproducts
+import for_mathlib.derived.example
+import breen_deligne.eval2
+import system_of_complexes.shift_sub_id
+import for_mathlib.AddCommGroup.explicit_products
+import condensed.Qprime_isoms
+import condensed.short_exact
+import condensed.bd_ses
+
+noncomputable theory
+
+open_locale nnreal
+
+universe u
+
+open category_theory category_theory.limits breen_deligne
+
+section step1
+
+variables (r' : ℝ≥0)
+variables (BD : breen_deligne.data) (κ : ℝ≥0 → ℕ → ℝ≥0)
+variables [∀ c, BD.suitable (κ c)] [∀ n, fact (monotone (function.swap κ n))]
+variables (M : ProFiltPseuNormGrpWithTinv₁.{u} r')
+
+abbreviation freeCond := Profinite_to_Condensed.{u} ⋙ CondensedSet_to_Condensed_Ab
+
+def QprimeFP_nat : ℝ≥0 ⥤ chain_complex (Condensed.{u} Ab.{u+1}) ℕ :=
+FPsystem r' BD ⟨M⟩ κ ⋙ (freeCond.{u}.map_FreeAb ⋙ FreeAb.eval _).map_homological_complex _
+
+def QprimeFP_int : ℝ≥0 ⥤ cochain_complex (Condensed.{u} Ab.{u+1}) ℤ :=
+QprimeFP_nat r' BD κ M ⋙ homological_complex.embed complex_shape.embedding.nat_down_int_up
+
+def QprimeFP : ℝ≥0 ⥤ bounded_homotopy_category (Condensed.{u} Ab.{u+1}) :=
+QprimeFP_nat r' BD κ M ⋙ chain_complex.to_bounded_homotopy_category
+
+end step1
