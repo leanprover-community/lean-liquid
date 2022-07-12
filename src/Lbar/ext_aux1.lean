afa5d34b3f9442d084c77ac03d16458e9d98120c
Johan Commelin <johan@commelin.net>
2022-07-12 18:21:04 +0200

    close a sorry

--- src/Lbar/ext_aux1.lean
+++ src/Lbar/ext_aux1.lean
@@ -50,6 +50,19 @@ begin
     { apply normed_group_hom.uniform_continuous, } },
   { intros x y, apply normed_group_hom.map_add, }
 end
+.
+
+attribute [simps] equiv.ulift add_equiv.ulift
+
+lemma SemiNormedGroup.forget₂_Ab_map {V W : SemiNormedGroup} (f : V ⟶ W) :
+  (forget₂ SemiNormedGroup Ab).map f = f.to_add_monoid_hom :=
+rfl
+
+lemma SemiNormedGroup.forget₂_Ab_obj (V : SemiNormedGroup) :
+  (forget₂ SemiNormedGroup Ab).obj V = AddCommGroup.of V :=
+rfl
+
+set_option pp.universes true
 
 --jmc: is this helpful??
 @[reassoc]
@@ -59,10 +72,31 @@ lemma ExtQprime_iso_aux_system_obj_aux'_natural (X Y : Profinite.{u}) (f : X ⟶
     Ab.ulift.map ((forget₂ _ _).map (SemiNormedGroup.Completion.map ((SemiNormedGroup.LocallyConstant.obj _).map f.op))) ≫
  (ExtQprime_iso_aux_system_obj_aux' V X).hom :=
 begin
-  ext1 φ, simp only [comp_apply],
+  ext1 ⟨φ⟩, simp only [comp_apply],
   dsimp only [ExtQprime_iso_aux_system_obj_aux', add_equiv.to_AddCommGroup_iso,
     add_equiv.trans_apply, add_equiv.coe_to_add_monoid_hom, add_equiv.coe_mk,
-    Ab.ulift_map_apply],
-  sorry
+    Ab.ulift_map_apply,
+    SemiNormedGroup.forget₂_Ab_map, SemiNormedGroup.forget₂_Ab_obj,
+    AddCommGroup.coe_of],
+  apply uniform_space.completion.induction_on φ; clear φ,
+  { refine @is_closed_eq _ _ _ _ (id _) _ _ _ _,
+    { dsimp [SemiNormedGroup.Completion_obj, SemiNormedGroup.LocallyConstant_obj_obj],
+      apply_instance },
+    { apply uniform_space.completion.continuous_map.comp uniform_space.completion.continuous_map },
+    { apply uniform_space.completion.continuous_map.comp,
+      dsimp only [Ab.ulift, add_monoid_hom.coe_mk, add_equiv.ulift_apply,
+        equiv.to_fun_as_coe, equiv.ulift_apply],
+      apply uniform_space.completion.continuous_map } },
+  { intros φ,
+    dsimp only [Ab.ulift, add_monoid_hom.coe_mk, add_equiv.ulift_apply,
+      equiv.to_fun_as_coe, equiv.ulift_apply,
+      SemiNormedGroup.LocallyConstant_obj_map,
+      SemiNormedGroup.Completion_map],
+    erw [normed_group_hom.completion_coe, normed_group_hom.completion_coe,
+      normed_group_hom.completion_coe, normed_group_hom.completion_coe],
+    congr' 1,
+    dsimp only [locally_constant.comap_hom_apply, locally_constant.map_hom_apply],
+    erw [locally_constant.comap_map],
+    exact f.continuous, }
 end
 .
