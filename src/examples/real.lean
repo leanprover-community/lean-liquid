a648e2dbc11b6891bc5c019df65dad93dd071267
Johan Commelin <johan@commelin.net>
2022-07-19 08:10:52 +0000

    example: real numbers are a conditionally complete linearly ordered field

---
+++ src/examples/real.lean
@@ -0,0 +1,21 @@
+import challenge_notations
+import challenge_prerequisites
+
+/-!
+
+This file shows that `ℝ` is a conditionally complete linearly ordered field.
+
+-/
+
+noncomputable theory
+
+open_locale liquid_tensor_experiment nnreal zero_object
+open liquid_tensor_experiment category_theory category_theory.limits opposite
+
+example : conditionally_complete_linear_order ℝ := infer_instance
+example : linear_ordered_field ℝ := infer_instance
+
+example : ℝ≥0 = {r : ℝ // r ≥ 0} := rfl
+
+-- TODO: once we bump mathlib past #3292 we can show that `ℝ` is the unique
+-- conditionally complete linearly ordered field, up to isomorphism.
