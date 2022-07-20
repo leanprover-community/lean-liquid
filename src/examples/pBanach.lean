fef1a667f1a34afa77b92d23f14049c93c1be0ea
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2022-07-20 15:08:07 +0200

    setting-up the lp example

--- src/examples/pBanach.lean
+++ src/examples/pBanach.lean
@@ -1,3 +1,4 @@
+import analysis.normed_space.lp_space
 import challenge_notations
 import challenge_prerequisites
 
@@ -13,9 +14,9 @@ open_locale liquid_tensor_experiment nnreal zero_object
 open liquid_tensor_experiment category_theory category_theory.limits opposite
 
 /-! Let $p$ be a nonnegative real, $V$ a $p$-Banach space, and $S$ a profinite set. -/
-variables (p : ℝ≥0) (V : pBanach.{0} p) (S : Profinite.{0})
-
 section pBanach
+
+variables (p : ℝ≥0) (V : pBanach.{0} p) (S : Profinite.{0})
 
 /-! The $p$-Banach space $V$ is a complete and separated topological $ℝ$-vector space. -/
 
@@ -51,8 +52,6 @@ example : uniformity V = ⨅ (ε : ℝ) (H : ε > 0),
   filter.principal { p : V × V | ∥p.1 - p.2∥ < ε } :=
 (p_banach.exists_p_norm V.p_banach').some.uniformity
 
-end pBanach
-
 /- When $V$ is viewed as condensed abelian group, the sections
 over $S$ are the continuous maps $S → V$.
 For technical reasons related to size issues in topos theory,
@@ -66,3 +65,73 @@ example (f : S → V) (hf : continuous f) : C(S,V) := ⟨f,hf⟩
 
 /- The group operation on `Γ_ V S` is pointwise addition, as expected. -/
 example (f g : Γ_ V S) (s : S) : (f + g) s = f s + g s := rfl
+
+end pBanach
+
+section lp
+
+open lp ennreal nnreal
+open_locale ennreal
+
+/- Although we restrict to `p ≤ 1`, it would be better to work with `p : ℝ≥0∞` rather than `p : ℝ≥0` because there is no coercion from `{x : ℝ≥0∞ // x ≤ 1}` to `{x : ℝ≥0 // x ≤ 1}`, for which the `simp` lemma `one_le_coe_iff` is needed. -/
+
+variables {p : ℝ≥0} [hp : fact (p ≤ 1)]
+
+
+/- We introduce the ℓ^q-space ℓ^q(T) for testing purposes.-/
+variables {q q' : ℝ≥0∞} [fact (1 ≤ q)] {T : ℕ → Type*}
+  [Π i, normed_group (T i)]
+  [Π i, normed_space ℝ (T i)]
+  [Π i, complete_space (T i)]
+
+example : uniform_space (lp T q) := infer_instance --needs 1≤ q, no completeness and no normed space structure
+
+example : topological_add_group (lp T q) := infer_instance --needs 1≤ q, no completeness and no normed space structure
+
+-- example : metric_space (lp T q) := infer_instance --needs 1≤ q, no completeness and no normed space structure
+
+example : separated_space (lp T q) := infer_instance--needs 1≤ q, no completeness and no normed space structure
+
+example : has_continuous_smul ℝ (lp T q) := infer_instance  --needs 1 ≤ q, no completeness and no normed space structure
+
+example : complete_space (lp T q) := infer_instance --needs 1 ≤ q, and completeness of every (F n)
+/-The true example-/
+
+def E : ℕ → Type* := λ n, ℝ
+/-First, three basic instances to access `lp E p`-/
+instance normed_group_En : Π n : ℕ, normed_group (E n) := by {intro _, unfold E, apply_instance}
+instance normed_space_En : Π n : ℕ, normed_space ℝ (E n) := sorry --by {intro _, unfold E, apply_instance}
+instance complete_space_En : Π n : ℕ, complete_space (E n) := sorry --by {intro _, unfold E, apply_instance}
+
+
+/-Then, one instance to make the following lemmas type-check; and five lemmas corresponding to the relevant fields of `p_banach`-/
+
+instance : metric_space (lp E p) := sorry --When `1 ≤ p`, this is a consequence of the ℓ^p(E) being a `normed_group`, but this is false when `p ≤ 1`.
+
+lemma top_add_group_p_le_one : topological_add_group (lp E p) := sorry--would follow from a `normed_group` (→ `metric_space`) instance on `lp E p`
+
+lemma cont_smul_p_le_one : has_continuous_smul ℝ (lp E p) := sorry
+
+lemma complete_p_le_one : complete_space (lp E p ) := sorry
+
+lemma separated_p_le_one : separated_space (lp E p) := infer_instance --sorry--would follow from a `normed_group` (→ `metric_space`) instance on `lp E p`
+
+lemma p_norm_lp : has_p_norm (lp E p) p :=
+begin
+  have := has_norm.norm,
+  rotate,
+  use lp E p,
+  apply_instance,
+  use this;
+  sorry,
+end
+
+--the following theorem should really be a `def`
+theorem lp_N_is_pBanach : p_banach (lp E p) p :=
+{ exists_p_norm := ⟨p_norm_lp⟩,
+  topological_add_group := top_add_group_p_le_one,
+  continuous_smul := cont_smul_p_le_one,
+  complete := complete_p_le_one,
+  separated := separated_p_le_one}
+
+end lp
