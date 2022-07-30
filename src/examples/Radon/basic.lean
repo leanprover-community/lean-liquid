ee3044b5b68007d992fb58d8ae63bf411f4d08ef
Adam Topaz <github@adamtopaz.com>
2022-07-30 14:10:52 -0600

    more sorries

--- src/examples/Radon/basic.lean
+++ src/examples/Radon/basic.lean
@@ -16,6 +16,7 @@ local attribute [instance]
 variable (X : Profinite.{0})
 
 abbreviation pre_Radon := weak_dual ℝ (C(X,ℝ))
+abbreviation pre_Radon_LC := weak_dual ℝ (locally_constant X ℝ)
 
 variable {X}
 
@@ -25,13 +26,26 @@ def indicator {X : Profinite.{0}} {T : discrete_quotient X} (t : T) : C(X,ℝ) :
 { to_fun := set.indicator (T.proj ⁻¹' {t}) 1,
   continuous_to_fun := sorry }
 
+def indicator_LC {X : Profinite.{0}} {T : discrete_quotient X} (t : T) : locally_constant X ℝ :=
+{ to_fun := set.indicator (T.proj ⁻¹' {t}) 1,
+  is_locally_constant := sorry }
+
 def pre_Radon.is_bdd (μ : X.pre_Radon) (p c : ℝ≥0) : Prop :=
 ∀ (T : discrete_quotient X),
   ∑ t : T, (∥ μ (indicator t) ∥₊)^(p : ℝ) ≤ c
 
+def pre_Radon_LC.is_bdd (μ : X.pre_Radon_LC) (p c : ℝ≥0) : Prop :=
+∀ (T : discrete_quotient X),
+  ∑ t : T, (∥ μ (indicator_LC t) ∥₊)^(p : ℝ) ≤ c
+
 variables (X)
+
 def Radon (p c : ℝ≥0) : Top.{0} :=
 Top.of { μ : X.pre_Radon | μ.is_bdd p c }
+
+def Radon_LC (p c : ℝ≥0) : Top.{0} :=
+Top.of { μ : X.pre_Radon_LC | μ.is_bdd p c }
+
 variables {X}
 
 def map_C {X Y : Profinite.{0}} (f : X ⟶ Y) :
@@ -41,9 +55,26 @@ def map_C {X Y : Profinite.{0}} (f : X ⟶ Y) :
   map_smul' := λ g1 g2, rfl,
   cont := continuous_map.continuous_comp_left f }
 
+def map_LC {X Y : Profinite.{0}} (f : X ⟶ Y) :
+  locally_constant Y ℝ →L[ℝ] locally_constant X ℝ :=
+{ to_fun := λ g, g.comap f,
+  map_add' := sorry,
+  map_smul' := sorry,
+  cont := sorry }
+
 def map_pre_Radon {X Y : Profinite.{0}} (f : X ⟶ Y) :
   X.pre_Radon →L[ℝ] Y.pre_Radon :=
 { to_fun := λ g, g.comp (map_C f),
+  map_add' := λ a b, rfl,
+  map_smul' := λ a b, rfl,
+  cont := begin
+    apply weak_dual.continuous_of_continuous_eval, intros ff,
+    apply weak_dual.eval_continuous
+  end }
+
+def map_pre_Radon_LC {X Y : Profinite.{0}} (f : X ⟶ Y) :
+  X.pre_Radon_LC →L[ℝ] Y.pre_Radon_LC :=
+{ to_fun := λ g, g.comp (map_LC f),
   map_add' := λ a b, rfl,
   map_smul' := λ a b, rfl,
   cont := begin
@@ -57,14 +88,34 @@ def pre_Radon_functor : Profinite.{0} ⥤ Top.{0} :=
   map_id' := λ X, by { ext, dsimp [map_pre_Radon], congr, ext, refl },
   map_comp' := λ X Y Z f g, by { ext, refl } }
 
+def pre_Radon_LC_functor : Profinite.{0} ⥤ Top.{0} :=
+{ obj := λ X, Top.of X.pre_Radon_LC,
+  map := λ X Y f, ⟨map_pre_Radon_LC f, continuous_linear_map.continuous _⟩,
+  map_id' := sorry,
+  map_comp' := sorry }
+
 lemma pre_Radon.is_bdd_map {X Y : Profinite.{0}} (f : X ⟶ Y)
   (p c : ℝ≥0) (μ : X.pre_Radon) (hμ : μ.is_bdd p c) :
   (map_pre_Radon f μ).is_bdd p c :=
 sorry
 
+lemma pre_Radon_LC.is_bdd_map {X Y : Profinite.{0}} (f : X ⟶ Y)
+  (p c : ℝ≥0) (μ : X.pre_Radon_LC) (hμ : μ.is_bdd p c) :
+  (map_pre_Radon_LC f μ).is_bdd p c :=
+sorry
+
 def map_Radon {X Y : Profinite.{0}} (f : X ⟶ Y) (p c : ℝ≥0) :
   X.Radon p c ⟶ Y.Radon p c :=
 { to_fun := λ μ, ⟨map_pre_Radon f μ.1, pre_Radon.is_bdd_map f p c μ.1 μ.2⟩,
+  continuous_to_fun := begin
+    apply continuous_subtype_mk,
+    refine continuous.comp _ continuous_subtype_coe,
+    apply continuous_linear_map.continuous
+  end }
+
+def map_Radon_LC {X Y : Profinite.{0}} (f : X ⟶ Y) (p c : ℝ≥0) :
+  X.Radon_LC p c ⟶ Y.Radon_LC p c :=
+{ to_fun := λ μ, ⟨map_pre_Radon_LC f μ.1, pre_Radon_LC.is_bdd_map f p c μ.1 μ.2⟩,
   continuous_to_fun := begin
     apply continuous_subtype_mk,
     refine continuous.comp _ continuous_subtype_coe,
@@ -82,6 +133,12 @@ def Radon_functor (p c) : Profinite.{0} ⥤ Top.{0} :=
     intros X Y Z f g, ext, refl,
   end } .
 
+def Radon_LC_functor (p c) : Profinite.{0} ⥤ Top.{0} :=
+{ obj := λ X, X.Radon_LC p c,
+  map := λ X Y f, map_Radon_LC f _ _,
+  map_id' := sorry,
+  map_comp' := sorry } .
+
 -- Key calculation 1
 def real_measures_iso (p c : ℝ≥0) [fact (0 < p)] [fact (p ≤ 1)] :
   real_measures.functor p ⋙ CompHausFiltPseuNormGrp₁.level.obj c ⋙ CompHaus_to_Top ≅
@@ -93,8 +150,8 @@ def LC_to_C (X : Profinite.{0}) : locally_constant X ℝ →L[ℝ] C(X,ℝ) :=
   map_smul' := λ f g, rfl,
   cont := (locally_constant.pkg X ℝ).continuous_coe }
 
-def pre_Radon_LC_comparison (X : Profinite.{0}) :
-  X.pre_Radon →L[ℝ] weak_dual ℝ (locally_constant X ℝ) :=
+def pre_Radon_comparison (X : Profinite.{0}) :
+  X.pre_Radon →L[ℝ] X.pre_Radon_LC :=
 { to_fun := λ μ, μ.comp X.LC_to_C,
   map_add' := λ f g, rfl,
   map_smul' := λ f g, rfl,
@@ -103,8 +160,8 @@ def pre_Radon_LC_comparison (X : Profinite.{0}) :
     apply weak_dual.eval_continuous,
   end }
 
-def pre_Radon_LC_comparison_inverse (X : Profinite.{0}) :
-  weak_dual ℝ (locally_constant X ℝ) →L[ℝ] X.pre_Radon :=
+def pre_Radon_comparison_inverse (X : Profinite.{0}) :
+  X.pre_Radon_LC →L[ℝ] X.pre_Radon :=
 { to_fun := λ μ,
   { to_fun := λ f, (locally_constant.pkg X ℝ).extend μ f,
     map_add' := sorry,
@@ -114,31 +171,41 @@ def pre_Radon_LC_comparison_inverse (X : Profinite.{0}) :
   map_smul' := sorry,
   cont := sorry }
 
-def pre_Radon_LC_equiv (X : Profinite.{0}) :
-  X.pre_Radon ≃L[ℝ] weak_dual ℝ (locally_constant X ℝ) :=
-{ inv_fun := X.pre_Radon_LC_comparison_inverse,
+def pre_Radon_equiv (X : Profinite.{0}) :
+  X.pre_Radon ≃L[ℝ] X.pre_Radon_LC :=
+{ inv_fun := X.pre_Radon_comparison_inverse,
   left_inv := sorry,
   right_inv := sorry,
-  continuous_to_fun := X.pre_Radon_LC_comparison.cont,
-  continuous_inv_fun := X.pre_Radon_LC_comparison_inverse.cont,
-  ..X.pre_Radon_LC_comparison }
+  continuous_to_fun := X.pre_Radon_comparison.cont,
+  continuous_inv_fun := X.pre_Radon_comparison_inverse.cont,
+  ..X.pre_Radon_comparison }
 
 def pre_Radon_cone (X : Profinite.{0}) : cone (X.diagram ⋙ pre_Radon_functor) :=
 pre_Radon_functor.map_cone X.as_limit_cone
 
-def pre_Radon_comparison (X : Profinite.{0}) :
+def pre_Radon_limit_comparison (X : Profinite.{0}) :
   Top.of X.pre_Radon ⟶ (Top.limit_cone (X.diagram ⋙ pre_Radon_functor)).X :=
 (Top.limit_cone_is_limit _).lift X.pre_Radon_cone
 
+def pre_Radon_LC_cone (X : Profinite.{0}) : cone (X.diagram ⋙ pre_Radon_LC_functor) :=
+pre_Radon_LC_functor.map_cone X.as_limit_cone
+
+def pre_Radon_LC_limit_comparison (X : Profinite.{0}) :
+  Top.of X.pre_Radon_LC ⟶ (Top.limit_cone (X.diagram ⋙ pre_Radon_LC_functor)).X :=
+(Top.limit_cone_is_limit _).lift X.pre_Radon_LC_cone
+
+instance is_iso_pre_Radon_LC_limit_comparison (X : Profinite.{0}) :
+  is_iso X.pre_Radon_LC_limit_comparison := sorry
+
 instance is_iso_pre_Radon_comparison (X : Profinite.{0}) :
-  is_iso X.pre_Radon_comparison := sorry
+  is_iso X.pre_Radon_limit_comparison := sorry
 
 def is_limit_pre_Radon_cone (X : Profinite.{0}) :
   is_limit X.pre_Radon_cone :=
-{ lift := λ S, (Top.limit_cone_is_limit _).lift S ≫ inv X.pre_Radon_comparison,
+{ lift := λ S, (Top.limit_cone_is_limit _).lift S ≫ inv X.pre_Radon_limit_comparison,
   fac' := begin
     intros S T, rw category.assoc,
-    have : inv X.pre_Radon_comparison ≫ (pre_Radon_cone X).π.app T =
+    have : inv X.pre_Radon_limit_comparison ≫ (pre_Radon_cone X).π.app T =
       (Top.limit_cone _).π.app _,
     { rw is_iso.inv_comp_eq, erw (Top.limit_cone_is_limit _).fac },
     rw [this, is_limit.fac],
