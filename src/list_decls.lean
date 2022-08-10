b0b5e2b5ab3130b65abb25ad90d38b1aa7060ba6
Johan Commelin <johan@commelin.net>
2022-08-10 09:56:37 +0000

    add list_decls

---
+++ src/list_decls.lean
@@ -0,0 +1,436 @@
+import challenge_notations
+import normed_snake_dual
+import prop819
+import challenge_prerequisites
+import liquid
+import banach
+import normed_spectral
+import statement
+import challenge
+import normed_snake
+import hacks_and_tricks.by_exactI_hack
+import hacks_and_tricks.type_pow
+import hacks_and_tricks.asyncI
+import condensed.adjunctions2
+import condensed.evaluation_homology
+import condensed.ab4
+import condensed.short_exact
+import condensed.bd_ses_aux
+import condensed.tensor_short_exact
+import condensed.coproducts
+import condensed.ab5
+import condensed.adjunctions_module
+import condensed.proetale_site
+import condensed.rescale
+import condensed.Qprime_isoms2
+import condensed.basic
+import condensed.sheafification_homology
+import condensed.filtered_colimits
+import condensed.kernel_comparison
+import condensed.projective_resolution
+import condensed.bd_ses
+import condensed.ab
+import condensed.Qprime_isoms
+import condensed.adjunctions
+import condensed.exact
+import condensed.tensor
+import condensed.acyclic
+import condensed.projective_resolution_module
+import condensed.sheafification_mono
+import condensed.top_comparison
+import condensed.bd_lemma
+import condensed.is_proetale_sheaf
+import condensed.is_iso_iff_extrdisc
+import condensed.condensify
+import condensed.filtered_colimits_commute_with_finite_limits
+import condensed.extr.basic
+import condensed.extr.equivalence
+import condensed.extr.lift_comphaus
+import examples.real
+import examples.pBanach
+import examples.Ext
+import facts.int
+import facts.default
+import facts.normed_group
+import facts.nnreal
+import thm95.homotopy
+import thm95.modify_complex
+import thm95.col_exact_prep
+import thm95.default
+import thm95.row_iso
+import thm95.polyhedral_iso
+import thm95.pfpng_iso
+import thm95.col_exact
+import thm95.double_complex
+import thm95.constants.default
+import thm95.constants.spectral_constants
+import Lbar.ext_aux2
+import Lbar.Lbar_le
+import Lbar.torsion_free_profinite
+import Lbar.finsupp_instance
+import Lbar.basic
+import Lbar.bounded
+import Lbar.ext_aux1
+import Lbar.ext
+import Lbar.sum_nnnorm
+import Lbar.nnnorm_add_class
+import Lbar.kernel_truncate
+import Lbar.ext_aux3
+import Lbar.iota
+import Lbar.pseudo_normed_group
+import Lbar.torsion_free_condensed
+import Lbar.ses
+import Lbar.ext_aux4
+import Lbar.squares
+import Lbar.functor
+import Lbar.ext_preamble
+import normed_free_pfpng.compare
+import normed_free_pfpng.basic
+import locally_constant.completion
+import locally_constant.completion_aux
+import locally_constant.Vhat
+import locally_constant.analysis
+import locally_constant.SemiNormedGroup
+import prop819.completion
+import prop819.strict_complex_iso
+import real_measures.basic
+import real_measures.default
+import real_measures.condensed
+import breen_deligne.apply_Pow
+import breen_deligne.homotopy
+import breen_deligne.category
+import breen_deligne.suitable
+import breen_deligne.eval2
+import breen_deligne.functorial_map
+import breen_deligne.constants
+import breen_deligne.universal_map
+import breen_deligne.eval_Pow_functor_nat_trans_compatibility
+import breen_deligne.main
+import breen_deligne.eg
+import breen_deligne.eval1half
+import breen_deligne.eval
+import combinatorial_lemma.profinite_setup
+import combinatorial_lemma.finite
+import combinatorial_lemma.default
+import combinatorial_lemma.partition
+import combinatorial_lemma.profinite
+import combinatorial_lemma.lem97
+import pseudo_normed_group.homotopy
+import pseudo_normed_group.breen_deligne
+import pseudo_normed_group.profinitely_filtered
+import pseudo_normed_group.basic
+import pseudo_normed_group.QprimeFP
+import pseudo_normed_group.FP2
+import pseudo_normed_group.bounded_limits
+import pseudo_normed_group.CLC
+import pseudo_normed_group.Tinv
+import pseudo_normed_group.splittable
+import pseudo_normed_group.sum_hom
+import pseudo_normed_group.system_of_complexes
+import pseudo_normed_group.FP
+import pseudo_normed_group.system_of_complexes2
+import pseudo_normed_group.LC
+import pseudo_normed_group.with_Tinv
+import pseudo_normed_group.category.CompHausFiltPseuNormGrp
+import pseudo_normed_group.category.CompHausFiltPseuNormGrpWithTinv
+import pseudo_normed_group.category.default
+import pseudo_normed_group.category.strictProFiltPseuNormGrpWithTinv
+import pseudo_normed_group.category.strictCompHausFiltPseuNormGrp
+import pseudo_normed_group.category.ProFiltPseuNormGrpWithTinv
+import pseudo_normed_group.category.strictProFiltPseuNormGrp
+import pseudo_normed_group.category.ProFiltPseuNormGrp
+import prop_92.concrete
+import prop_92.prop_92
+import prop_92.extension_profinite
+import free_pfpng.lemmas
+import free_pfpng.setup
+import free_pfpng.epi
+import free_pfpng.basic
+import free_pfpng.mono
+import free_pfpng.acyclic
+import free_pfpng.main
+import polyhedral_lattice.int
+import polyhedral_lattice.Hom
+import polyhedral_lattice.category
+import polyhedral_lattice.basic
+import polyhedral_lattice.cech
+import polyhedral_lattice.topology
+import polyhedral_lattice.pseudo_normed_group
+import polyhedral_lattice.cosimplicial_extra
+import polyhedral_lattice.finsupp
+import polyhedral_lattice.quotient
+import polyhedral_lattice.cosimplicial
+import system_of_complexes.completion
+import system_of_complexes.shift_sub_id
+import system_of_complexes.rescale
+import system_of_complexes.truncate
+import system_of_complexes.basic
+import system_of_complexes.double
+import invpoly.basic
+import invpoly.ses
+import invpoly.functor
+import normed_group.controlled_exactness
+import normed_group.pseudo_normed_group
+import normed_group.normed_with_aut
+import for_mathlib.triangle
+import for_mathlib.preserves_exact
+import for_mathlib.wide_pullback_iso
+import for_mathlib.random_homological_lemmas
+import for_mathlib.nnreal_int_binary
+import for_mathlib.int
+import for_mathlib.real
+import for_mathlib.nat_trans
+import for_mathlib.homological_complex_equiv_functor_category
+import for_mathlib.is_biprod
+import for_mathlib.is_iso_neg
+import for_mathlib.nnreal_nat_binary
+import for_mathlib.free_abelian_group2
+import for_mathlib.ab4
+import for_mathlib.short_exact
+import for_mathlib.monoidal_category
+import for_mathlib.yoneda
+import for_mathlib.exact_lift_desc
+import for_mathlib.homological_complex_op
+import for_mathlib.snake_lemma3
+import for_mathlib.homology_map_datum
+import for_mathlib.short_complex
+import for_mathlib.bicartesian4
+import for_mathlib.chain_complex_cons
+import for_mathlib.map_to_sheaf_is_iso
+import for_mathlib.bicartesian3
+import for_mathlib.homotopy_category_coproducts
+import for_mathlib.AddCommGroup
+import for_mathlib.snake_lemma_naturality
+import for_mathlib.snake_lemma2
+import for_mathlib.ab5
+import for_mathlib.rational_cones
+import for_mathlib.exact_filtered_colimits
+import for_mathlib.homology_iso_datum
+import for_mathlib.homotopy_category_lemmas
+import for_mathlib.is_quasi_iso_sigma
+import for_mathlib.Gordan
+import for_mathlib.pi_induced
+import for_mathlib.ennreal
+import for_mathlib.homological_complex_map_d_to_d_from
+import for_mathlib.exact_seq3
+import for_mathlib.ab52
+import for_mathlib.tsum
+import for_mathlib.pid
+import for_mathlib.whisker_adjunction
+import for_mathlib.snake_lemma_naturality2
+import for_mathlib.has_homology
+import for_mathlib.internal_hom
+import for_mathlib.homological_complex_abelian
+import for_mathlib.sum_str
+import for_mathlib.bicartesian
+import for_mathlib.product_op
+import for_mathlib.order
+import for_mathlib.homotopy_category
+import for_mathlib.homotopy_category_pretriangulated
+import for_mathlib.coprod_op
+import for_mathlib.complex_extend
+import for_mathlib.short_complex_projections
+import for_mathlib.kernel_comparison
+import for_mathlib.snake_lemma
+import for_mathlib.sheafification_equiv_compatibility
+import for_mathlib.homology_map
+import for_mathlib.split_exact
+import for_mathlib.homology_lift_desc
+import for_mathlib.derived_functor
+import for_mathlib.abelian_category
+import for_mathlib.types
+import for_mathlib.ext
+import for_mathlib.derived_functor_zero
+import for_mathlib.equalizers
+import for_mathlib.abelian_group_object
+import for_mathlib.nnrat
+import for_mathlib.cech
+import for_mathlib.limit_flip_comp_iso
+import for_mathlib.colim_preserves_colimits
+import for_mathlib.imker
+import for_mathlib.homology
+import for_mathlib.neg_one_pow
+import for_mathlib.truncation_Ext
+import for_mathlib.fin_functor
+import for_mathlib.exact_seq
+import for_mathlib.homological_complex
+import for_mathlib.SemiNormedGroup
+import for_mathlib.exact_seq2
+import for_mathlib.mapping_cone
+import for_mathlib.FreeAb
+import for_mathlib.homotopy_category_op
+import for_mathlib.equivalence_additive
+import for_mathlib.single_coproducts
+import for_mathlib.Ext_quasi_iso
+import for_mathlib.projectives
+import for_mathlib.module_epi
+import for_mathlib.horseshoe
+import for_mathlib.pow_functor
+import for_mathlib.projective_replacement
+import for_mathlib.salamander
+import for_mathlib.sheaf
+import for_mathlib.homological_complex_shift
+import for_mathlib.homotopy_category_functor_compatibilities
+import for_mathlib.topology
+import for_mathlib.AddCommGroup_instances
+import for_mathlib.short_complex_homological_complex
+import for_mathlib.short_complex_colimits
+import for_mathlib.nnreal_to_nat_colimit
+import for_mathlib.homology_exact
+import for_mathlib.is_iso_iota
+import for_mathlib.arrow_preadditive
+import for_mathlib.acyclic
+import for_mathlib.hom_single_iso2
+import for_mathlib.hom_single_iso
+import for_mathlib.preadditive_yoneda
+import for_mathlib.homological_complex2
+import for_mathlib.exact_functor
+import for_mathlib.commsq
+import for_mathlib.bicartesian2
+import for_mathlib.preserves_limits
+import for_mathlib.chain_complex_exact
+import for_mathlib.homology_iso
+import for_mathlib.composable_morphisms
+import for_mathlib.nat_iso_map_homological_complex
+import for_mathlib.homology_iso_Ab
+import for_mathlib.nnreal
+import for_mathlib.SheafOfTypes_sheafification
+import for_mathlib.additive_functor
+import for_mathlib.is_quasi_iso
+import for_mathlib.presieve
+import for_mathlib.embed_preserves_colimits
+import for_mathlib.sheafification_mono
+import for_mathlib.unflip
+import for_mathlib.is_locally_constant
+import for_mathlib.les_homology
+import for_mathlib.quotient_map
+import for_mathlib.yoneda_left_exact
+import for_mathlib.Fintype
+import for_mathlib.concrete_equalizer
+import for_mathlib.SemiNormedGroup_ulift
+import for_mathlib.short_complex_functor_category
+import for_mathlib.two_step_resolution
+import for_mathlib.CompHaus
+import for_mathlib.truncation
+import for_mathlib.free_abelian_group
+import for_mathlib.exact_seq4
+import for_mathlib.preserves_finite_limits
+import for_mathlib.triangle_shift
+import for_mathlib.fintype_induction
+import for_mathlib.has_homology_aux
+import for_mathlib.pullbacks
+import for_mathlib.free_abelian_exact
+import for_mathlib.short_exact_sequence
+import for_mathlib.universal_delta_functor.basic
+import for_mathlib.universal_delta_functor.Ext
+import for_mathlib.Profinite.arrow_limit
+import for_mathlib.Profinite.clopen_limit
+import for_mathlib.Profinite.extend
+import for_mathlib.Profinite.product
+import for_mathlib.Profinite.compat_discrete_quotient
+import for_mathlib.Profinite.disjoint_union
+import for_mathlib.Profinite.quotient_map
+import for_mathlib.arrow.split
+import for_mathlib.abelian_sheaves.functor_category
+import for_mathlib.abelian_sheaves.exact
+import for_mathlib.abelian_sheaves.main
+import for_mathlib.endomorphisms.ab4
+import for_mathlib.endomorphisms.basic
+import for_mathlib.endomorphisms.homology
+import for_mathlib.endomorphisms.functor
+import for_mathlib.endomorphisms.Ext
+import for_mathlib.AddCommGroup.ab4
+import for_mathlib.AddCommGroup.tensor_short_exact
+import for_mathlib.AddCommGroup.epi
+import for_mathlib.AddCommGroup.explicit_products
+import for_mathlib.AddCommGroup.explicit_limits
+import for_mathlib.AddCommGroup.exact
+import for_mathlib.AddCommGroup.tensor
+import for_mathlib.AddCommGroup.kernels
+import for_mathlib.AddCommGroup.direct_sum_colimit
+import for_mathlib.AddCommGroup.pt
+import for_mathlib.derived.lemmas
+import for_mathlib.derived.defs
+import for_mathlib.derived.les
+import for_mathlib.derived.homological
+import for_mathlib.derived.les3
+import for_mathlib.derived.ProjectiveResolution
+import for_mathlib.derived.derived_cat
+import for_mathlib.derived.ext_coproducts
+import for_mathlib.derived.les2
+import for_mathlib.derived.example
+import for_mathlib.derived.K_projective
+import for_mathlib.derived.bounded_homotopy_category
+import for_mathlib.derived.Ext_lemmas
+import for_mathlib.derived.les_facts
+import for_mathlib.simplicial.complex
+import for_mathlib.simplicial.iso
+import for_mathlib.Cech.homotopy
+import for_mathlib.Cech.split
+import for_mathlib.Cech.adjunction
+import laurent_measures.aux_lemmas
+import laurent_measures.thm69
+import laurent_measures.basic
+import laurent_measures.bounded
+import laurent_measures.prop72
+import laurent_measures.int_nat_shifts
+import laurent_measures.simpler_laurent_measures
+import laurent_measures.ext
+import laurent_measures.ses
+import laurent_measures.ses2
+import laurent_measures.no_longer_needed_maybe
+import laurent_measures.theta
+import laurent_measures.condensed
+import laurent_measures.functor
+import rescale.basic
+import rescale.polyhedral_lattice
+import rescale.FiltrationPow
+import rescale.normed_group
+import rescale.CLC
+import rescale.Tinv
+import rescale.pseudo_normed_group
+import rescale.LC
+import data.list.sort meta.expr system.io
+
+open tactic declaration environment io io.fs (put_str_ln close)
+
+-- The next instance is there to prevent PyYAML trying to be too smart
+meta def my_name_to_string : has_to_string name :=
+⟨λ n, "\"" ++ to_string n ++ "\""⟩
+
+local attribute [instance] my_name_to_string
+
+meta def pos_line (p : option pos) : string :=
+match p with
+| some x := to_string x.line
+| _      := ""
+end
+
+meta def file_name (p : option string) : string :=
+match p with
+| some x := x
+| _      := ""
+end
+
+meta def print_item_crawl (env : environment) (h : handle) (decl : declaration) : io unit :=
+let name := decl.to_name in
+do
+   put_str_ln h ((to_string name) ++ ":"), 
+   put_str_ln h  ("  File: " ++ file_name (env.decl_olean name)),
+   put_str_ln h ("  Line: " ++ pos_line (env.decl_pos name))
+
+/-- itersplit l n will cut a list l into 2^n pieces (not preserving order) -/
+meta def itersplit {α} : list α → ℕ → list (list α)
+| l 0 := [l]
+| l 1 := let (l1, l2) := l.split in [l1, l2]
+| l (k+2) := let (l1, l2) := l.split in itersplit l1 (k+1) ++ itersplit l2 (k+1)
+
+meta def main : io unit :=
+do curr_env ← run_tactic get_env,
+   h ← mk_file_handle "decls.yaml" mode.write,
+   let decls := curr_env.fold [] list.cons,
+   let filtered_decls := decls.filter
+     (λ x, not (to_name x).is_internal),
+   let pieces := itersplit filtered_decls 3,
+   pieces.mmap' (λ l, l.mmap' (print_item_crawl curr_env h)),
+   close h
