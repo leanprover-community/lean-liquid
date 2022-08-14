5828498c83c04cdb96c1a0290ae61d309810f046
Adam Topaz <github@adamtopaz.com>
2022-08-13 19:54:53 -0600

    kill some chunky sorries

--- src/examples/Radon/defs.lean
+++ src/examples/Radon/defs.lean
@@ -68,14 +68,102 @@ def weak_dual_C_to_LC (X : Profinite.{0}) :
   weak_dual ℝ C(X,ℝ) →L[ℝ] weak_dual ℝ (locally_constant X ℝ) :=
 weak_dual.comap $ lc_to_c _
 
+lemma dense_range_coe₂ (X : Profinite.{0}) :
+  dense_range (λ p : locally_constant X ℝ × locally_constant X ℝ,
+    (lc_to_c X p.1, lc_to_c X p.2)) :=
+begin
+  letI : uniform_space (locally_constant.pkg X ℝ).space :=
+    (locally_constant.pkg X ℝ).uniform_struct,
+  exact (locally_constant.pkg X ℝ).dense.prod_map (locally_constant.pkg X ℝ).dense
+end
+
 def weak_dual_LC_to_C (X : Profinite.{0}) :
   weak_dual ℝ (locally_constant X ℝ) →ₗ[ℝ] weak_dual ℝ C(X,ℝ) :=
 { to_fun := λ f,
   { to_fun := (locally_constant.pkg X ℝ).extend f,
-    map_add' := sorry,
-    map_smul' := sorry,
+    map_add' := begin
+      letI : uniform_space (locally_constant.pkg X ℝ).space :=
+        (locally_constant.pkg X ℝ).uniform_struct,
+      letI : add_group (locally_constant.pkg X ℝ).space :=
+        continuous_map.add_group,
+      letI : topological_add_group (locally_constant.pkg X ℝ).space :=
+        continuous_map.topological_add_group,
+      rw ← prod.forall',
+      refine is_closed_property (dense_range_coe₂ X) _ _,
+      { apply is_closed_eq,
+        { refine (locally_constant.pkg X ℝ).continuous_extend.comp continuous_add },
+        { refine continuous.add _ _;
+          refine (locally_constant.pkg X ℝ).continuous_extend.comp _,
+          exact continuous_fst,
+          exact continuous_snd } },
+      { rintro ⟨φ, ψ⟩, dsimp only,
+        have hf := continuous_linear_map.uniform_continuous f,
+        rw [← (lc_to_c X).map_add],
+        erw [(locally_constant.pkg X ℝ).extend_coe hf, (locally_constant.pkg X ℝ).extend_coe hf,
+          (locally_constant.pkg X ℝ).extend_coe hf, map_add], }
+    end,
+    map_smul' := begin
+      letI : uniform_space (locally_constant.pkg X ℝ).space :=
+        (locally_constant.pkg X ℝ).uniform_struct,
+      letI : add_group (locally_constant.pkg X ℝ).space :=
+        continuous_map.add_group,
+      letI : topological_add_group (locally_constant.pkg X ℝ).space :=
+        continuous_map.topological_add_group,
+      letI : has_smul ℝ (locally_constant.pkg X ℝ).space :=
+        continuous_map.has_smul,
+      letI : has_continuous_smul ℝ (locally_constant.pkg X ℝ).space :=
+        continuous_map.has_continuous_smul,
+      intros r φ,
+      apply (locally_constant.pkg X ℝ).induction_on φ; clear φ,
+      { apply is_closed_eq,
+        { refine (locally_constant.pkg X ℝ).continuous_extend.comp
+            (continuous_const.smul continuous_id), },
+        { refine continuous_const.smul (locally_constant.pkg X ℝ).continuous_extend } },
+      { intro φ,
+        have hf := continuous_linear_map.uniform_continuous f,
+        erw [← (lc_to_c X).map_smul, (locally_constant.pkg X ℝ).extend_coe hf,
+          (locally_constant.pkg X ℝ).extend_coe hf, map_smul],
+        refl }
+    end,
     cont := (locally_constant.pkg X ℝ).continuous_extend },
-  map_add' := sorry,
-  map_smul' := sorry }
+  map_add' := begin
+    letI : uniform_space (locally_constant.pkg X ℝ).space :=
+      (locally_constant.pkg X ℝ).uniform_struct,
+    intros f g, ext ff, dsimp,
+    apply (locally_constant.pkg X ℝ).induction_on ff,
+    { apply is_closed_eq,
+      apply (locally_constant.pkg X ℝ).continuous_extend, apply_instance,
+      let G : C(X,ℝ) → ℝ × ℝ := λ ff,
+        ((locally_constant.pkg X ℝ).extend f ff, (locally_constant.pkg X ℝ).extend g ff),
+      change continuous ((λ a : ℝ × ℝ, a.1 + a.2) ∘ G),
+      refine continuous.comp continuous_add _,
+      apply continuous.prod_mk,
+      apply (locally_constant.pkg X ℝ).continuous_extend, apply_instance,
+      apply (locally_constant.pkg X ℝ).continuous_extend, apply_instance },
+    { intros a,
+      rw [(locally_constant.pkg X ℝ).extend_coe],
+      rw [(locally_constant.pkg X ℝ).extend_coe],
+      rw [(locally_constant.pkg X ℝ).extend_coe],
+      refl,
+      any_goals { apply continuous_linear_map.uniform_continuous },
+      any_goals { apply_instance } },
+  end,
+  map_smul' := begin
+    letI : uniform_space (locally_constant.pkg X ℝ).space :=
+      (locally_constant.pkg X ℝ).uniform_struct,
+    intros r f, ext ff, dsimp,
+    apply (locally_constant.pkg X ℝ).induction_on ff,
+    { apply is_closed_eq,
+      apply (locally_constant.pkg X ℝ).continuous_extend, apply_instance,
+      refine continuous.comp (continuous_mul_left r) _,
+      apply (locally_constant.pkg X ℝ).continuous_extend, apply_instance },
+    { intros a,
+      rw [(locally_constant.pkg X ℝ).extend_coe],
+      rw [(locally_constant.pkg X ℝ).extend_coe],
+      refl,
+      any_goals { apply_instance },
+      exact f.uniform_continuous,
+      exact (r • f).uniform_continuous },
+  end }
 
 end Profinite
